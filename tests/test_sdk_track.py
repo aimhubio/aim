24b4a763d13349c89448eb0a7ffca8e8dad58735
Gor Arakelyan <arakelyangor10@gmail.com>
2019-10-09 16:00:48 +0400

    add sdk tracking tests

---
+++ tests/test_sdk_track.py
@@ -0,0 +1,11 @@
+import unittest
+import random
+
+from aim.sdk.track import Loss
+from aim.sdk.track.track import track
+
+
+class TestSDKTrack(unittest.TestCase):
+    def test_sdk_track(self):
+        for e in range(100):
+            track(Loss(random.random() / e))
