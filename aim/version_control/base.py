f617699d24978419d4c4c51dea21b5c0de720fd1
Gor Arakelyan <arakelyangor10@gmail.com>
2019-11-13 14:06:54 +0400

    add version control adapter abstract  methods

--- aim/version_control/base.py
+++ aim/version_control/base.py
@@ -3,5 +3,13 @@ from abc import ABC, abstractmethod
 
 class Base(ABC):
     @abstractmethod
+    def commit(self):
+        ...
+
+    @abstractmethod
+    def checkout(self):
+        ...
+
+    @abstractmethod
     def get_diff(self):
         ...
--- aim/version_control/git.py
+++ aim/version_control/git.py
@@ -1,6 +1,22 @@
 from aim.version_control.base import Base
+import os
+
+from git import Repo
 
 
 class GitAdapter(Base):
+    def commit(self):
+        ...
+
+    def checkout(self):
+        ...
+
     def get_diff(self):
-        print('Git diff')
+        repo = Repo(os.environ['PWD'], search_parent_directories=True)
+        h_commit = repo.head.commit
+        diff = h_commit.diff('HEAD~1', create_patch=True)
+        change_types = ('A', 'C', 'D', 'R', 'M', 'T')
+
+        for change_type in change_types:
+            for diff_item in diff.iter_change_type(change_type):
+                print(change_type, diff_item)
