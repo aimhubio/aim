4f3baae4739d4c9eb51dea8aea2c88d2a566cb3f
jialin-wu-02 <jialin.wu.02@gmail.com>
2020-05-22 16:50:56 -0700

    fix typo & add pytest.ini

--- tests/engine/test_aim_repo.py
+++ tests/engine/test_aim_repo.py
@@ -9,7 +9,7 @@ class TestAimRepo(unittest.TestCase):
     @staticmethod
     def get_aim_repo(directory):
         """
-        Searches for .aim repository in the givne directory
+        Searches for .aim repository in the given directory
         and returns AimRepo object if exists
         """
         # Try to find closest .aim repository
@@ -33,12 +33,12 @@ class TestAimRepo(unittest.TestCase):
         """
         Test aim repo can be created correctly.
         """
-        prefix = os.getcwd() + "/"
+        prefix = os.getcwd() + '/'
         with tempfile.TemporaryDirectory(prefix=prefix) as tmpdirname:
             ar = AimRepo(tmpdirname)
             ar.init()
             # repo should be found in the current temporary folder.
-            self.assertNotEqual(TestAimRepo.get_aim_repo(tmpdirname), None, "AimRepo not created successfully.")
+            self.assertNotEqual(TestAimRepo.get_aim_repo(tmpdirname), None, 'AimRepo not created successfully.')
             self.assertTrue(ar.exists())
             # Examine config & branch
             self.assertEqual(ar.config.get('active_branch'), AIM_DEFAULT_BRANCH_NAME)
@@ -48,7 +48,7 @@ class TestAimRepo(unittest.TestCase):
         """
         Test aim repo can be removed correctly.
         """
-        prefix = os.getcwd() + "/"
+        prefix = os.getcwd() + '/'
         with tempfile.TemporaryDirectory(prefix=prefix) as tmpdirname:
             ar = AimRepo(tmpdirname)
             ar.init()
@@ -62,12 +62,12 @@ class TestAimRepo(unittest.TestCase):
         Init => Create branch test => checkout test => remove test
         => checkout test again => remove test again
         """
-        prefix = os.getcwd() + "/"
-        branch_name = "test"
+        prefix = os.getcwd() + '/'
+        branch_name = 'test'
         with tempfile.TemporaryDirectory(prefix=prefix) as tmpdirname:
             ar = AimRepo(tmpdirname)
             ar.init()
-            # create branch "test"
+            # create branch 'test'
             ar.create_branch(branch_name)
             self.assertEqual(ar.config.get('active_branch'), AIM_DEFAULT_BRANCH_NAME)
             branches = ar.config.get('branches')
@@ -77,59 +77,59 @@ class TestAimRepo(unittest.TestCase):
                 if b.get('name') == branch_name:
                     is_found = True
             self.assertTrue(is_found)
-            # checkout branch "test"
+            # checkout branch 'test'
             ar.checkout_branch(branch_name)
             self.assertEqual(ar.config.get('active_branch'), branch_name)
-            # remove branch "test"
+            # remove branch 'test'
             ar.remove_branch(branch_name)
             branches = ar.config.get('branches')
             self.assertEqual(len(branches), 1)
-            # checkout branch "test" again
+            # checkout branch 'test' again
             self.assertRaises(AttributeError, ar.checkout_branch, branch_name)
-            # remove branch "test" again
+            # remove branch 'test' again
             self.assertRaises(AttributeError, ar.remove_branch, branch_name)
     
     def test_commits(self):
         """
         Test commit operations.
         Init => store test file => commit in default branch 
-        => checkout "test" branch => store another test file => commit
+        => checkout 'test' branch => store another test file => commit
         """
-        prefix = os.getcwd() + "/"
-        branch_name = "test"
+        prefix = os.getcwd() + '/'
+        branch_name = 'test'
         with tempfile.TemporaryDirectory(prefix=prefix) as tmpdirname:
             ar = AimRepo(tmpdirname)
             ar.init()
             # store a random test file used to commit
             test_file_one_path = os.path.join(ar.index_path, 'test_file_one.txt')
-            test_file_one = open(test_file_one_path,"w+")
-            test_file_one.write("Test file one contents.")
+            test_file_one = open(test_file_one_path,'w+')
+            test_file_one.write('Test file one contents.')
             test_file_one.close()
-            ar.store_file("test_file_one", "test_file_one", ("text",))
+            ar.store_file('test_file_one', 'test_file_one', ('text',))
             # commit in master branch
-            result = ar.commit("1", "test commit message first")
+            result = ar.commit('1', 'test commit message first')
             # after the commit, there shoukd be three files (config, meta, test_file_one)
-            self.assertEqual(len(ar.ls_commit_files("default", "1")), 3)
+            self.assertEqual(len(ar.ls_commit_files('default', '1')), 3)
             self.assertEqual(result, {
-                'branch': "default",
-                'commit': "1"
+                'branch': 'default',
+                'commit': '1'
             })
             # checkout to test branch
             ar.create_branch(branch_name)
             ar.checkout_branch(branch_name)
             # store random test file
             test_file_two_path = os.path.join(ar.index_path, 'test_file_two.txt')
-            test_file_two = open(test_file_two_path,"w+")
-            test_file_two.write("Test file one contents.")
+            test_file_two = open(test_file_two_path,'w+')
+            test_file_two.write('Test file one contents.')
             test_file_two.close()
             # commit again in test branch
-            result_two = ar.commit("2", "test commit message second")
+            result_two = ar.commit('2', 'test commit message second')
             self.assertEqual(result_two, {
                 'branch': 'test',
                 'commit': '2'
             })
             # should be two files (config, test_file_two)
-            self.assertEqual(len(ar.ls_commit_files("test", "2")), 2)
+            self.assertEqual(len(ar.ls_commit_files('test', '2')), 2)
 
 
 
