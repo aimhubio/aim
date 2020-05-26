3abe91db77be89292832eb8cb52515f64554d130
James Jiao <james_jiao@berkeley.edu>
2020-05-26 10:52:02 -0700

    change serializtion from json to protobuf

--- aim/sdk/artifacts/text.py
+++ aim/sdk/artifacts/text.py
@@ -3,30 +3,44 @@ import time
 
 from aim.sdk.artifacts import Record
 from aim.sdk.artifacts.artifact import Artifact
+from aim.sdk.artifacts.proto.text_pb2 import TextRecord
 
 
 class Text(Artifact, metaclass=ABCMeta):
     cat = ('text',)
     _step_counter = {}
 
-    def __init__(self, document_name: str, text: str):
-        self.document_name = document_name
+    def __init__(self, name: str, text: str, epoch: int = None, step: int = None):
+        self.name = name
         self.text = text
         self.timestamp = int(time.time())
+
+        self.epoch = epoch or 0
+
+        if step is not None:
+            self.step = step
+        else:
+            self._step_counter.setdefault(name, 0)
+            self._step_counter[name] += 1
+            self.step = self._step_counter[name]
 
         super(Text, self).__init__(self.cat)
 
     def __str__(self):
-        return '{document_name}: {text}'.format(document_name=self.document_name,
+        return '{document_name}: {text}'.format(document_name=self.name,
                                                 text=self.text)
 
     def serialize(self) -> Record:
-        content = {"text": self.text, "timestamp": self.timestamp}
+        text_pb = TextRecord()
+        text_pb.text = self.text
+
+        data_bytes = self.serialize_pb_object(text_pb, self.step, self.epoch)
 
         return Record(
-            name=self.document_name,
+            name=self.name,
             cat=self.cat,
-            content=content
+            content=data_bytes,
+            binary_type=self.PROTOBUF
         )
 
     def save_blobs(self, name: str, abs_path: str = None):
