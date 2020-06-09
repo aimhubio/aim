c9e86bf4116a5520a371ba9a30b0258d24db2dcd
Gor Arakelyan <arakelyangor10@gmail.com>
2020-06-09 15:44:21 +0400

    Execute process and store logs into a commit of specific experiment (#149)

--- aim/sdk/init.py
+++ aim/sdk/init.py
@@ -3,19 +3,42 @@ import uuid
 import time
 
 from aim.engine.aim_repo import AimRepo
+from aim.engine.configs import (
+    AIM_BRANCH_ENV_VAR,
+    AIM_COMMIT_ENV_VAR,
+    AIM_AUTOMATED_EXEC_ENV_VAR,
+)
 
 
 def init(overwrite=False):
+    # Get ENV VARS
+    branch_name = os.getenv(AIM_BRANCH_ENV_VAR)
+    commit_hash = os.getenv(AIM_COMMIT_ENV_VAR)
+    automated_exec = os.getenv(AIM_AUTOMATED_EXEC_ENV_VAR)
+
     # Init repo if doesn't exist and return repo instance
-    repo = AimRepo.get_working_repo()
+    repo = AimRepo.get_working_repo(branch_name, commit_hash)
     if not repo:
-        repo = AimRepo(os.getcwd())
+        repo = AimRepo(os.getcwd(), branch_name, commit_hash)
         repo.init()
 
-    # Check if repo index is empty or not
-    # Reset index or commit according to `overwrite` argument
-    if not repo.is_index_empty():
-        if overwrite:
-            repo.reset_index()
-        else:
-            repo.commit(str(uuid.uuid1()), int(time.time()))
+    if not automated_exec:
+        # Check if repo index is empty or not
+        # Reset index or commit according to `overwrite` argument
+        if not repo.is_index_empty():
+            if overwrite:
+                repo.reset_index()
+            else:
+                repo.commit(str(uuid.uuid1()), int(time.time()))
+    else:
+        # TODO: handle automated training overwrite
+        pass
+
+    # Handle aim commit
+    if automated_exec:
+        # Init commit
+        repo.commit_init()
+
+        # Finish commit
+        import atexit
+        atexit.register(repo.commit_finish)
