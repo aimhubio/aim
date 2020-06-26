7c1b255301c5d63adf7d9716f8be0e3f6d378c41
bhaveshk658 <59468276+bhaveshk658@users.noreply.github.com>
2020-06-26 08:15:15 -0700

    Add tensorflow 1.x integration with checkpoints and saving/loading models. (#158)
    * Save models using Saver + example
    
    * Test loading models with an example
    
    * Add tensorflow check from distribution branch
    
    * Copy .aim file and unzip to restore model
    
    * Clean up examples
    
    * Clarify models and kept sess open, added TfUtils
    
    * Use TemporaryDirectory for zip file and updated examples

--- aim/engine/utils.py
+++ aim/engine/utils.py
@@ -121,6 +121,11 @@ def is_pytorch_module(inst):
     """
     return inst_has_typename(inst, ['torch', 'Module'])
 
+def is_tensorflow_session(inst):
+    """
+    Check whether `inst` is isntance of tensorflow session
+    """
+    return inst_has_typename(inst, ['tensorflow', 'session'])
 
 def is_pytorch_optim(inst):
     """
---
+++ examples/tf_model_save.py
@@ -0,0 +1,118 @@
+import aim
+from aim import track
+from aim.artifacts.utils import TfUtils
+
+import tensorflow as tf
+
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+learning_rate = 0.1
+num_steps = 500
+batch_size = 128
+display_step = 100
+
+# Network Parameters
+n_hidden_1 = 256 # 1st layer number of neurons
+n_hidden_2 = 256 # 2nd layer number of neurons
+n_hidden_3 = 256 # 3rd layer number of neurons
+num_input = 784 # MNIST data input (img shape: 28*28)
+num_classes = 10 # MNIST total classes (0-9 digits)
+
+# tf Graph input
+X = tf.placeholder("float", [None, num_input])
+Y = tf.placeholder("float", [None, num_classes])
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, num_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([num_classes]))
+}
+
+
+# Create model
+def neural_net_with_ops(x):
+    # Hidden fully connected layer with 256 neurons
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+
+    # Hidden fully connected layer with 256 neurons
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+
+    # Output fully connected layer with a neuron for each class
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+
+    return out_layer
+
+
+def neural_net_with_layers(x):
+    # Input Layer
+    input_layer = tf.layers.dense(inputs=x, units=num_input, name='input')
+
+    # Hidden fully connected layer with 256 neurons  # can have `name` parameter
+    hidden_1 = tf.layers.dense(inputs=input_layer, units=n_hidden_1, activation=tf.nn.relu, name='hidden-1')
+    hidden_2 = tf.layers.dense(inputs=hidden_1, units=n_hidden_2, activation=tf.nn.relu, name='hidden-2')
+    hidden_3 = tf.layers.dense(inputs=hidden_2, units = n_hidden_3, activation=tf.nn.relu, name='hidden-3')
+
+    # Output fully connected layer with a neuron for each class
+    out_layer = tf.layers.dense(inputs=hidden_3, units=num_classes, name='output')
+
+    return out_layer
+
+
+logits = neural_net_with_ops(X)
+
+# Define loss and optimizer
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(loss_op)
+
+# Evaluate model (with test logits, for dropout to be disabled)
+correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    # Run the initializer
+    sess.run(init)
+
+    for e in range(10):
+        for step in range(1, num_steps+1):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            # Run optimization op (backprop)
+            sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+            if step % display_step == 0 or step == 1:
+                # Calculate batch loss and accuracy
+                loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
+                                                                     Y: batch_y,
+                                                                     })
+
+                print("Step " + str(step) + ", Epoch " + str(e+1) +
+                      ", Minibatch Loss= " +
+                      "{:.4f}".format(loss) + ", Training Accuracy= " +
+                      "{:.3f}".format(acc))
+        track(aim.checkpoint,
+             'checkpoint_test', 'chp_epoch_{}'.format(e),
+              sess, e, lr_rate=learning_rate,
+              meta={
+                  'learning_rate': learning_rate,
+                  'batch_size': batch_size,
+                  'classes': num_classes,
+              })
+        learning_rate = learning_rate / 2.0
+    print("Optimization Finished!")
+
+    # Calculate accuracy for MNIST test images
+    print("Testing Accuracy:", \
+        sess.run(accuracy, feed_dict={X: mnist.test.images,
+                                      Y: mnist.test.labels}))
