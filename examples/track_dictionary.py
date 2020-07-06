b870ac1573e75b772998b35452cc9bc17084993e
Gor Arakelyan <arakelyangor10@gmail.com>
2020-07-06 17:25:04 +0400

    Add general dictionary artifact for tracking

--- examples/track_dictionary.py
+++ examples/track_dictionary.py
@@ -4,12 +4,16 @@ from aim import track
 
 aim.init()
 
-track(aim.hyperparams, {
+track(aim.dictionary, {
     'num_epochs': 10,
     'fc_units': 128,
-})
+}, namespace='params')
 
-track(aim.dataset, {
+track(aim.dictionary, {
     'name': 'Dataset name',
     'version': 'Dataset version',
+}, namespace='dataset')
+
+track(aim.dictionary, {
+    'foo': 'bar',
 })
