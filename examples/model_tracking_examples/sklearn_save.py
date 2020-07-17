78a3e666ce16638e8f58f8cf7707e22dd2106b86
Bhavesh Kalisetti <bhaveshk658@berkeley.edu>
2020-07-17 10:57:23 -0700

    Scikit-learn model saving example

---
+++ examples/model_tracking_examples/sklearn_save.py
@@ -0,0 +1,23 @@
+
+# Save Model Using Pickle
+import aim
+from aim import track
+
+import pandas
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+import pickle
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+test_size = 0.33
+seed = 7
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
+# Fit the model on training set
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+# save the model to disk
+track(aim.checkpoint, 'checkpoint-test', 'diabetes', model, 0, 0, meta={})
