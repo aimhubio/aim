64a697b943ebd065789b4981a73fe33d96e57d62
Gor Arakelyan <arakelyangor10@gmail.com>
2020-08-03 14:32:30 +0400

    Tidy up init files

--- aim/ql/__init__.py
+++ aim/ql/__init__.py
@@ -1,3 +1,2 @@
-from aim.ql.tree import AbstractSyntaxTree, BinaryExpressionTree
-from aim.ql.grammar.expression import Expression
-from aim.ql.grammar.atom import Atom
+from aim.ql.tree import *
+from aim.ql.grammar import *
