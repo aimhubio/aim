3fa9dfda352aa1cb8907a8ea15d0a6fb4806c79e
Gor Arakelyan <arakelyangor10@gmail.com>
2020-09-01 16:44:07 +0400

    Add Aim adapter for keras

--- aim/sdk/__init__.py
+++ aim/sdk/__init__.py
@@ -1,8 +1,10 @@
+# Main functions
 from aim.sdk.track import track, set_params
 from aim.sdk.init import init
 from aim.sdk.select import select
 from aim.sdk.session import Session
 
+# Artifact identifiers
 from aim.artifacts import (
     metric,
     hyperparams,
