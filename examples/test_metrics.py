4fa366010599975e526c5d33de4213a104620efc
Gor Arakelyan <arakelyangor10@gmail.com>
2020-11-06 18:31:41 +0400

    Move aim flush command to separate file

--- examples/test_metrics.py
+++ examples/test_metrics.py
@@ -15,5 +15,4 @@ for i in range(100, 4000):
     sess.track(i, name='metric')
     sess.track(i*2, name='metric', subset='train', foo='baz')
     sess.track(i*3, name='metric', subset='test', bar='baz')
-    sess.flush()
     time.sleep(0.1)
