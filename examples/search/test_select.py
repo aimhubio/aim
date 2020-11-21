c9a15ac405c5baf134717bf01a32e907b28f507e
Gor Arakelyan <arakelyangor10@gmail.com>
2020-11-21 13:44:12 +0400

    Return only selected metrics in SelectResult

--- examples/search/test_select.py
+++ examples/search/test_select.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from aim.engine.repo.repo import AimRepo
 from aim.ql.grammar.statement import Statement
 
@@ -6,9 +8,14 @@ if __name__ == '__main__':
     repo = AimRepo.get_working_repo(mode='r')
 
     parser = Statement()
-    parsed_stmt = parser.parse('loss if context.subset != test')
+    parsed_stmt = parser.parse('metric if experiment == test_metrics')
     statement_select = parsed_stmt.node['select']
     statement_expr = parsed_stmt.node['expression']
 
     res = repo.select(statement_select, statement_expr)
-    print(res.get_selected_metrics_context())
+
+    runs_list = []
+    for run in res.runs:
+        runs_list.append(run.to_dict(include_only_selected_agg_metrics=True))
+
+    pprint(runs_list)
