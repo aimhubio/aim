4045f41768f099e84aec59a185b69aaffed43177
khazhak <khazhak.galstyan@codefights.com>
2021-03-16 17:43:51 +0400

    [refactor] hugging face integration finalized

--- aim/sdk/adapters/hugging_face.py
+++ aim/sdk/adapters/hugging_face.py
@@ -1,75 +1,76 @@
-from transformers.trainer_callback import TrainerCallback
+from typing import Optional
+
 from aim.engine.utils import convert_to_py_number
 from aim.sdk.session.session import Session
-from aim.sdk.session.configs import DEFAULT_FLUSH_FREQUENCY
 from aim.sdk.flush import flush
-import importlib.util
 
-def is_aim_available():
-    return importlib.util.find_spec("aim") is not None
+class AimCallback(object):
+    __hf_callback_cls = None
 
+    @staticmethod
+    def __new__(cls, *args, **kwargs):
+        hf_callback_inst = cls.__get_callback_cls()
+        return hf_callback_inst(*args, **kwargs)
 
-class AimCallback(TrainerCallback):
-    """
-    A :class:`~transformers.TrainerCallback` that sends the logs to `Aim <https://aimstack.io/>`__.
-        Args:
-        experiment_name (:obj:`str`, `optional`):
-            The name of the experiment.
-    """
+    @classmethod
+    def __get_callback_cls(cls):
+        if cls.__hf_callback_cls is not None:
+            return cls.__hf_callback_cls
 
-    def __init__(self, experiment_name='default'):
-        has_aim = is_aim_available()
-        assert has_aim, "AimCallback requires aim to be installed. Run `pip install aim`."
-        self._experiment_name = experiment_name
-        self._initialized = False
-        self._current_split = None
+        from transformers.trainer_callback import TrainerCallback
 
-    def setup(self, args, state, model):
-        """
-        Setup the optional Aim integration.
-        """
-        self._initialized = True
+        class _HuggingFaceCallback(TrainerCallback):
+            def __init__(self, experiment_name: Optional[str] = None):
+                self._experiment_name = experiment_name
+                self._initialized = False
+                self._current_shfit = None
 
-        self._aim_session = Session(
-                        repo=args.logging_dir,
-                        experiment=self._experiment_name
-                    )
+            def setup(self, args, state, model):
+                self._initialized = True
 
-        combined_dict = {**args.to_sanitized_dict()}
-        if hasattr(model, "config") and model.config is not None:
-            model_config = model.config.to_dict()
-            combined_dict = {**model_config, **combined_dict}
-        print("yoyo: ", combined_dict)
-        self._aim_session.set_params(combined_dict, name='hparams')
+                self._aim_session = Session(
+                                repo=args.logging_dir,
+                                experiment=self._experiment_name
+                            )
 
-    def on_train_begin(self, args, state, control, model=None, **kwargs):
-        if not self._initialized:
-            self.setup(args, state, model)
-        self._current_split = 'train'
-    
-    def on_evaluate(self, args, state, control, **kwargs):
-        self._current_split = 'val'
-    
-    def on_prediction_step(self, args, state, control, **kwargs):
-        self._current_split = 'test'
+                combined_dict = {**args.to_sanitized_dict()}
+                if hasattr(model, "config") and model.config is not None:
+                    model_config = model.config.to_dict()
+                    combined_dict = {**model_config, **combined_dict}
+                self._aim_session.set_params(combined_dict, name='hparams')
 
-    def on_log(self, args, state, control, logs=None, **kwargs):
-        print("on_log")
-        if not self._initialized:
-            self.setup(args, state, model)
+            def on_train_begin(self, args, state, control, model=None, **kwargs):
+                if not self._initialized:
+                    self.setup(args, state, model)
+                self._current_shfit = 'train'
+            
+            def on_evaluate(self, args, state, control, **kwargs):
+                self._current_shfit = 'val'
+            
+            def on_prediction_step(self, args, state, control, **kwargs):
+                self._current_shfit = 'pred'
 
-        context = {}
-        for k, v in logs.items():
-            context['subset'] = self._current_split
-            print("tracking:  ", convert_to_py_number(v), k, context['subset'])    
-            self._aim_session.track(convert_to_py_number(v), name=k, **context)
-    
-    def on_epoch_end(self, args, state, control, **kwargs):
-        flush_func = self._aim_session.flush \
-            if self._aim_session is not None \
-            else flush
-        flush_func()
+            def on_log(self, args, state, control, logs=None, **kwargs):
+                if not self._initialized:
+                    self.setup(args, state, model)
 
-    def on_train_end(self, args, state, control, **kwargs):
-        print("on_train_end")
-        self._aim_session.close()
+                context = {}
+                for k, v in logs.items():
+                    context['subset'] = self._current_shfit
+                    self._aim_session.track(convert_to_py_number(v), name=k, **context)
+            
+            def on_epoch_end(self, args, state, control, **kwargs):
+                flush_func = self._aim_session.flush \
+                    if self._aim_session is not None \
+                    else flush
+                flush_func()
+
+            def __del__(self):
+                if self._initialized:
+                    self._aim_session.close()
+
+        cls.__hf_callback_cls = _HuggingFaceCallback
+        return cls.__hf_callback_cls
+
+    def __init__(self, experiment: Optional[str] = None ):
+        pass
