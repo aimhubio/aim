8b104b97cdabbfbbddd01ee44c71f0c4f5aef6a4
khazhak <khazhak.galstyan@gmail.com>
2021-05-25 15:46:28 +0400

    feat, opening new session for each KT trial

--- aim/sdk/adapters/keras_tuner.py
+++ aim/sdk/adapters/keras_tuner.py
@@ -31,20 +31,28 @@ class AimCallback(object):
                          = DEFAULT_FLUSH_FREQUENCY):
 
                 self._repo_path = repo
+                self._system_tracking_interval = system_tracking_interval
+                self._flush_frequency = flush_frequency
                 self.tuner = tuner
                 self.trial = None
                 self._experiment_name = experiment
-                self._aim_session = Session(
-                    repo=self._repo_path,
-                    experiment=self._experiment_name,
-                    system_tracking_interval=system_tracking_interval,
-                    flush_frequency=flush_frequency
-                )
+                self._started_trials = []
+                self._aim_session = None
 
             def on_epoch_begin(self, epoch, logs=None):
                 trial_dict = self.tuner.oracle.ongoing_trials
                 tuner_key = next(iter(trial_dict))
                 self._current_trial_id = trial_dict[tuner_key].trial_id
+                if not self._current_trial_id in self._started_trials:
+                    if self._aim_session is not None and self._aim_session.active:
+                        self._aim_session.close()
+                    self._aim_session = Session(
+                        repo=self._repo_path,
+                        experiment=self._experiment_name,
+                        system_tracking_interval=self._system_tracking_interval,
+                        flush_frequency=self._flush_frequency
+                    )
+                    self._started_trials.append(self._current_trial_id)
                 trial = self.tuner.oracle.get_trial(self._current_trial_id)
                 hparams = trial.hyperparameters.values
                 self._aim_session.set_params(hparams, name='hparams')
@@ -62,7 +70,7 @@ class AimCallback(object):
                 self._aim_session.flush()
 
             def __del__(self):
-                if self._aim_session.active:
+                if self._aim_session is not None and self._aim_session.active:
                     self._aim_session.close()
 
         cls.__kt_callback_cls = _KerasTunerCallback
