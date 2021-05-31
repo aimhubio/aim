3d8b869117b852ecd8f47ffcd308eb16c65ef973
mihran113 <vanyanmihran@gmail.com>
2021-06-01 03:11:23 +0400

    Add helper function to process a run from run data gotten from to be added endpoint's (commits/search/metric/align) request body for x_axis custom alignment

--- aim/web/app/commits/utils.py
+++ aim/web/app/commits/utils.py
@@ -1,11 +1,13 @@
 import json
 from typing import Optional
+
 from aim.artifacts.metric import Metric as MetricArtifact
+from aim.engine.repo.run import Run
 from aim.ql.grammar import Expression
 from aim.ql.utils import match
-from aim.web.app.db import db
-from aim.web.app.commits.models import TFSummaryLog
 from aim.web.adapters.tf_summary_adapter import TFSummaryAdapter
+from aim.web.app.commits.models import TFSummaryLog
+from aim.web.app.db import db
 from aim.web.app.utils import normalize_type, unsupported_float_type
 
 
@@ -128,3 +130,91 @@ def process_trace_record(r, trace, x_axis_trace, x_idx):
         base.timestamp,
         x_axis_trace.current_x_axis_value if x_axis_trace else None
     ))
+
+
+def process_custom_aligned_run(project, run_data, x_axis_metric_name) -> Run or None:
+    # get run_hash and experiment_name from request data
+    run_hash = run_data.get('run_hash')
+    experiment_name = run_data.get('experiment_name')
+    if run_hash is None or experiment_name is None:
+        return None
+
+    # initialize Run object and try to get metric corresponding to x_axis_metric_name
+    selected_run = Run(project.repo, experiment_name, run_hash)
+    x_axis_metric = selected_run.get_all_metrics().get(x_axis_metric_name)
+    if x_axis_metric is None:
+        return None
+
+    # open run storage and metric artifact
+    try:
+        selected_run.open_storage()
+        x_axis_metric.open_artifact()
+    except:
+        return None
+
+    # collect selected metrics and traces
+    for metric in run_data.get('metrics'):
+        metric_name = metric.get('name')
+        selected_metric = selected_run.get_all_metrics().get(metric_name)
+        if selected_metric is None:
+            continue
+        for trace in metric.get('traces'):
+            trace_context = trace.get('context')
+            # get the corresponding traces for x_axis_metric and selected_metric
+            selected_trace = selected_metric.get_trace(trace_context)
+            x_axis_trace = x_axis_metric.get_trace(trace_context)
+            if x_axis_trace is None or selected_trace is None:
+                continue
+            # initialize alignment fields for selected_trace
+            selected_trace.alignment = {
+                'is_synced': True,
+                'is_asc': True,
+                'skipped_steps': 0,
+                'aligned_by': {
+                    'metric_name': x_axis_metric.name,
+                    'trace_context': x_axis_trace.context
+                }
+            }
+
+            def process_single_x_axis_trace_record(x_idx):
+                new_x_axis_value = x_axis_trace.tmp_data.get(x_idx)
+                if not new_x_axis_value:
+                    # if there was no available value for x_idx index from trace cached data, read from storage
+                    try:
+                        x_r = next(x_axis_trace.read_records(x_idx))
+                        _, x_axis_metric_record = MetricArtifact.deserialize_pb(x_r)
+                        if not unsupported_float_type(x_axis_metric_record.value):
+                            new_x_axis_value = x_axis_metric_record.value
+                        else:
+                            selected_trace.alignment['skipped_steps'] += 1
+                    except StopIteration:
+                        selected_trace.alignment['is_synced'] = False
+                if new_x_axis_value:
+                    # check if the resulting array is ascending or not
+                    if x_axis_trace.current_x_axis_value and \
+                            new_x_axis_value < x_axis_trace.current_x_axis_value:
+                        selected_trace.alignment['is_asc'] = False
+                    x_axis_trace.current_x_axis_value = new_x_axis_value
+                    x_axis_trace.tmp_data[x_idx] = x_axis_trace.current_x_axis_value
+                return new_x_axis_value
+
+            # get values of x_axis_trace and store them in the data attribute of selected_trace as a plain list
+            start, stop, step = trace.get('slice')
+            for i in range(start, stop, step):
+                x_axis_value = process_single_x_axis_trace_record(i)
+                selected_trace.append(x_axis_value)
+            if (stop-1) % step != 0:
+                x_axis_value = process_single_x_axis_trace_record(stop-1)
+                selected_trace.append(x_axis_value)
+
+            selected_metric.append(selected_trace)
+        selected_run.add(selected_metric)
+
+    # close run storage and metric artifact
+    try:
+        selected_run.close_storage()
+        x_axis_metric.close_artifact()
+    except:
+        pass
+
+    return selected_run
