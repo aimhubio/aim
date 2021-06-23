5bb6ed13db9f713bb5e48977c01449075eef3af1
mihran113 <vanyanmihran@gmail.com>
2021-06-23 05:31:43 +0400

    Move static files serving to FastAPI

--- aim/web/app/views.py
+++ aim/web/app/views.py
@@ -1,42 +1,40 @@
 import os
 
-from flask import Blueprint, send_from_directory
-from flask_restful import Api, Resource
+from aim.web.app.utils import APIRouter  # wrapper for fastapi.APIRouter
+from fastapi.responses import FileResponse
 
 from aim.web.app.projects.project import Project
-from aim.web.app.utils import send_from_directory_gzip_compressed
 
-general_bp = Blueprint('general', __name__)
-general_api = Api(general_bp)
+general_router = APIRouter()
 
 
-def serve_wrong_urls(e):
-    from aim.web.run import application
-    static_dir = os.path.join(os.path.dirname(application.root_path), 'ui', 'build')
-    return send_from_directory_gzip_compressed(static_dir, 'index.html'), 200
+@general_router.get('/static-files/{path:path}/')
+async def serve_static_files(path):
+    from aim.web.run import app
+    static_file_name = os.path.join(app.root_path, 'ui', 'build', path)
+    compressed_file_name = '{}.gz'.format(static_file_name)
+    if os.path.exists(compressed_file_name):
+        return FileResponse(compressed_file_name, headers={'Content-Encoding': 'gzip'})
+    return FileResponse(static_file_name)
 
 
-@general_api.resource('/')
-class ServeMainPage(Resource):
-    def get(self):
-        from aim.web.run import application
-        static_dir = os.path.join(os.path.dirname(application.root_path), 'ui', 'build')
-        return send_from_directory_gzip_compressed(static_dir, 'index.html')
+@general_router.get('/static/{exp_name}/{commit_hash}/media/images/{path}/')
+async def serve_images(exp_name, commit_hash, path):
+    project = Project()
+    image_file = os.path.join(project.repo_path,
+                              exp_name, commit_hash,
+                              'objects', 'media', 'images',
+                              path)
+    return FileResponse(image_file)
 
 
-@general_api.resource('/static-files/<path:path>')
-class ServeStaticFiles(Resource):
-    def get(self, path):
-        from aim.web.run import application
-        static_dir = os.path.join(os.path.dirname(application.root_path), 'ui', 'build')
-        return send_from_directory_gzip_compressed(static_dir, path)
-
-
-@general_api.resource('/static/<exp_name>/<commit_hash>/media/images/<path>')
-class ServeImages(Resource):
-    def get(self, exp_name, commit_hash, path):
-        project = Project()
-        images_dir = os.path.join(project.repo_path,
-                                  exp_name, commit_hash,
-                                  'objects', 'media', 'images')
-        return send_from_directory(images_dir, path)
+# do not change the placement of this method
+# as it also serves as a fallback for wrong url routes
+@general_router.get('/{path:path}/')
+async def serve_index_html():
+    from aim.web.run import app
+    static_file_name = os.path.join(app.root_path, 'ui', 'build', 'index.html')
+    compressed_file_name = '{}.gz'.format(static_file_name)
+    if os.path.exists(compressed_file_name):
+        return FileResponse(compressed_file_name, headers={'Content-Encoding': 'gzip'})
+    return FileResponse(static_file_name)
