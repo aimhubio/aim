22ae5e99973068294c0fec980e0b0144961782a9
Arsen Alkamyan <alkamyan97@gmail.com>
2021-06-25 23:58:40 +0400

    Add Metric page layout structure

--- aim/web/ui_v2/src/pages/Metrics/Metrics.tsx
+++ aim/web/ui_v2/src/pages/Metrics/Metrics.tsx
@@ -1,31 +1,57 @@
 import React from 'react';
-import { Box, Grid, Paper } from '@material-ui/core';
+import { Box, Grid, makeStyles, Paper } from '@material-ui/core';
 import BreadCrumbs from 'components/BreadCrumbs/BreadCrumbs';
 import SelectForm from './components/SelectForm/SelectForm';
-import GroupBy from './components/Grouping/Grouping';
+import Grouping from './components/Grouping/Grouping';
 import Controls from './components/Controls/Controls';
 
+const useStyles = makeStyles(({ spacing }) => ({
+  paper: {
+    padding: spacing(1),
+    margin: spacing(0.5),
+  },
+}));
+
 function Metrics(): React.FunctionComponentElement<React.ReactNode> {
+  const classes = useStyles();
   return (
     <Box component='section'>
       <Grid container direction='column' justify='center'>
         <Grid item>
           <BreadCrumbs />
         </Grid>
-        <Grid item>
-          <Paper square>
-            <SelectForm />
-            <GroupBy />
-          </Paper>
+        <Grid xs={12} item>
+          <Grid container>
+            <Grid xs={10} item>
+              <Paper className={classes.paper} square>
+                <SelectForm />
+              </Paper>
+            </Grid>
+            <Grid item xs={2}>
+              <Paper square className={classes.paper}>
+                <Grouping />
+              </Paper>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Paper>
-            <div>Chart</div>
-            <Controls />
-          </Paper>
+        <Grid xs={12} item>
+          <Grid container>
+            <Grid item xs={11}>
+              <Paper square className={classes.paper}>
+                <div>Chart</div>
+              </Paper>
+            </Grid>
+            <Grid xs={1}>
+              <Paper square className={classes.paper}>
+                <Controls />
+              </Paper>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Paper>Table</Paper>
+        <Grid xs={12} item>
+          <Paper square className={classes.paper}>
+            Table
+          </Paper>
         </Grid>
       </Grid>
     </Box>
