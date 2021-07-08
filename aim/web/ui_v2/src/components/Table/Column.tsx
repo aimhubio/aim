ef255ebe6f71b2bdda749fc463ad4f491cba1395
Ruben Karapetyan <karapetyanrouben@gmail.com>
2021-07-09 01:58:50 +0400

    Added base table components

---
+++ aim/web/ui_v2/src/components/Table/Column.tsx
@@ -0,0 +1,26 @@
+// @ts-nocheck
+
+import React from 'react';
+
+export const Alignment = {
+  LEFT: 'left',
+  CENTER: 'center',
+  RIGHT: 'right',
+};
+
+export const FrozenDirection = {
+  LEFT: 'left',
+  RIGHT: 'right',
+  DEFAULT: true,
+  NONE: false,
+};
+
+/**
+ * Column for BaseTable
+ */
+class Column extends React.Component {}
+
+Column.Alignment = Alignment;
+Column.FrozenDirection = FrozenDirection;
+
+export default Column;
