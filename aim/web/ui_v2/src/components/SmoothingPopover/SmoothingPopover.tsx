bf057ee336d3574c80771e05dd7db3e9c0193463
Arsen Alkamyan <alkamyan97@gmail.com>
2021-07-13 19:19:52 +0400

    added smoothing data algorithms

--- aim/web/ui_v2/src/components/SmoothingPopover/SmoothingPopover.tsx
+++ aim/web/ui_v2/src/components/SmoothingPopover/SmoothingPopover.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Divider,
@@ -8,14 +8,50 @@ import {
   Switch,
 } from '@material-ui/core';
 
-const marks = [
-  { value: 0, label: '0' },
-  { value: 0.25, label: '0.25' },
-  { value: 0.5, label: '0.5' },
-  { value: 0.75, label: '0.75' },
-  { value: 0.99, label: '0.99' },
-];
+const emaProps = {
+  marks: [
+    { value: 0, label: '0' },
+    { value: 0.25, label: '0.25' },
+    { value: 0.5, label: '0.5' },
+    { value: 0.75, label: '0.75' },
+    { value: 0.99, label: '0.99' },
+  ],
+  step: 0.01,
+  min: 0,
+  max: 0.99,
+};
+
+const cmaProps = {
+  marks: [
+    { value: 1, label: '1' },
+    { value: 25, label: '25' },
+    { value: 51, label: '51' },
+    { value: 75, label: '75' },
+    { value: 99, label: '99' },
+  ],
+  step: 1,
+  min: 2,
+  max: 99,
+};
+
 function SmoothingPopover(): React.FunctionComponentElement<React.ReactNode> {
+  const [smoothingAlgorithm, setSmoothingAlgorithm] =
+    React.useState<string>('ema');
+
+  const [factor, setFactor] = useState<number>(emaProps.min);
+
+  const handleAlgorithmChange = React.useCallback((event) => {
+    setSmoothingAlgorithm(event.target.id);
+    const sliderProps = smoothingAlgorithm === 'ema' ? emaProps : cmaProps;
+    setFactor(sliderProps.min);
+  }, []);
+
+  const handleFactorChange = (event: any, val: any) => {
+    if (val !== factor) {
+      setFactor(val);
+    }
+  };
+  const sliderProps = smoothingAlgorithm === 'ema' ? emaProps : cmaProps;
   return (
     <Box>
       <Box p={1}>
@@ -23,21 +59,25 @@ function SmoothingPopover(): React.FunctionComponentElement<React.ReactNode> {
         <Slider
           defaultValue={0}
           getAriaValueText={(val) => `${val}s`}
-          marks={marks}
-          step={0.25}
-          max={0.99}
-          min={0}
+          onChange={handleFactorChange}
+          marks={sliderProps.marks}
+          step={sliderProps.step}
+          max={sliderProps.max}
+          min={sliderProps.min}
           valueLabelDisplay='auto'
         />
       </Box>
       <Divider />
       <MenuList>
-        <MenuItem>Exponantial Moving area</MenuItem>
-        <MenuItem>Central Moving area</MenuItem>
+        <MenuItem id='ema' onClick={handleAlgorithmChange}>
+          Exponential Moving area
+        </MenuItem>
+        <MenuItem id='cma' onClick={handleAlgorithmChange}>
+          Central Moving area
+        </MenuItem>
       </MenuList>
       <Divider />
       <Box p={0.5}>Curve Interpolation method:</Box>
-
       <Box p={1}>
         Select Method
         <Box component='span' marginLeft={1}>
