c35415601eea53eea58a983dfe6f97fe30dc3957
Vardges Hovhannisyan <43929589+VkoHov@users.noreply.github.com>
2021-07-20 14:57:56 +0400

    Add routes dynamic rendering (#523)
    * Add routes dynamic rendering
    
    * tidy up
    
    Co-authored-by: Vardges Hovhannisyan <vardges@aimhub.io>

--- aim/web/ui_v2/src/App.tsx
+++ aim/web/ui_v2/src/App.tsx
@@ -3,9 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Box, makeStyles } from '@material-ui/core';
 
 import SideBar from 'components/SideBar/SideBar';
-import { routes } from 'routes/routes';
 import ProjectWrapper from 'components/ProjectWrapper/ProjectWrapper';
 import Theme from 'components/Theme/Theme';
+import { routes } from 'routes/routes';
 
 const useStyles = makeStyles(({ spacing }) => ({
   main: {
@@ -25,12 +25,14 @@ function App(): React.FunctionComponentElement<React.ReactNode> {
           <Box component='main' bgcolor='grey.200' className={classes.main}>
             <React.Suspense fallback={null}>
               <Switch>
-                <Route path={routes.RUNS.path}>
-                  <routes.RUNS.component />
-                </Route>
-                <Route path={routes.METRICS.path}>
-                  <routes.METRICS.component />
-                </Route>
+                {Object.values(routes).map((route, index) => {
+                  const { component: Component, path } = route;
+                  return (
+                    <Route path={path} key={index}>
+                      <Component />
+                    </Route>
+                  );
+                })}
               </Switch>
             </React.Suspense>
           </Box>
