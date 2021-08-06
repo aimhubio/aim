f5d024dc8c31740a6702e02c5441612b2ee60a0b
mahnerak <mahnerak@gmail.com>
2021-08-06 13:18:19 +0400

    Cython implementations of encodings

--- aim/storage/encoding/encoding_native.pyx
+++ aim/storage/encoding/encoding_native.pyx
@@ -6,6 +6,8 @@ import cython
 from libcpp.vector cimport vector
 from libcpp.pair cimport pair
 
+import struct
+
 # @cython.locals(N=cython.int, i=cython.int, start=cython.int)
 #@cython.returns(bytes)
 # cdef (int, unsigned char[:])[:] split_path(const unsigned char[:] buffer)
@@ -13,31 +15,24 @@ from libcpp.pair cimport pair
 ctypedef long long ll
 
 cdef inline ll q(
-    ll b0,
-    ll b1,
-    ll b2,
-    ll b3,
-    ll b4,
-    ll b5,
-    ll b6,
-    ll b7
+    const unsigned char* b
 ) nogil:
     return (
-        (b0 << 56) |
-        (b1 << 48) |
-        (b2 << 40) |
-        (b3 << 32) |
-        (b4 << 24) |
-        (b5 << 16) |
-        (b6 << 8) |
-        (b7 << 0)
+        ((<ll>b[0]) << 56) |
+        ((<ll>b[1]) << 48) |
+        ((<ll>b[2]) << 40) |
+        ((<ll>b[3]) << 32) |
+        ((<ll>b[4]) << 24) |
+        ((<ll>b[5]) << 16) |
+        ((<ll>b[6]) << 8) |
+        ((<ll>b[7]) << 0)
     )
 
 
 #@cython.wraparound(False)
 #@cython.boundscheck(False)
 @cython.exceptval(check=False)
-cdef vector[pair[ll, ll]] split_path(const unsigned char[:] buffer, ll N) nogil:
+cdef vector[pair[ll, ll]] split_path(const unsigned char* buffer, ll N) nogil:
     # cdef int N = len(buffer)
     cdef vector[pair[ll, ll]] path
     cdef ll start = 0
@@ -60,8 +55,7 @@ cdef vector[pair[ll, ll]] split_path(const unsigned char[:] buffer, ll N) nogil:
             # path.append(str_key)
 
         # int_key, = struct.unpack('>q', buffer[i + 1: i + 9])
-        v = q(buffer[i + 1 + 0], buffer[i + 1 + 1], buffer[i + 1 + 2], buffer[i + 1 + 3],
-              buffer[i + 1 + 4], buffer[i + 1 + 5], buffer[i + 1 + 6], buffer[i + 1 + 7])
+        v = q(buffer + i + 1)
         # v = i + 1
         p.first = v
         p.second = 0
@@ -86,3 +80,39 @@ cpdef decode_path(buffer):
         else:
             path.append(si.first)
     return path
+
+cpdef inline bytes encode_int(long long value):
+    return value.to_bytes(8, "little", True)
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef inline long decode_int(unsigned char* buffer) nogil:
+    return (
+        ((<long long>buffer[0]) <<  0) |
+        ((<long long>buffer[1]) <<  8) |
+        ((<long long>buffer[2]) << 16) |
+        ((<long long>buffer[3]) << 24) |
+        ((<long long>buffer[4]) << 32) |
+        ((<long long>buffer[5]) << 40) |
+        ((<long long>buffer[6]) << 48) |
+        ((<long long>buffer[7]) << 56)
+    )
+
+# Handling `float`
+
+_struct_float = struct.Struct('d')
+
+@cython.exceptval(check=False)
+cpdef inline bytes encode_float(double value):
+    # TODO handle numpy scalars maybe?
+    return _struct_float.pack(value)
+
+cpdef inline double decode_float(unsigned char* bytes) nogil:
+    return (<double*> bytes)[0]
+
+cpdef inline bytes encode_str(str value):
+    return value.encode('utf-8')
+
+cpdef inline str decode_str(bytes buffer):
+    return buffer.decode('utf-8')
