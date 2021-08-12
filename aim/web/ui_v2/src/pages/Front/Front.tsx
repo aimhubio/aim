cee077244984fba12b84d427fdd9bfa2dd55cfb6
Arsen Alkamyan <alkamyan97@gmail.com>
2021-08-12 11:49:25 +0400

    Front page statistics card

--- aim/web/ui_v2/src/pages/Front/Front.tsx
+++ aim/web/ui_v2/src/pages/Front/Front.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import { Box, Grid, Typography } from '@material-ui/core';
+
+import './frontStyle.scss';
 
 function Front(): React.FunctionComponentElement<React.ReactNode> {
-  return <div>Front</div>;
+  return (
+    <section className='Front__container'>
+      <Grid container spacing={1}>
+        <Grid item>
+          <Box>
+            <Typography>Statistics</Typography>
+            <div className='Front__Statistics__card'>
+              <Typography>Experiments</Typography>
+              <Typography>Runs</Typography>
+            </div>
+            <div className='Front__Statistics__card'>
+              <Typography>Experiments</Typography>
+              <Typography>Runs</Typography>
+            </div>
+          </Box>
+        </Grid>
+        <Grid xs item>
+          Activity
+        </Grid>
+      </Grid>
+    </section>
+  );
 }
 export default Front;
