e4759a20adec942a977095a2d389f65368b7c833
Karo <44981312+KaroMourad@users.noreply.github.com>
2021-08-13 15:22:37 +0400

    Fix some styles

--- aim/web/ui_v2/src/pages/Metrics/Metrics.tsx
+++ aim/web/ui_v2/src/pages/Metrics/Metrics.tsx
@@ -12,43 +12,41 @@ import ChartPanel from 'components/ChartPanel/ChartPanel';
 import { IMetricProps } from 'types/pages/metrics/Metrics';
 import { ChartTypeEnum } from 'utils/d3';
 
-import useStyles from './metricsStyle';
+import './Metrics.scss';
 
 function Metrics(
   props: IMetricProps,
 ): React.FunctionComponentElement<React.ReactNode> {
-  const classes = useStyles();
-
   return (
-    <div ref={props.wrapperElemRef}>
+    <div ref={props.wrapperElemRef} className='Metrics__container'>
       <Box
         bgcolor='grey.200'
         component='section'
         height='100vh'
         overflow='hidden'
-        className={classes.section}
+        className='Metrics'
       >
         <Grid
           container
           direction='column'
           justifyContent='center'
-          className={classes.fullHeight}
+          className='Metrics__fullHeight'
           spacing={1}
         >
           <Grid item>
-            <Paper className={classes.paper}>
+            <Paper className='Metrics__paper'>
               <AppBar onBookmarkCreate={props.onBookmarkCreate} />
             </Paper>
           </Grid>
           <Grid item>
             <Grid container alignItems='stretch' spacing={1}>
               <Grid xs item>
-                <Paper className={classes.paper}>
+                <Paper className='Metrics__paper'>
                   <SelectForm />
                 </Paper>
               </Grid>
               <Grid item>
-                <Paper className={classes.paper}>
+                <Paper className='Metrics__paper'>
                   <Box height='100%' display='flex'>
                     <Grouping
                       groupingData={props.groupingData}
@@ -68,7 +66,7 @@ function Metrics(
           </Grid>
           <Grid
             ref={props.chartElemRef}
-            className={classes.chartContainer}
+            className='Metrics__chart__container'
             item
           >
             {!!props.lineChartData?.[0]?.length ? (
@@ -112,7 +110,7 @@ function Metrics(
               justifyContent='center'
               display='flex'
               alignItems='center'
-              className={classes.resize}
+              className='Metrics__resize'
               height='6px'
             >
               <MoreHorizIcon />
@@ -122,9 +120,9 @@ function Metrics(
             item
             xs
             ref={props.tableElemRef}
-            className={classes.tableContainer}
+            className='Metrics__table__container'
           >
-            <Paper className={classes.paper}>
+            <Paper className='Metrics__paper'>
               {props.tableData?.length > 0 ? (
                 <Table
                   ref={props.tableRef}
