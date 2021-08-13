00f13fb6e453d483b84322780c75e93560f9e16c
mahnerak <mahnerak@gmail.com>
2021-08-13 19:17:15 +0400

    Simplified hash for numbers

--- aim/storage/hashing/c_hash.pyx
+++ aim/storage/hashing/c_hash.pyx
@@ -2,5 +2,5 @@
 
 from aim.storage.hashing cimport c_hash
 
-cpdef Py_hash_t det_hash(v):
-    return c_hash.le_Py_HashDouble(v, v)
+cpdef Py_hash_t det_hash(double v):
+    return c_hash.le_Py_HashDouble(v)
