e06ca1bcbf06c2cb8bbf9a658fe6dcbcb1726db3
Ruben Karapetyan <karapetyanrouben@gmail.com>
2021-08-17 18:58:18 +0400

    Add handler for fetching and setting project metrics and params (#597)

--- aim/web/ui_v2/src/services/models/projects/projectsModel.ts
+++ aim/web/ui_v2/src/services/models/projects/projectsModel.ts
@@ -1,6 +1,9 @@
+import { omit } from 'lodash-es';
+
 import projectsService from 'services/api/projects/projectsService';
 import {
   IProject,
+  IProjectParamsMetrics,
   IProjectsModelState,
 } from 'types/services/models/projects/projectsModel';
 import createModel from 'services/models/model';
@@ -23,9 +26,43 @@ function getProjectsData() {
   };
 }
 
+function getParamsAndMetrics() {
+  const { call, abort } = projectsService.getParamsAndMetrics();
+
+  return {
+    call: () =>
+      call().then((data: IProjectParamsMetrics) => {
+        model.setState({
+          metrics: data.metrics,
+          params: removeExampleTypes(
+            omit(data.params, 'experiment_name', 'status'),
+          ),
+        });
+      }),
+    abort,
+  };
+}
+
+function removeExampleTypes(params: IProjectParamsMetrics['params']) {
+  for (let paramKey in params) {
+    const param = params[paramKey];
+    if (typeof param === 'object') {
+      if (param.hasOwnProperty('__example_type__')) {
+        params[paramKey] = true;
+      } else {
+        removeExampleTypes(param);
+      }
+    } else {
+      params[paramKey] = true;
+    }
+  }
+  return params;
+}
+
 const projectsModel = {
   ...model,
   getProjectsData,
+  getParamsAndMetrics,
 };
 
 export default projectsModel;
