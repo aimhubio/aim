373ec5e0f79b122da473fbc33cec008ec885c6a1
rubenaprikyan <rubenaprikyan@gmail.com>
2021-09-02 03:07:40 +0400

    change px to rem

--- aim/web/ui_v2/src/components/Button/Button.tsx
+++ aim/web/ui_v2/src/components/Button/Button.tsx
@@ -5,27 +5,27 @@ import MUButton from '@material-ui/core/Button';
 import './Button.scss';
 
 const sizes = {
-  small: '28px',
-  medium: '32px',
-  large: '36px',
+  small: '1.75rem',
+  medium: '2rem',
+  large: '2.25rem',
 };
 
 const fontSizes = {
-  small: '12px',
-  medium: '14px',
-  large: '14px',
+  small: '0.75rem',
+  medium: '0.875rem',
+  large: '0.875rem',
 };
 
 function Button(
   props: IButtonProps,
 ): React.FunctionComponentElement<React.ReactNode> {
   const styleOverrides = {
-    borderRadius: '6px',
-    padding: `9px ${props.withOnlyIcon ? '9px' : '20px'}`,
-    marginBottom: '5px',
+    borderRadius: '0.375rem',
+    padding: `0.5rem ${props.withOnlyIcon ? '0.5rem' : '1.25rem'}`,
+    marginBottom: '0.3125rem',
     fontSize: fontSizes[props.size || 'medium'],
     height: sizes[props.size || 'medium'],
-    minWidth: props.withOnlyIcon ? '36px' : '100px',
+    minWidth: props.withOnlyIcon ? '2.25rem' : '6.25rem',
   };
 
   return (
