a6c442b838bc72be0fb222d6dae4b317f3a6e386
Karen Hambardzumyan <mahnerak@gmail.com>
2021-09-21 22:18:23 +0400

    Storage Documentation for Encodings & Hashing (#757)
    * Documentation for Encodings
    
    * Documentation for Hashing
    
    * Avoid looping over a temporary return value
    
    * Disable warnings for exported symbol

--- aim/storage/encoding/encoding.py
+++ aim/storage/encoding/encoding.py
@@ -1,81 +1,122 @@
-from aim.storage.types import AimObjectKey, AimObjectPath
-import struct
+"""Encodings for primitive values for AimObject-compatible data.
 
-from aim.storage.encoding.encoding_native import *  # noqa
+This includes None, booleans, integer and floating-point numbers, strings,
+byte-array blobs and control flags for array and dict-like objects.
+
+The encodings are implemented `(key, value)` design in mind, so using them to
+stream and store in chunks is easy to manage.
+"""
+
+from aim.storage.encoding.encoding_native import PATH_SENTINEL_CODE
+from aim.storage.encoding.encoding_native import (
+    encode_int64,
+    encode_double,
+    encode_utf_8_str,
+    encode_int64_big_endian,
+)
+from aim.storage.encoding.encoding_native import (
+    decode_int64,
+    decode_double,
+    decode_utf_8_str,
+)
+from aim.storage.encoding.encoding_native import decode_path  # noqa F401
 from aim.storage.utils import ArrayFlag, ObjectFlag
+from aim.storage.types import AimObjectKey, AimObjectPath
 
 from typing import Union, Any
 
-
+# The choice of `PATH_SENTINEL` is not arbitrary.
+# This sentinel 1) consists of a single byte
+#               2) does not appear in any utf-8 encoded string
+#               3) comes right before max byte 0xff which make a room for
+#                  many tricks
 _PATH_SENTINEL = b'\xfe'
+assert _PATH_SENTINEL[0] == PATH_SENTINEL_CODE
 
+# We define a mapping for primitive types into a single bit
+# The types are going to be stored in a single byte, so 7 more bits are reserved
 _NONE = 0
 _BOOL = 1
-_INT = 2  # Stored as 64-bit Long Long
-_FLOAT = 3  # Stored as 64-bit Double
+_INT = 2
+_FLOAT = 3
 _STRING = 4
 _BYTES = 5
 _ARRAY = 6
 _OBJECT = 7
 
-# ARRAY_FLAG = object()
-# OBJECT_FLAG = object()
-
-# Handling `None`
-
-# @overload
-
-# cdef inline bytes encode_none():
-#     return b""
-
 
 def encode(value: Any) -> bytes:
+    """Automatically detect and encode the value into a buffer.
+    This function is to encode only primitive objects.
+
+    Type id of the value is encoded with the content as well in order to enable
+    automatic decoding.
+    The first byte of the buffer encodes type of the encoded value and the rest
+    are for encoded content.
+    """
+
     if value is None:
+        # No need to encode a content for None-values
         type_id = _NONE
-        encoding = b""
+        encoding = b''
     elif value is bool:
+        # Booleans are encoded in a single byte
+        # 0 and 1 for False and True respectively
         type_id = _BOOL
         encoding = b'\x01' if value else b'\x00'
     elif isinstance(value, int):
+        # We encode integers in signed 64-bit
         type_id = _INT
-        encoding = encode_int(value)
+        encoding = encode_int64(value)
     elif isinstance(value, float):
+        # We encode floats in double-precision
         type_id = _FLOAT
-        encoding = encode_float(value)
+        encoding = encode_double(value)
     elif isinstance(value, str):
+        # Strings are stored in utf-8
         type_id = _STRING
-        encoding = encode_str(value)
+        encoding = encode_utf_8_str(value)
     elif isinstance(value, bytes):
+        # Byte arrays / BLOBS can be used to encode arbitrary binary content
         type_id = _BYTES
         encoding = value
     elif value is ArrayFlag:
+        # Array flag is used to denote non-finised object, expecting
+        # encodings for its elements
         type_id = _ARRAY
-        encoding = b""
+        encoding = b''
     elif value is ObjectFlag:
+        # Object flag is used to denote non-finised object, expecting
+        # encodings for its elements
         type_id = _OBJECT
-        encoding = b""
+        encoding = b''
     else:
-        type_id = _NONE
-        encoding = b""
+        raise NotImplementedError
 
+    # Finally, we prepend the content `encoding` with a single byte which
+    # encodes the type of the value
     type_byte = bytes([type_id])
     return type_byte + encoding
 
 
 def decode(buffer: bytes):
+    """Automatically detect and decode the value from a buffer."""
+
+    # First, we extract type_id and the content buffer
     type_id = buffer[0]
     buffer = buffer[1:]
 
+    # We check for type_id and decode the content for the corresponding type
     if type_id == _NONE:
         return None
     elif type_id == _BOOL:
         return buffer[0] != 0
     elif type_id == _INT:
-        return decode_int(buffer)
+        return decode_int64(buffer)
     elif type_id == _FLOAT:
-        return decode_float(buffer)
+        return decode_double(buffer)
     elif type_id == _STRING:
-        return decode_str(buffer)
+        return decode_utf_8_str(buffer)
     elif type_id == _BYTES:
         return buffer
     elif type_id == _ARRAY:
@@ -86,23 +127,49 @@ def decode(buffer: bytes):
         return None
 
 
-_struct_q_be = struct.Struct('>q')
-
-
 def encode_key(key: Union[int, str]):
+    """Encode the value as key in the path.
+
+    This is different from what the general `encode()` will result.
+    Here we omit type_id for strings to avoid redundancies, and add an
+    additional path sentinel for numeric (integer) ones.
+    Integers are encoded in big-endian to make the encoded keys to preserve
+    the relative order when comparing as byte array buffers in lexicographic
+    order.
+    For example, `1000 > 249` but having them encoded in little-endian may have
+    lead their encoded versions not to maintain the relative order:
+    `b'\\xe8\\x03\\x00\\x00\\x00\\x00\\x00\\x00' < b'\\xf9\\x00\\x00\\x00\\x00\\x00\\x00\\x00'`
+    However, in big-endiang encoding:
+    `b'\\x00\\x00\\x00\\x00\\x00\\x00\\x03\\xe8' > b'\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\xf9'`
+    """
     if isinstance(key, str):
-        encoded = encode_str(key)
-        # if _PATH_SENTINEL in encoded:
-        #     raise ValueError(f"The keys may not contain `{_PATH_SENTINEL}`. "
-        #                      f"The provided key was: {key}")
+        encoded = encode_utf_8_str(key)
         return encoded
     elif isinstance(key, int):
-        return _PATH_SENTINEL + _struct_q_be.pack(key)
+        return _PATH_SENTINEL + encode_int64_big_endian(key)
     else:
         raise ValueError(f'Value {key} of type {type(key)} is not supported')
 
 
 def encode_path(path: Union[AimObjectKey, AimObjectPath] = ()):
+    """Encode the path into a buffer.
+    The encoding of path is designed to effectively use prefix-trees that may
+    be used by storage engines.
+
+    The path is encoded as concatenation of encoded keys, separated with
+    so-called `PATH_SENTINEL`.
+    Keys can be either strings or integers:
+      *  Strings keys are encoded in utf-8. Any non-empty string is allowed as
+         a path key, containing of any characters. There is no risk of
+         ambiguous encodings because the `PATH_SENTINEL` is chosen such it can
+         never be occurred in correctly encoded utf-8 string. Such key may
+         include even `PATH_SENTINEL` as it is encoded using other bytes.
+      *  Integers are encoded in signed 64-bit integers, and a `PATH_SENTINEL`
+         is prepended.
+    If the key encoding starts with the `PATH_SENTINEL` it can only encode an
+    integer, as 1) no utf-8 encoded string may start with `PATH_SENTINEL`
+                2) the empty strings are not allowed
+    """
     if isinstance(path, (int, str)):
         path = (path,)
     return b''.join([encode_key(key) + _PATH_SENTINEL for key in path])
--- aim/storage/encoding/encoding_native.pyx
+++ aim/storage/encoding/encoding_native.pyx
@@ -4,82 +4,158 @@
 # cython: cdivision=True
 # cython: nonecheck=False
 
-import cython
-
 from aim.storage.encoding.encoding_native cimport *
 
-cdef inline ll q(
-    const unsigned char* b
-) nogil:
+
+cpdef inline bytes encode_int64_big_endian(int64 value):
+    """Decode an 64-bit signed integer from a buffer
+    encoded in big-endian
+    """
+    return bytes([
+        (value >> 56) & 0xff,
+        (value >> 48) & 0xff,
+        (value >> 40) & 0xff,
+        (value >> 32) & 0xff,
+        (value >> 24) & 0xff,
+        (value >> 16) & 0xff,
+        (value >> 8) & 0xff,
+        (value >> 0) & 0xff
+    ])
+
+
+cpdef inline bytes encode_int64(int64 value):
+    """Encode an 64-bit signed integer into a buffer"""
+    return (<unsigned char*>(&value))[:8]
+
+
+cpdef inline int64 decode_int64_big_endian(const unsigned char* buf) nogil:
+    """Decode an 64-bit signed integer from a buffer
+    encoded in big-endian
+    """
     return (
-        ((<ll>b[0]) << 56) |
-        ((<ll>b[1]) << 48) |
-        ((<ll>b[2]) << 40) |
-        ((<ll>b[3]) << 32) |
-        ((<ll>b[4]) << 24) |
-        ((<ll>b[5]) << 16) |
-        ((<ll>b[6]) << 8) |
-        ((<ll>b[7]) << 0)
+        ((<int64>buf[0]) << 56) |
+        ((<int64>buf[1]) << 48) |
+        ((<int64>buf[2]) << 40) |
+        ((<int64>buf[3]) << 32) |
+        ((<int64>buf[4]) << 24) |
+        ((<int64>buf[5]) << 16) |
+        ((<int64>buf[6]) << 8) |
+        ((<int64>buf[7]) << 0)
     )
 
 
+cpdef inline int64 decode_int64(const unsigned char* buffer) nogil:
+    """Decode an 64-bit signed integer from a buffer"""
+    return (<int64*>buffer)[0]
 
-cdef inline vector[pair[ll, ll]] split_path(const unsigned char* buffer, ll N) nogil:
-    cdef vector[pair[ll, ll]] path
-    cdef ll start = 0
-    cdef ll i = 0
-    cdef ll v
-    cdef pair[ll, ll] p
-    while i < N:
-        if buffer[i] != 254:
-            i += 1
+
+cpdef inline vector[pair[int64, int64]] split_path(
+    const unsigned char* buffer,
+    int64 length
+) nogil:
+    """Split the encoded path in order to decode into string and integer keys.
+    The main goal of this functions is to quickly split the encoded path into
+    smaller chunks in advance so the keys may be easily decoded.
+
+    Args:
+        buffer: binary-encoded path, e.g. for the path `('foo', 19, 'α-γ')`
+                the encoded path is:
+                `b'foo\\xfe\\xfe\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x13\\xfe\\xce\\xb1-\\xce\\xb3\\xfe'`
+        length: the length of buffer
+
+    Returns:
+        An array of pairs denoting a key; If the key is integer then the value
+        is encoded in `(value, 0)` otherwise the pair `(start, end)` denotes
+        starting and ending offsets of key occurring in the provided buffer.
+        For example, the result for the sample buffer will be:
+          `[(0, 3),   # the segment from 0...3
+            (19, 0),  # integer with value 19
+            (14, 17)  # the segment 14...17
+           ]`
+    """
+    cdef vector[pair[int64, int64]] path
+    cdef int64 start = 0
+    cdef int64 cursor = 0
+    # We use `tmp_pair` to create a pair before pushing it into vector of chunks
+    cdef pair[int64, int64] tmp_pair
+
+    # Here we run over the buffer to detect the boundaries of key encodings.
+    # The strategy is as follows:
+    #  *  As we detect a new segment, we try to extend the boundary to right
+    #     as long as it does not reach the path sentinel / separator symbol.
+    #  *  When the cursor has reached a path separator, we store the resulting
+    #     segment and forward the cursor right.
+    #  *  If we detect a zero-length segment => we reached double-sentinel
+    #     notation of integer keys in the path. In that case we decode an
+    #     integer, store it in `(value, 0)` pair and forward the cursor right.
+    while cursor < length:
+        if buffer[cursor] != PATH_SENTINEL_CODE:
+            # The current segment is `(start, cursor)` so extend it +1 to right
+            cursor += 1
             continue
 
-        if start < i:
-            p.first = start
-            p.second = i
-            path.push_back(p)
-            start = i + 1
-            i += 1
-            continue
+        if start < cursor:
+            # The segment encodes a string key.
+            # Store the segment boundaries
+            tmp_pair.first = start
+            tmp_pair.second = cursor
+            path.push_back(tmp_pair)
+        else:
+            # The segment encodes a integer key
+            # Store the integer in `(value, 0)` pair
+            tmp_pair.first = decode_int64_big_endian(buffer + cursor + 1)
+            tmp_pair.second = 0
+            path.push_back(tmp_pair)
+            # Move forward 1 byte over the integer key start sentinel,
+            # then another 8 bytes for the 64-bit integer key
+            cursor += 1 + 8
 
-        v = q(buffer + i + 1)
-
-        p.first = v
-        p.second = 0
-        path.push_back(p)
-        i += 10
-        start = i
+        # Making a new segment starting right after the path sentinel
+        cursor += 1
+        start = cursor
 
     return path
-
 
 
 cpdef inline decode_path(bytes buffer):
+    """Decode the binary-encoded path from the given buffer.
+
+    Args:
+        buffer: binary-encoded path, e.g.
+                `b'foo\\xfe\\xfe\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x13\\xfe\\xce\\xb1-\\xce\\xb3\\xfe'`
+
+    Returns:
+        The decoded path as a list of keys of type string or integer.
+        For example, the corresponding path for the sample buffer will be:
+        `['foo', 19, 'α-γ']`
+    """
     path = []
-    cdef vector[pair[ll, ll]] p = split_path(buffer, len(buffer))
-    for si in p:
-        if si.second > 0:
-            path.append(buffer[si.first:si.second].decode('utf-8'))
+    cdef vector[pair[int64, int64]] segments = split_path(buffer, len(buffer))
+    for item in segments:
+        if item.second == 0:
+            # In this case the pair only encodes a single integer
+            path.append(item.first)
         else:
-            path.append(si.first)
+            # The pair denotes start and end offsets of the string key
+            # in the buffer encoded in utf-8
+            path.append(buffer[item.first:item.second].decode('utf-8'))
     return path
 
-cpdef inline bytes encode_int(long long value):
+
+cpdef inline bytes encode_double(double value):
+    """Encode an double precision floating-point number into a buffer"""
     return (<unsigned char*>(&value))[:8]
 
-cpdef inline long decode_int(const unsigned char* buffer) nogil:
-    return (<long long*>buffer)[0]
 
-
-cpdef inline bytes encode_float(double value):
-    return (<unsigned char*>(&value))[:8]
-
-cpdef inline double decode_float(const unsigned char* bytes) nogil:
+cpdef inline double decode_double(const unsigned char* bytes) nogil:
+    """Decode an double precision floating-point number from a buffer"""
     return (<double*> bytes)[0]
 
-cpdef inline bytes encode_str(str value):
+
+cpdef inline bytes encode_utf_8_str(str value):
+    """Encode an string into a buffer in utf-8"""
     return value.encode('utf-8')
 
-cpdef inline str decode_str(bytes buffer):
+cpdef inline str decode_utf_8_str(bytes buffer):
+    """Decode an string from a buffer encoded in utf-8"""
     return buffer.decode('utf-8')
