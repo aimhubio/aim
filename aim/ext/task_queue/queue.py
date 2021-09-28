a181fa33506c7329db88408fc3c54ee496ef6493
alberttorosyan <32957250+alberttorosyan@users.noreply.github.com>
2021-09-28 17:41:20 +0400

    Move track into separate thread
    * Added task queue extension.
    * Integrated into Run.track()
    * Warn user when track task backlog is 80% full
    * Deepcopy tracked valu

---
+++ aim/ext/task_queue/queue.py
@@ -0,0 +1,67 @@
+import threading
+import queue
+import atexit
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class TaskQueue(object):
+    def __init__(self, name, num_workers=1, max_backlog=0):
+        self.name = name
+        self.max_backlog = max_backlog
+        self.num_workers = num_workers
+
+        self._queue = queue.Queue(maxsize=max_backlog)
+
+        self._threads = []
+        self._shutdown = False
+        self._stopped = False
+        atexit.register(self.stop_workers)
+
+        for thread_num in range(self.num_workers):
+            thread = threading.Thread(target=self.worker)
+            thread.daemon = True
+            self._threads.append(thread)
+            thread.start()
+
+    def register_task(self, task_func, *args, **kwargs):
+        warn_queue_full = False
+        if self._stopped:
+            logger.debug('Cannot register task: task queue is stopped.')
+        else:
+            backlog_size = self._queue.qsize()
+            if backlog_size > self.max_backlog * 0.8:  # queue is 80% full
+                warn_queue_full = True
+            self._queue.put((task_func, args, kwargs))
+        return warn_queue_full
+
+    def worker(self):
+        while True:
+            if self._shutdown:
+                logger.debug(f'Shutting down worker thread {threading.get_ident()}.')
+                break
+            task_f, args, kwargs = self._queue.get()
+            task_f(*args, **kwargs)
+            self._queue.task_done()
+
+    def wait_for_finish(self):
+        if self._stopped:
+            return
+        self._queue.join()
+
+    def stop_workers(self):
+        if self._stopped:
+            return
+
+        self._stopped = True
+        pending_task_count = self._queue.qsize()
+        if pending_task_count:
+            logger.warning(f'Processing {pending_task_count} pending tasks in queue \'{self.name}\'... '
+                           f'Please do not kill the process.')
+            self._queue.join()
+        self._shutdown = True
+        logger.debug('No pending tasks left.')
+
+    def __del__(self):
+        self.stop_workers()
--- aim/sdk/repo.py
+++ aim/sdk/repo.py
@@ -8,6 +8,8 @@ from typing import Dict, Iterator, NamedTuple, Optional
 from weakref import WeakValueDictionary
 
 from aim.ext.sshfs.utils import mount_remote_repo, unmount_remote_repo
+from aim.ext.task_queue.queue import TaskQueue
+
 from aim.sdk.configs import AIM_REPO_NAME
 from aim.sdk.run import Run
 from aim.sdk.utils import search_aim_repo, clean_repo_path
@@ -50,6 +52,8 @@ class Repo:
     """
     _pool = WeakValueDictionary()  # TODO: take read only into account
     _default_path = None  # for unit-tests
+
+    tracking_queue = TaskQueue('metric_tracking', max_backlog=10_000_000)  # single thread task queue for Run.track
 
     def __init__(self, path: str, *, read_only: bool = None, init: bool = False):
         if read_only is not None:
