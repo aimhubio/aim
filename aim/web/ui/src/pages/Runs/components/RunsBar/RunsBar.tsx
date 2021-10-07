fe335434cfb0ba02072ed10c24ae1cc7dd9ebd3e
rubenaprikyan <rubenaprikyan@gmail.com>
2021-10-07 14:43:49 +0400

    Change full path usage of import statement

--- aim/web/ui/src/pages/Runs/components/RunsBar/RunsBar.tsx
+++ aim/web/ui/src/pages/Runs/components/RunsBar/RunsBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppBar from 'components/AppBar/AppBar';
-import LiveUpdateSettings from '../../../../components/LiveUpdateSettings/LiveUpdateSettings';
+import LiveUpdateSettings from 'components/LiveUpdateSettings/LiveUpdateSettings';
 
 function RunsBar(props: {
   enabled: boolean;
