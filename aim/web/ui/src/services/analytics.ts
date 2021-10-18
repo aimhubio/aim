56fefbaa6d2664a54bfd21295c41a48a74fe90df
Gor Arakelyan <arakelyangor10@gmail.com>
2021-10-18 22:24:35 +0400

    Update segment integration script and key (#865)

--- aim/web/ui/src/services/analytics.ts
+++ aim/web/ui/src/services/analytics.ts
@@ -5,30 +5,31 @@ export function isDev() {
   return process.env.NODE_ENV === 'development';
 }
 
+let initialized = false;
+
 const enabled = () => {
   return !isDev() && window.analytics !== false; //!isDev() && cookies.getCookie(configs.USER_ANALYTICS_COOKIE_NAME) == 1;
 };
 
 const init = () => {
-  if (!enabled()) {
-    return;
-  }
-
+  if (initialized) return;
   if (
     window.location.hostname.indexOf('aimstack.io') !== -1 &&
     (window.location.hostname.indexOf('demo') !== -1 ||
       window.location.hostname.indexOf('play') !== -1)
   ) {
+    window.analytics._writeKey = SEGMENT_DEMO_WRITE_KEY;
     window.analytics.load(SEGMENT_DEMO_WRITE_KEY);
   } else {
+    window.analytics._writeKey = SEGMENT_WRITE_KEY;
     window.analytics.load(SEGMENT_WRITE_KEY);
   }
+  initialized = true;
 };
 
 const pageView = (pageName, pageCat = null) => {
-  if (!enabled()) {
-    return;
-  }
+  if (!enabled()) return;
+  init();
   window.analytics.page(pageCat, pageName, {
     path: window.location.pathname,
     url: window.location.hostname,
@@ -39,9 +40,8 @@ const pageView = (pageName, pageCat = null) => {
 };
 
 const trackEvent = (eventName: string, properties = {}) => {
-  if (!enabled()) {
-    return;
-  }
+  if (!enabled()) return;
+  init();
   window.analytics.track(eventName, properties, {
     path: window.location.pathname,
     url: window.location.hostname,
