1bcea02a322895036a3280fc74dbabd7d7292fbf
alberttorosyan <32957250+alberttorosyan@users.noreply.github.com>
2021-10-20 22:08:22 +0400

    Run finalization fixes
    * disable separate thread for tracking by default
    * use structured db as a context manager for setting run.finalized_at
    * improved structured db as a context manager

--- tests/storage/test_structured_db.py
+++ tests/storage/test_structured_db.py
@@ -1,7 +1,7 @@
-from tests.base import TestBase
+from tests.base import PrefilledDataTestBase
 
 
-class TestStructuredDatabase(TestBase):
+class TestStructuredDatabase(PrefilledDataTestBase):
     def test_entity_chaining_syntax(self):
         run = self.repo.structured_db.find_run('missing_run_hash')
         self.assertFalse(run.experiment)
@@ -19,3 +19,13 @@ class TestStructuredDatabase(TestBase):
             for run in db.runs():
                 self.assertEqual('my experiment', run.experiment.name)
                 self.assertEqual(10, len(run.experiment.runs))
+
+    def test_context_manager_nesting(self):
+        with self.repo.structured_db as db1:
+            with self.repo.structured_db as db2:
+                db1.create_experiment('exp 1')
+                db2.create_experiment('exp 2')
+
+        experiment_names = set((exp.name for exp in self.repo.structured_db.experiments()))
+        expected_names = set(('exp 1', 'exp 2'))
+        self.assertTrue(experiment_names.issuperset(expected_names))
