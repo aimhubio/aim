f5bf2b851ba2d88b89653a301d461242cafe259c
Arsen Alkamyan <44676488+arsengit@users.noreply.github.com>
2021-10-20 21:59:21 +0400

    Apply formatting on system metrics names  (#862)
    * created mock data for system metrics
    
    * tuned system metric options in SelectForm component
    
    * implemented grouping by system metrics in AlignmentPopover

---
+++ aim/web/ui/src/types/utils/formatSystemMetricName.d.ts
@@ -0,0 +1,10 @@
+export type systemMetricsDictType = {
+  __system__cpu: string;
+  __system__p_memory_percent: string;
+  __system__memory_percent: string;
+  __system__disk_percent: string;
+  __system__gpu: string;
+  __system__gpu_memory_percent: string;
+  __system__gpu_power_watts: string;
+  __system__gpu_temp: string;
+};
---
+++ aim/web/ui/src/utils/formatSystemMetricName.ts
@@ -0,0 +1,6 @@
+import { systemMetricsDict } from 'config/systemMetrics/systemMetrics';
+import { systemMetricsDictType } from 'types/utils/formatSystemMetricName';
+
+export function formatSystemMetricName(metric: string): string {
+  return systemMetricsDict[metric as keyof systemMetricsDictType] || metric;
+}
