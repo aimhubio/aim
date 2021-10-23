9c98e2da1effaf41411b357ecd85a2d705cd70e4
Arsen Alkamyan <alkamyan97@gmail.com>
2021-10-23 19:58:22 +0400

    Tuned Control popover styles and implemented Text component

--- aim/web/ui/src/components/TooltipContentPopover/TooltipContentPopover.tsx
+++ aim/web/ui/src/components/TooltipContentPopover/TooltipContentPopover.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Checkbox, TextField, Typography } from '@material-ui/core';
+import { Checkbox, Divider, TextField, Typography } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import {
   CheckBox as CheckBoxIcon,
   CheckBoxOutlineBlank,
 } from '@material-ui/icons';
 
-import { ToggleButton, Badge } from 'components/kit';
+import { ToggleButton, Badge, Text } from 'components/kit';
 
 import { ITooltipContentPopoverProps } from 'types/components/TooltipContentPopover/TooltipContentPopover';
 import { IGroupingSelectOption } from 'types/services/models/metrics/metricsAppModel';
@@ -61,80 +61,92 @@ function TooltipContentPopover({
   }, [selectOptions]);
 
   return (
-    <div className='TooltipContentPopover__container'>
-      <div className='TooltipContentPopover__params'>
-        <div>
-          <Autocomplete
-            id='select-params'
-            size='small'
-            multiple
-            disableCloseOnSelect
-            options={
-              inputValue.trim() !== ''
-                ? paramsOptions
-                    .slice()
-                    .sort(
-                      (a, b) =>
-                        a.label.indexOf(inputValue) -
-                        b.label.indexOf(inputValue),
-                    )
-                : paramsOptions
-            }
-            value={values}
-            onChange={onSelectedParamsChange}
-            onInputChange={(e, value) => setInputValue(value)}
-            groupBy={(option) => option.group}
-            getOptionLabel={(option) => option.label}
-            getOptionSelected={(option, value) => option.value === value.value}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                variant='outlined'
-                label='Select Params'
-                placeholder='Select'
+    <div className='TooltipContentPopover'>
+      <div className='TooltipContentPopover__section'>
+        <Text
+          component='h4'
+          tint={50}
+          className='TooltipContentPopover__subtitle'
+        >
+          Parameters
+        </Text>
+        <Autocomplete
+          id='select-params'
+          size='small'
+          multiple
+          disableCloseOnSelect
+          options={
+            inputValue.trim() !== ''
+              ? paramsOptions
+                  .slice()
+                  .sort(
+                    (a, b) =>
+                      a.label.indexOf(inputValue) - b.label.indexOf(inputValue),
+                  )
+              : paramsOptions
+          }
+          value={values}
+          onChange={onSelectedParamsChange}
+          onInputChange={(e, value) => setInputValue(value)}
+          groupBy={(option) => option.group}
+          getOptionLabel={(option) => option.label}
+          getOptionSelected={(option, value) => option.value === value.value}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant='outlined'
+              label='Select Params'
+              placeholder='Select'
+            />
+          )}
+          renderOption={(option, { selected }) => (
+            <React.Fragment>
+              <Checkbox
+                color='primary'
+                icon={<CheckBoxOutlineBlank />}
+                checkedIcon={<CheckBoxIcon />}
+                style={{ marginRight: 4 }}
+                checked={selected}
               />
-            )}
-            renderOption={(option, { selected }) => (
-              <React.Fragment>
-                <Checkbox
-                  color='primary'
-                  icon={<CheckBoxOutlineBlank />}
-                  checkedIcon={<CheckBoxIcon />}
-                  style={{ marginRight: 4 }}
-                  checked={selected}
+              <Typography noWrap={true} title={option.label}>
+                {option.label}
+              </Typography>
+            </React.Fragment>
+          )}
+          renderTags={(value, getTagProps) => (
+            <div style={{ maxHeight: 110, overflow: 'auto' }}>
+              {value.map((selected, i) => (
+                <Badge
+                  key={i}
+                  {...getTagProps({ index: i })}
+                  label={selected.label}
+                  size='small'
+                  className='Select__Chip'
                 />
-                <Typography noWrap={true} title={option.label}>
-                  {option.label}
-                </Typography>
-              </React.Fragment>
-            )}
-            renderTags={(value, getTagProps) => (
-              <div style={{ maxHeight: 110, overflow: 'auto' }}>
-                {value.map((selected, i) => (
-                  <Badge
-                    key={i}
-                    {...getTagProps({ index: i })}
-                    label={selected.label}
-                    size='small'
-                    className='Select__Chip'
-                  />
-                ))}
-              </div>
-            )}
-          />
-        </div>
-        <div className='TooltipContentPopover__toggle'>
-          <ToggleButton
-            title='Display tooltip'
-            id='display'
-            value={displayTooltip ? 'Show' : 'Hide'}
-            leftLabel='Hide'
-            rightLabel='Show'
-            leftValue={'Hide'}
-            rightValue={'Show'}
-            onChange={onDisplayTooltipChange}
-          />
-        </div>
+              ))}
+            </div>
+          )}
+        />
+      </div>
+      <Divider className='TooltipContentPopover__Divider' />
+      <div className='TooltipContentPopover__section'>
+        <Text
+          component='h4'
+          tint={50}
+          className='TooltipContentPopover__subtitle'
+        >
+          Toggle Chart Tooltip Visibility
+        </Text>
+        <ToggleButton
+          title='Select Mode'
+          id='display'
+          value={displayTooltip ? 'Show' : 'Hide'}
+          leftLabel='Hide'
+          rightLabel='Show'
+          leftValue={'Hide'}
+          rightValue={'Show'}
+          onChange={onDisplayTooltipChange}
+        />
       </div>
     </div>
   );
