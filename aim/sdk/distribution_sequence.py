155442ab2bf0cce55d32dad244f9d1fe178184e7
mihran113 <vanyanmihran@gmail.com>
2021-11-25 18:04:50 +0400

    Add ability to track distributions:  - Add `Distribution`: `CustomObject` sub-class  - Add `Distributions`: `Sequence` sub-class  - Add utility functions for distributions and allow querying for distributions  - Add endpoints and utils for visualization of distributions on single run page view  - Some refactoring along the way

---
+++ aim/sdk/distribution_sequence.py
@@ -0,0 +1,37 @@
+from typing import Union, Tuple
+
+from aim.sdk.sequence import Sequence
+from aim.sdk.objects.distribution import Distribution
+
+
+class Distributions(Sequence):
+    """Class representing series of Image objects or Image lists."""
+
+    @classmethod
+    def allowed_dtypes(cls) -> Tuple[str, ...]:
+        return (Distribution.get_typename(),)
+
+    @classmethod
+    def sequence_name(cls) -> str:
+        return 'distributions'
+
+    def first_step(self):
+        """Get sequence tracked first step.
+
+        Required to implement ranged and sliced data fetching.
+        """
+        return self._meta_tree['first_step']
+
+    def last_step(self):
+        """Get sequence tracked last step.
+
+        Required to implement ranged and sliced data fetching.
+        """
+        return self._meta_tree['last_step']
+
+    def record_length(self):
+        """Get tracked records longest list length or `None` if Image objects are tracked.
+
+        Required to implement ranged and sliced data fetching.
+        """
+        return self._meta_tree.get('record_max_length', None)
