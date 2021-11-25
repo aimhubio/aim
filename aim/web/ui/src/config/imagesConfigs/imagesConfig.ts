5ff069239cf4899ca7272e9c849d56fbe408cda0
Ruben Karapetyan <karapetyanrouben@gmail.com>
2021-11-25 08:49:57 +0400

    Apply throttling on images blob data update (#1052)
    * Apply throttling on images blob data update
    
    * Apply throttled update only when images blobs object is already set on state

--- aim/web/ui/src/config/imagesConfigs/imagesConfig.ts
+++ aim/web/ui/src/config/imagesConfigs/imagesConfig.ts
@@ -3,3 +3,5 @@ export const imageFixedHeight = 110;
 export const batchCollectDelay = 200;
 
 export const batchSendDelay = 300;
+
+export const blobsUpdateThrottleDelay = 250;
--- aim/web/ui/src/services/models/imagesExplore/imagesExploreAppModel.ts
+++ aim/web/ui/src/services/models/imagesExplore/imagesExploreAppModel.ts
@@ -6,6 +6,7 @@ import { saveAs } from 'file-saver';
 import { RowHeightSize } from 'config/table/tableConfigs';
 import { BookmarkNotificationsEnum } from 'config/notification-messages/notificationMessages';
 import { ResizeModeEnum, RowHeightEnum } from 'config/enums/tableEnums';
+import { blobsUpdateThrottleDelay } from 'config/imagesConfigs/imagesConfig';
 
 import {
   getImagesExploreTableColumns,
@@ -786,18 +787,36 @@ async function getImagesMetricsData(
 }
 
 async function getImagesBlobsData(uris: string[]) {
-  const imagesBlobs: { [key: string]: string } =
-    model.getState()?.imagesBlobs || {};
   const stream = await imagesExploreService.getImagesByURIs(uris).call();
   let gen = adjustable_reader(stream);
   let buffer_pairs = decode_buffer_pairs(gen);
   let decodedPairs = decodePathsVals(buffer_pairs);
   let objects = iterFoldTree(decodedPairs, 1);
 
+  const throttledBlobsUpdate = _.throttle(function () {
+    model.setState({
+      imagesBlobs: { ...(model.getState()?.imagesBlobs || {}) },
+    });
+  }, blobsUpdateThrottleDelay);
+
+  let firsItem = true;
   for await (let [keys, val] of objects) {
+    const imagesBlobs: { [key: string]: string } =
+      model.getState()?.imagesBlobs || {};
     imagesBlobs[keys[0]] = arrayBufferToBase64(val as ArrayBuffer) as string;
-    model.setState({ imagesBlobs: { ...imagesBlobs } });
+
+    if (firsItem) {
+      model.setState({
+        imagesBlobs: { ...imagesBlobs },
+      });
+      firsItem = false;
+    } else {
+      throttledBlobsUpdate();
+    }
   }
+
+  throttledBlobsUpdate.cancel();
+  model.setState({ imagesBlobs: { ...(model.getState()?.imagesBlobs || {}) } });
 }
 
 function sortWithAllGroupFields(
