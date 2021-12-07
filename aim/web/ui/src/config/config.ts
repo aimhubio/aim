f2079bd2fc3c35fea0d2204f47a545b96c0283ae
Ruben Karapetyan <karapetyanrouben@gmail.com>
2021-12-07 18:13:07 +0400

    Add ability to provide custom base path for API

--- aim/web/ui/src/config/config.ts
+++ aim/web/ui/src/config/config.ts
@@ -10,5 +10,7 @@ try {
 export const isDEVModeOn: boolean = process.env.NODE_ENV === 'development';
 
 export const API_HOST: string = isDEVModeOn
-  ? 'http://127.0.0.1:43800/api'
-  : `${globalScope.location.protocol}//${globalScope.location.hostname}:${globalScope.location.port}/api`;
+  ? `http://127.0.0.1:43800${(window as any).API_BASE_PATH}/api`
+  : `${globalScope.location.protocol}//${globalScope.location.hostname}:${
+      globalScope.location.port
+    }${(window as any).API_BASE_PATH}/api`;
