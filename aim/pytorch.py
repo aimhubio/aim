d4fb230bd1cdd61ca13a5d6f1ada6df4b2513f6f
mihran113 <vanyanmihran@gmail.com>
2021-12-08 17:39:57 +0400

    Moved distribution tracking utils to `sdk/adapters/pytorch`

---
+++ aim/pytorch.py
@@ -0,0 +1,2 @@
+# Alias to SDK PyTorch utils
+from aim.sdk.adapters.pytorch import track_params_dists, track_gradients_dists # noqa
--- examples/pytorch_track.py
+++ examples/pytorch_track.py
@@ -1,82 +1,10 @@
-from aim import Run, Distribution
+from aim import Run
+from aim.pytorch import track_gradients_dists, track_params_dists
 
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-
-
-# Move to SDK/adapters/pytorch
-# track_params_dists, track_gradients_dists, get_model_layers
-
-def track_params_dists(model, run):
-    data_hist = get_model_layers(model, 'data')
-
-    for name, params in data_hist.items():
-        if 'weight' in params:
-            run.track(Distribution(params['weight']),
-                      name=name,
-                      context={
-                          'type': 'data',
-                          'params': 'weights',
-                      })
-        if 'bias' in params:
-            run.track(Distribution(params['bias']),
-                      name=name,
-                      context={
-                          'type': 'data',
-                          'params': 'biases',
-                      })
-
-
-def track_gradients_dists(model, run):
-    grad_hist = get_model_layers(model, 'grad')
-
-    for name, params in grad_hist.items():
-        if 'weight' in params:
-            run.track(Distribution(params['weight']),
-                      name=name,
-                      context={
-                          'type': 'gradients',
-                          'params': 'weights',
-                      })
-        if 'bias' in params:
-            run.track(Distribution(params['bias']),
-                      name=name,
-                      context={
-                          'type': 'gradients',
-                          'params': 'biases',
-                      })
-
-
-def get_model_layers(model, dt, parent_name=None):
-    layers = {}
-    for name, m in model.named_children():
-        layer_name = '{}__{}'.format(parent_name, name) \
-            if parent_name \
-            else name
-        layer_name += '.{}'.format(type(m).__name__)
-
-        if len(list(m.named_children())):
-            layers.update(get_model_layers(m, dt, layer_name))
-        else:
-            layers[layer_name] = {}
-            if hasattr(m, 'weight') \
-                    and m.weight is not None \
-                    and hasattr(m.weight, dt):
-                layers[layer_name]['weight'] = get_pt_tensor(getattr(m.weight, dt)).numpy()
-
-            if hasattr(m, 'bias') \
-                    and m.bias is not None \
-                    and hasattr(m.bias, dt):
-                layers[layer_name]['bias'] = get_pt_tensor(getattr(m.bias, dt)).numpy()
-
-    return layers
-
-
-# Move tensor from GPU to CPU
-def get_pt_tensor(t):
-    return t.cpu() if hasattr(t, 'is_cuda') and t.is_cuda else t
 
 
 # Initialize a new Run
@@ -191,7 +119,6 @@ for epoch in range(num_epochs):
             if i % 300 == 0:
                 aim_run.track(loss.item(), name='loss', epoch=epoch, context={'subset': 'val'})
                 aim_run.track(acc, name='accuracy', epoch=epoch, context={'subset': 'val'})
-
 
 
 # Test the model
