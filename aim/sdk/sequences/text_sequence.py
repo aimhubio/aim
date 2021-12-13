ab0c84a8ce7dd0a29a6438aad8bb596a7e3c0545
mihran113 <vanyanmihran@gmail.com>
2021-12-13 11:36:55 +0400

    Add missing method and typename for `Texts` sequence class

--- aim/sdk/sequences/text_sequence.py
+++ aim/sdk/sequences/text_sequence.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 from aim.sdk.sequence import Sequence
 from aim.sdk.objects.text import Text
@@ -8,8 +8,9 @@ class Texts(Sequence):
     """Class representing series of Text objects."""
 
     @classmethod
-    def allowed_dtypes(cls) -> Tuple[str, ...]:
-        return (Text.get_typename(),)
+    def allowed_dtypes(cls) -> Union[str, Tuple[str, ...]]:
+        text_typename = Text.get_typename()
+        return text_typename, f'list({text_typename})'
 
     @classmethod
     def sequence_name(cls) -> str:
@@ -28,3 +29,10 @@ class Texts(Sequence):
         Required to implement ranged and sliced data fetching.
         """
         return self._meta_tree['last_step']
+
+    def record_length(self):
+        """Get tracked records longest list length or `None` if Text objects are tracked.
+
+        Required to implement ranged and sliced data fetching.
+        """
+        return self._meta_tree.get('record_max_length', None)
