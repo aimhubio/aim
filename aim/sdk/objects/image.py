61c07fa2a2ca4e989e1720a42bf0fba78ef2e2d7
Arthur Poghosyan <poghosyan.arthur94@gmail.com>
2021-12-24 14:17:45 +0400

    added support to read from file, added compress option

--- aim/sdk/objects/image.py
+++ aim/sdk/objects/image.py
@@ -1,3 +1,5 @@
+import os.path
+
 from PIL import Image as PILImage
 
 from io import BytesIO
@@ -17,21 +19,24 @@ class Image(CustomObject):
     Args:
          image (:obj:): pillow `Image` object or `torch.Tensor` or `numpy.array` used to construct `aim.Image`.
          caption (:obj:`str`, optional): Optional image caption. '' by default.
+         compress (:obj:`bool`, optional): If True, will convert to JPG and reduce image quality
     """
 
     AIM_NAME = 'aim.image'
 
-    def __init__(self, image, caption: str = ''):
+    def __init__(self, image, caption: str = '', compress=False):
         super().__init__()
 
-        if inst_has_typename(image, ['PIL', 'Image']):
-            self._from_pil_image(image)
+        if isinstance(image, str):
+            self._from_file_path(image, compress)
+        elif inst_has_typename(image, ['PIL', 'Image']):
+            self._from_pil_image(image, compress)
         elif inst_has_typename(image, ['torch', 'Tensor']):
-            self._from_torch_tensor(image)
+            self._from_torch_tensor(image, compress)
         elif inst_has_typename(image, ['tensorflow', 'Tensor']):
-            self._from_tf_tensor(image)
+            self._from_tf_tensor(image, compress)
         elif inst_has_typename(image, ['numpy', 'array']):
-            self._from_numpy_array(image)
+            self._from_numpy_array(image, compress)
         else:
             raise TypeError(f'Cannot convert to aim.Image. Unsupported type {type(image)}.')
         self.caption = caption
@@ -101,18 +106,42 @@ class Image(CustomObject):
             'height': self.height,
         }
 
-    def _from_pil_image(self, pil_image: PILImage.Image):
+    def _from_pil_image(self, pil_image: PILImage.Image, compress=False):
         assert isinstance(pil_image, PILImage.Image)
-        image_bytes_p = BytesIO()
-        pil_image.save(image_bytes_p, format='png')
+        img_container = BytesIO()
 
-        self.storage['data'] = BLOB(data=image_bytes_p.getvalue())
+        if compress:
+            if pil_image.mode == 'RGBA' or isinstance(pil_image.info.get("transparency"), bytes):
+                # Remove transparency
+                alpha = pil_image.convert('RGBA').split()[-1]  # Get only alpha
+                background = PILImage.new("RGBA", pil_image.size, (255, 255, 255, 255))
+                background.paste(pil_image, mask=alpha)
+                pil_image = background.convert('RGB')
+
+            params = {
+                'format': 'jpeg',
+                'optimize': True,
+                'quality': 80
+            }
+        else:
+            params = {
+                'format': 'png'
+            }
+
+        pil_image.save(img_container, **params)
+        self.storage['data'] = BLOB(data=img_container.getvalue())
         self.storage['source'] = 'PIL.Image'
         self.storage['mode'] = pil_image.mode
-        self.storage['format'] = 'png'
+        self.storage['format'] = params['format']
         self.storage['width'], self.storage['height'] = pil_image.size
 
-    def _from_numpy_array(self, array: np.ndarray):
+    def _from_file_path(self, file_path, compress=False):
+        if not os.path.isfile(file_path):
+            raise ValueError("Invalid image file path.")
+
+        return self._from_pil_image(PILImage.open(file_path), compress)
+
+    def _from_numpy_array(self, array: np.ndarray, compress=False):
         if array.ndim not in {2, 3}:
             raise ValueError('Cannot convert to aim.Image. array must have 2/3-D shape.')
 
@@ -120,10 +149,9 @@ class Image(CustomObject):
             pil_image = PILImage.fromarray(array[:, :, 0])
         else:
             pil_image = PILImage.fromarray(array)
+        self._from_pil_image(pil_image, compress)
 
-        self._from_pil_image(pil_image)
-
-    def _from_torch_tensor(self, tensor):
+    def _from_torch_tensor(self, tensor, compress=False):
         try:
             import torch
             assert isinstance(tensor, torch.Tensor)
@@ -140,9 +168,9 @@ class Image(CustomObject):
             pil_image = PILImage.fromarray(array[:, :, 0])
         else:
             pil_image = PILImage.fromarray(array)
-        self._from_pil_image(pil_image)
+        self._from_pil_image(pil_image, compress)
 
-    def _from_tf_tensor(self, tensor):
+    def _from_tf_tensor(self, tensor, compress=False):
         try:
             import tensorflow as tf
             assert isinstance(tensor, tf.Tensor)
@@ -161,7 +189,7 @@ class Image(CustomObject):
             pil_image = PILImage.fromarray(array[:, :, 0])
         else:
             pil_image = PILImage.fromarray(array)
-        self._from_pil_image(pil_image)
+        self._from_pil_image(pil_image, compress)
 
 
 def convert_to_aim_image_list(images, labels=None) -> List[Image]:
