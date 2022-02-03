0fd7a84ce579e4952eaf3711a207be359e599107
rubenaprikyan <rubenaprikyan@gmail.com>
2022-02-03 17:04:40 +0400

    Add analytics keys map object

---
+++ aim/web/ui/src/config/analytics/analyticsKeysMap.ts
@@ -0,0 +1,268 @@
+export default {
+  metrics: {
+    pageView: '[MetricsExplorer] Page view',
+    liveUpdate: '[MetricsExplorer] Live update',
+    bookmarks: '[MetricsExplorer] Create bookmark',
+    searchClick: '[MetricsExplorer] Search click',
+    useAdvancedSearch: '[MetricsExplorer] Use advanced search',
+    attachTag: '[MetricsExplorer] Attach tag',
+    table: {
+      exports: {
+        csv: '[MetricsExplorer][Table] Export CSV',
+      },
+      archiveRunsBatch: '[MetricsExplorer][Table] Archive runs by batch',
+      deleteRunsBatch: '[MetricsExplorer][Table] Delete runs by batch',
+      changeResizeMode: '[MetricsExplorer][Table] Change view (Resize) mode',
+      resetColumnsOrder: '[MetricsExplorer][Table] Reset columns order',
+      showAllColumns: '[MetricsExplorer][Table] Show all columns',
+      hideAllColumns: '[MetricsExplorer][Table] Hide all columns',
+      changeTableRowHeight: '[MetricsExplorer][Table] Change rows height',
+      resetSorting: '[MetricsExplorer][Table] Reset sorting',
+    },
+    chart: {
+      controls: {
+        changeAggregation:
+          '[MetricsExplorer][Chart][Controls] Change aggregation mode',
+        changeXAxisProperties:
+          '[MetricsExplorer][Chart][Controls] Change X-Axis properties',
+        selectAxesScale: '[MetricsExplorer][Chart][Controls] Select Axes Scale',
+        selectSmoothingOptions:
+          '[MetricsExplorer][Chart][Controls] Select smoothing options',
+        outliers: '[MetricsExplorer][Chart][Controls] Change outliers',
+        selectHighlightMode:
+          '[MetricsExplorer][Chart][Controls] Select highlight mode',
+        tooltip: {
+          changeTooltipVisibility:
+            '[MetricsExplorer][Chart][Controls] Change tooltip visibility',
+        },
+      },
+    },
+    groupings: {
+      chart: {
+        modeChange: '[MetricsExplorer][Grouping][Chart] mode change',
+      },
+      color: {
+        modeChange: '[MetricsExplorer][Grouping][Color] mode change',
+        paletteChange: '[MetricsExplorer][Grouping][Color] palette change',
+        persistenceChange:
+          '[MetricsExplorer][Grouping][Color] persistence change',
+      },
+      stroke: {
+        modeChange: '[MetricsExplorer][Grouping][Stroke] mode change',
+        persistenceChange:
+          '[MetricsExplorer][Grouping][Stroke] persistence change',
+      },
+    },
+  },
+  params: {
+    pageView: '[ParamsExplorer] Page view',
+    liveUpdate: '[ParamsExplorer] Live update',
+    bookmarks: '[ParamsExplorer] Create bookmark',
+    searchClick: '[ParamsExplorer] Search click',
+    useAdvancedSearch: '[ParamsExplorer] Use advanced search',
+    attachTag: '[ParamsExplorer] Attach tag',
+    table: {
+      exports: {
+        csv: '[ParamsExplorer][Table] Export CSV',
+      },
+      archiveRunsBatch: '[ParamsExplorer][Table] Archive runs by batch',
+      deleteRunsBatch: '[ParamsExplorer][Table] Delete runs by batch',
+      changeResizeMode: '[ParamsExplorer][Table] Change view (Resize) mode',
+      resetColumnsOrder: '[ParamsExplorer][Table] Reset columns order',
+      showAllColumns: '[ParamsExplorer][Table] Show all columns',
+      hideAllColumns: '[ParamsExplorer][Table] Hide all columns',
+      changeTableRowHeight: '[ParamsExplorer][Table] Change rows height',
+      resetSorting: '[ParamsExplorer][Table] Reset sorting',
+    },
+    chart: {
+      controls: {
+        changeCurveInterpolationMode:
+          '[ParamsExplorer][Chart][Controls] Change curve interpolation mode',
+        tooltip: {
+          changeTooltipVisibility:
+            '[ParamsExplorer][Chart][Controls] Change tooltip visibility',
+        },
+        changeColorIndicatorMode:
+          '[ParamsExplorer][Chart][Controls] Change color indicator mode',
+      },
+    },
+    groupings: {
+      chart: {
+        modeChange: '[ParamsExplorer][Grouping][Chart] mode change',
+      },
+      color: {
+        modeChange: '[ParamsExplorer][Grouping][Color] mode change',
+        paletteChange: '[ParamsExplorer][Grouping][Color] palette change',
+        persistenceChange:
+          '[ParamsExplorer][Grouping][Color] persistence change',
+      },
+      stroke: {
+        modeChange: '[ParamsExplorer][Grouping][Stroke] mode change',
+        persistenceChange:
+          '[ParamsExplorer][Grouping][Stroke] persistence change',
+      },
+    },
+  },
+  runs: {
+    pageView: '[RunsExplorer] Page view',
+    liveUpdate: '[RunsExplorer] Live update',
+    bookmarks: '[RunsExplorer] Create bookmark',
+    searchClick: '[RunsExplorer] Search click',
+    table: {
+      exports: {
+        csv: '[RunsExplorer][Table] Export CSV',
+      },
+      archiveRunsBatch: '[RunsExplorer][Table] Archive runs by batch',
+      deleteRunsBatch: '[RunsExplorer][Table] Delete runs by batch',
+      changeResizeMode: '[RunsExplorer][Table] Change view (Resize) mode',
+      resetColumnsOrder: '[RunsExplorer][Table] Reset columns order',
+      showAllColumns: '[RunsExplorer][Table] Show all columns',
+      hideAllColumns: '[RunsExplorer][Table] Hide all columns',
+      changeTableRowHeight: '[RunsExplorer][Table] Change rows height',
+      resetSorting: '[RunsExplorer][Table] Reset sorting',
+    },
+  },
+  scatters: {
+    pageView: '[ScattersExplorer] Page view',
+    liveUpdate: '[ScattersExplorer] Live update',
+    bookmarks: '[ScattersExplorer] Create bookmark',
+    searchClick: '[ScattersExplorer] Search click',
+    useAdvancedSearch: '[ScattersExplorer] Use advanced search',
+    attachTag: '[ScattersExplorer] Attach tag',
+    table: {
+      exports: {
+        csv: '[ScattersExplorer][Table] Export CSV',
+      },
+      archiveRunsBatch: '[ScattersExplorer][Table] Archive runs by batch',
+      deleteRunsBatch: '[ScattersExplorer][Table] Delete runs by batch',
+      changeResizeMode: '[ScattersExplorer][Table] Change view (Resize) mode',
+      resetColumnsOrder: '[ScattersExplorer][Table] Reset columns order',
+      showAllColumns: '[ScattersExplorer][Table] Show all columns',
+      hideAllColumns: '[ScattersExplorer][Table] Hide all columns',
+      changeTableRowHeight: '[ScattersExplorer][Table] Change rows height',
+      resetSorting: '[ScattersExplorer][Table] Reset sorting',
+    },
+    chart: {
+      controls: {
+        changeTrendlineVisibility:
+          '[ScattersExplorer][Chart][Controls] Change trendline visibility',
+        tooltip: {
+          changeTooltipVisibility:
+            '[ScattersExplorer][Chart][Controls] Change tooltip visibility',
+        },
+      },
+    },
+    groupings: {
+      chart: {
+        modeChange: '[ScattersExplorer][Grouping][Chart] mode change',
+      },
+      color: {
+        modeChange: '[ScattersExplorer][Grouping][Color] mode change',
+        paletteChange: '[ScattersExplorer][Grouping][Color] palette change',
+        persistenceChange:
+          '[ScattersExplorer][Grouping][Color] persistence change',
+      },
+    },
+  },
+  images: {
+    pageView: '[ImagesExplorer] Page view',
+    bookmarks: '[ImagesExplorer] Create bookmark',
+    searchClick: '[ImagesExplorer] Search click',
+    useAdvancedSearch: '[ImagesExplorer] Use advanced search',
+    table: {
+      exports: {
+        csv: '[ImagesExplorer][Table] Export CSV',
+      },
+      archiveRunsBatch: '[ImagesExplorer][Table] Archive runs by batch',
+      deleteRunsBatch: '[ImagesExplorer][Table] Delete runs by batch',
+      changeResizeMode: '[ImagesExplorer][Table] Change view (Resize) mode',
+      resetColumnsOrder: '[ImagesExplorer][Table] Reset columns order',
+      showAllColumns: '[ImagesExplorer][Table] Show all columns',
+      hideAllColumns: '[ImagesExplorer][Table] Hide all columns',
+      changeTableRowHeight: '[ImagesExplorer][Table] Change rows height',
+      resetSorting: '[ImagesExplorer][Table] Reset sorting',
+    },
+    groupings: {
+      group: {
+        modeChange: '[ImagesExplorer][Grouping][Group] mode change',
+      },
+    },
+    imagesPanel: {
+      clickApplyButton: '[ImagesExplorer][ImagesPanel] Click apply button',
+      controls: {
+        changeImageProperties:
+          '[ImagesExplorer][ImagesPanel][Controls] Change image properties',
+        sortImages: '[ImagesExplorer][ImagesPanel][Controls] Sort images',
+        groupStacking:
+          '[ImagesExplorer][ImagesPanel][Controls] Click group stacking',
+        tooltip: {
+          changeTooltipVisibility:
+            '[ImagesExplorer][ImagesPanel][Controls] Change tooltip visibility',
+        },
+      },
+    },
+  },
+  runDetails: {
+    pageView: '[RunDetail] Page view',
+    tabs: {
+      metrics: {
+        pageView: '[RunDetail] [Metrics] Page view',
+      },
+      params: {
+        pageView: '[RunDetail] [Params] Page view',
+      },
+      system: {
+        pageView: '[RunDetail] [System] Page view',
+      },
+      distributions: {
+        pageView: '[RunDetail] [Distributions] Page view',
+        clickApplyButton: '[RunDetail] [Distributions] Click apply button',
+        changeContext: '[RunDetail] [Distributions] Change context',
+        onClickHeatMapCell:
+          '[RunDetail] [Distributions] Click heatmap cell context',
+      },
+      images: {
+        pageView: '[RunDetail] [Images] Page view',
+        clickApplyButton: '[RunDetail] [Images] Click apply button',
+        changeContext: '[RunDetail] [Images] Change context',
+      },
+      audios: {
+        pageView: '[RunDetail] [Audios] Page view',
+        clickApplyButton: '[RunDetail] [Audios] Click apply button',
+        changeContext: '[RunDetail] [Audios] Change context',
+      },
+      figures: {
+        pageView: '[RunDetail] [Figures] Page view',
+        clickApplyButton: '[RunDetail] [Figures] Click apply button',
+        changeContext: '[RunDetail] [Figures] Change context',
+      },
+      settings: {
+        pageView: '[RunDetail] [Settings] Page view',
+        deleteRun: '[RunDetail] [Settings] Delete run',
+        archiveRun: '[RunDetail] [Settings] Archive run',
+      },
+    },
+  },
+  tags: {
+    pageView: '[TagsPage] Page view',
+    create: '[TagsPage] Create tag',
+  },
+  bookmarks: {
+    pageView: '[BookmarksPage] Page view',
+    view: '[BookmarksPage] View bookmark',
+  },
+  home: {
+    pageView: '[HomePage] Page view',
+    activityCellClick: '[HomePage] Click on Activity cell',
+    createGithubIssue: '[HomePage] Click on create gitHub issue',
+    slackCommunity: '[HomePage] Click on Join Aim slack community',
+    docs: '[HomePage] Click on documentation icon',
+    colab: '[HomePage] Click on colab notebook icon',
+    liveDemo: '[HomePage] Click on Live demo icon',
+    copyCodeSnippet: '[HomePage] Copy Code Snippet',
+  },
+  sidebar: {
+    slack: '[SideBar] Click on slack icon',
+    docs: '[Sidebar] Click on docs icon',
+  },
+};
