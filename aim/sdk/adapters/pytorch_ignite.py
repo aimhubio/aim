da70971d1a967ddf86553baf1f69ea22fae123a7
mihran113 <vanyanmihran@gmail.com>
2022-02-15 21:48:13 +0400

    Update docstrings for newly added classes

--- aim/sdk/adapters/pytorch_ignite.py
+++ aim/sdk/adapters/pytorch_ignite.py
@@ -15,12 +15,25 @@ try:
     from ignite.engine import Engine, Events
 except ImportError:
     raise RuntimeError(
-        "This contrib module requires pythorch_ignite to be installed. "
-        "Please install it with command: \n pip install pythorch-ignite"
+        'This contrib module requires pythorch_ignite to be installed. '
+        'Please install it with command: \n pip install pythorch-ignite'
     )
 
 
 class AimLogger(BaseLogger):
+    """
+        `Aim` tracking client handler to log parameters and metrics during the training
+        and validation.
+
+        Args:
+            repo: Aim repository path
+            experiment: Aim run's experiment name
+            train_metric_prefix: prefix to identify training metrics
+            val_metric_prefix: prefix to identify validation metrics
+            test_metric_prefix: prefix to identify test metrics
+            system_tracking_interval: Sets the tracking interval in seconds
+                for system usage metrics (CPU, Memory, etc.)
+    """
     def __init__(self,
                  repo: Optional[str] = None,
                  experiment: Optional[str] = None,
@@ -48,7 +61,8 @@ class AimLogger(BaseLogger):
             self._run = Run(
                 repo=self._repo_path,
                 experiment=self._experiment_name,
-                system_tracking_interval=self._system_tracking_interval
+                system_tracking_interval=self._system_tracking_interval,
+                log_system_params=True
             )
         return self._run
 
@@ -109,6 +123,23 @@ class AimLogger(BaseLogger):
 
 
 class OutputHandler(BaseOutputHandler):
+    """Helper handler to log engine's output and/or metrics.
+
+        Args:
+            tag: common title for all produced plots. For example, 'training'
+            metric_names: list of metric names to plot or a string "all" to plot all available
+                metrics.
+            output_transform: output transform function to prepare `engine.state.output` as a number.
+                For example, `output_transform = lambda output: output`
+                This function can also return a dictionary, e.g `{'loss': loss1, 'another_loss': loss2}` to label the plot
+                with corresponding keys.
+            global_step_transform: global step transform function to output a desired global step.
+                Input of the function is `(engine, event_name)`. Output of function should be an integer.
+                Default is None, global_step based on attached engine. If provided,
+                uses function output as global_step. To setup global step from another engine, please use
+                :meth:`~ignite.contrib.handlers.mlflow_logger.global_step_from_engine`.
+            state_attributes: list of attributes of the ``trainer.state`` to plot.
+    """
     def __init__(
         self,
         tag: str,
@@ -124,7 +155,7 @@ class OutputHandler(BaseOutputHandler):
     def __call__(self, engine: Engine, logger: AimLogger, event_name: Union[str, Events]) -> None:
 
         if not isinstance(logger, AimLogger):
-            raise TypeError("Handler 'OutputHandler' works only with AimLogger")
+            raise TypeError('Handler \'OutputHandler\' works only with AimLogger')
 
         rendered_metrics = self._setup_output_metrics_state_attrs(engine)
 
@@ -132,30 +163,38 @@ class OutputHandler(BaseOutputHandler):
 
         if not isinstance(global_step, int):
             raise TypeError(
-                f"global_step must be int, got {type(global_step)}."
-                " Please check the output of global_step_transform."
+                f'global_step must be int, got {type(global_step)}.'
+                ' Please check the output of global_step_transform.'
             )
 
         metrics = {}
         for keys, value in rendered_metrics.items():
-            key = "_".join(keys)
+            key = '_'.join(keys)
             metrics[key] = value
 
         logger.log_metrics(metrics, step=global_step)
 
 
 class OptimizerParamsHandler(BaseOptimizerParamsHandler):
-    def __init__(self, optimizer: Optimizer, param_name: str = "lr", tag: Optional[str] = None):
+    """Helper handler to log optimizer parameters
+
+        Args:
+            optimizer: torch optimizer or any object with attribute ``param_groups``
+                as a sequence.
+            param_name: parameter name
+            tag: common title for all produced plots. For example, 'generator'
+    """
+    def __init__(self, optimizer: Optimizer, param_name: str = 'lr', tag: Optional[str] = None):
         super(OptimizerParamsHandler, self).__init__(optimizer, param_name, tag)
 
     def __call__(self, engine: Engine, logger: AimLogger, event_name: Union[str, Events]) -> None:
         if not isinstance(logger, AimLogger):
-            raise TypeError("Handler OptimizerParamsHandler works only with AimLogger")
+            raise TypeError('Handler OptimizerParamsHandler works only with AimLogger')
 
         global_step = engine.state.get_event_attrib_value(event_name)
-        tag_prefix = f"{self.tag}_" if self.tag else ""
+        tag_prefix = f'{self.tag}_' if self.tag else ''
         params = {
-            f"{tag_prefix}{self.param_name}_group_{i}": float(param_group[self.param_name])
+            f'{tag_prefix}{self.param_name}_group_{i}': float(param_group[self.param_name])
             for i, param_group in enumerate(self.optimizer.param_groups)
         }
 
