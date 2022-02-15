64be2d404c3acf6607df2ba31ef1058eb39bf434
Karen Hambardzumyan <mahnerak@gmail.com>
2022-02-16 02:47:28 +0400

    Types only for type annotations

--- aim/storage/utils.py
+++ aim/storage/utils.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 class ArrayFlagType:
     def __repr__(self):
         return "<ArrayFlag>"
@@ -34,8 +36,36 @@ class CustomObjectFlagType:
         return f"<CustomObjectFlag type={self.aim_name}>"
 
 
-__all__ = [
-    'ArrayFlag',
-    'ObjectFlag',
-    'CustomObjectFlagType',
-]
+class BLOB:
+    def __init__(
+        self,
+        data=None,
+        loader_fn=None
+    ):
+        self.data = data
+        self.loader_fn = loader_fn
+
+    def __bytes__(self):
+        return bytes(self.load())
+
+    def load(self):
+        if self.data is None:
+            assert self.loader_fn is not None
+            self.data = self.loader_fn()
+            self.loader_fn = None
+        return self.data
+
+    def __deepcopy__(self, memo):
+        data = self.load()
+        instance = self.__class__(data=deepcopy(data, memo=memo))
+        memo[id(self)] = instance
+        return instance
+
+    def transform(self, transform_fn):
+        if self.data is not None:
+            return self.__class__(transform_fn(self.data))
+
+        def loader():
+            return transform_fn(self.load())
+
+        return self.__class__(loader_fn=loader)
