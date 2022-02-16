8194546fafbf8e82bd4cb72744a586d064ae2d74
Arsen Alkamyan <alkamyan97@gmail.com>
2022-02-17 00:02:47 +0400

    Added RunOverviewSidebar base component structure

---
+++ aim/web/ui/src/pages/RunDetail/RunOverviewTab/RunOverviewSidebar/RunOverviewSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { IRunOverviewSidebarProps } from './RunOverviewSidebar.d';
+
+function RunOverviewSidebar(props: IRunOverviewSidebarProps) {
+  return <div></div>;
+}
+
+export default React.memo(RunOverviewSidebar);
