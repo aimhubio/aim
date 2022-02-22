9f5b28b4c5329f49d88469b32c28462ee7a0fc1d
alberttorosyan <32957250+alberttorosyan@users.noreply.github.com>
2022-02-22 23:05:47 +0400

    [feat] Add GH Actions workflow for Docker image creation.

---
+++ docker/create-docker-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ./docker
+
+if [ $UPDATE_LATEST_TAG == 1 ]
+then
+  docker image build --no-cache	 \
+    -t aimstack/aim:$AIM_VERSION -t aimstack/aim:latest --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
+else
+  docker image build --no-cache	\
+    -t aimstack/aim:$AIM_VERSION --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
+fi
+docker image push --all-tags aimstack/aim
