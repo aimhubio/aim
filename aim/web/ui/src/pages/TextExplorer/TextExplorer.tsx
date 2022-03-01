451d6f4ce001b141518e95ce627f447ed0e782ae
Ruben Aprikyan <rubenaprikyan@gmail.com>
2022-03-01 14:36:48 +0400

    Add initial methods for model (#1425)

--- aim/web/ui/src/pages/TextExplorer/TextExplorer.tsx
+++ aim/web/ui/src/pages/TextExplorer/TextExplorer.tsx
@@ -1,15 +1,53 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
 
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 import BusyLoaderWrapper from 'components/BusyLoaderWrapper/BusyLoaderWrapper';
 import TableLoader from 'components/TableLoader/TableLoader';
 
+import { ANALYTICS_EVENT_KEYS } from 'config/analytics/analyticsKeysMap';
+
+import textExplorerAppModel from 'services/models/textExplorer/textExplorerAppModel';
+import * as analytics from 'services/analytics';
+
 import './TextExplorer.scss';
 
 function TextExplorer() {
+  const route = useRouteMatch<any>();
   const tableElemRef = React.useRef<HTMLDivElement>(null);
   const textsWrapperRef = React.useRef<any>(null);
   const wrapperElemRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    textExplorerAppModel.initialize(route.params.appId);
+    let appRequestRef: {
+      call: () => Promise<void>;
+      abort: () => void;
+    };
+    let textRequestRef: {
+      call: () => Promise<void>;
+      abort: () => void;
+    };
+    if (route.params.appId) {
+      appRequestRef = textExplorerAppModel.getAppConfigData(route.params.appId);
+      appRequestRef.call().then(() => {
+        textRequestRef = textExplorerAppModel.getTextData();
+        textRequestRef.call();
+      });
+    } else {
+      textExplorerAppModel.setDefaultAppConfigData();
+      textRequestRef = textExplorerAppModel.getTextData();
+      textRequestRef.call();
+    }
+    // analytics.pageView(ANALYTICS_EVENT_KEYS.images.pageView);
+    return () => {
+      textRequestRef?.abort();
+      if (appRequestRef) {
+        appRequestRef.abort();
+      }
+    };
+  }, [route.params.appId]);
+
   return (
     <ErrorBoundary>
       <div className='TextExplorer__container' ref={wrapperElemRef}>
