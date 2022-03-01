5b952ba1e99d84ba18ade9f6655bab864c9ee6dc
VkoHov <vkohovhannisyan96@gmail.com>
2022-03-01 19:38:02 +0400

    Data list height calc

--- aim/web/ui/src/components/kit/DataList/config.ts
+++ aim/web/ui/src/components/kit/DataList/config.ts
@@ -1,3 +1,3 @@
 export const DATA_LIST_HEADER_HEIGHT = 34;
-export const DATA_LIST_ROW_HEIGHT = 33;
-export const SEARCH_BAR_HEIGHT = 42;
+export const DATA_LIST_ROW_HEIGHT = 32;
+export const SEARCH_BAR_HEIGHT = 46;
