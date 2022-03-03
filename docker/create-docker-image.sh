dc71668e1009664ffc24d2156daa8852665289c7
mihran113 <vanyanmihran@gmail.com>
2022-03-04 02:29:41 +0400

    fix syntax

--- docker/create-docker-image.sh
+++ docker/create-docker-image.sh
@@ -7,13 +7,8 @@ echo '-----'
 
 if [ $UPDATE_LATEST_TAG == 1 ]
 then
-#  docker image build --no-cache	 \
-#    -t aimstack/aim:$AIM_VERSION -t aimstack/aim:latest --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
     echo 'latest'
 else
-#  docker image build --no-cache	\
-#    -t aimstack/aim:$AIM_VERSION --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
     ${AIM_VERSION::-8}
     echo 'yooohoooo'
 fi
-#docker image push --all-tags aimstack/aim
