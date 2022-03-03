27a8179c8f7c537291be28930bce2e86ee593dd3
mihran113 <vanyanmihran@gmail.com>
2022-03-04 03:33:48 +0400

    Clean up mess

--- docker/create-docker-image.sh
+++ docker/create-docker-image.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo '-----'
-echo $UPDATE_LATEST_TAG
-echo $AIM_VERSION
-echo '-----'
-
 if [ $UPDATE_LATEST_TAG == 1 ]
 then
-    echo 'latest'
+  docker image build --no-cache	 \
+    -t aimstack/aim:$AIM_VERSION -t aimstack/aim:latest --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
 else
-    echo ${AIM_VERSION::-8}
-    echo 'yooohoooo'
+  if [[ "$AIM_VERSION" == *"dev"* ]]; then
+    AIM_VERSION=${AIM_VERSION::-8}
+  fi
+  docker image build --no-cache	\
+    -t aimstack/aim:$AIM_VERSION --build-arg AIM_VERSION=${AIM_VERSION::-8} -f Dockerfile .
 fi
+docker image push --all-tags aimstack/aim
