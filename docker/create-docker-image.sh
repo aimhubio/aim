14dec39fbbd6909975145e80f588fd3eb495a8a6
mihran113 <vanyanmihran@gmail.com>
2022-03-04 02:26:26 +0400

    Test docker env vars

--- docker/create-docker-image.sh
+++ docker/create-docker-image.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+echo '-----'
+echo $UPDATE_LATEST_TAG
+echo $AIM_VERSION
+echo '-----'
+
 if [ $UPDATE_LATEST_TAG == 1 ]
 then
-  docker image build --no-cache	 \
-    -t aimstack/aim:$AIM_VERSION -t aimstack/aim:latest --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
+#  docker image build --no-cache	 \
+#    -t aimstack/aim:$AIM_VERSION -t aimstack/aim:latest --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
+    echo 'latest'
 else
-  docker image build --no-cache	\
-    -t aimstack/aim:$AIM_VERSION --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
+#  docker image build --no-cache	\
+#    -t aimstack/aim:$AIM_VERSION --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
+    ${AIM_VERSION::-8}
+    echo 'yooohoooo'
 fi
-docker image push --all-tags aimstack/aim
+#docker image push --all-tags aimstack/aim
