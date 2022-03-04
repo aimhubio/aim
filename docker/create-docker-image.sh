9cf616ca3ac6508e5edaa2a0460062d4accce802
mihran113 <vanyanmihran@gmail.com>
2022-03-04 16:02:58 +0400

    Address PR comments

--- docker/create-docker-image.sh
+++ docker/create-docker-image.sh
@@ -4,10 +4,11 @@ if [ $UPDATE_LATEST_TAG == 1 ]
 then
   docker image build --no-cache	 \
     -t aimstack/aim:$AIM_VERSION -t aimstack/aim:latest --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
+elif [ $UPDATE_LATEST_TAG == "nightly" ]
+then
+  docker image build --no-cache	 \
+    -t aimstack/aim:$AIM_VERSION -t aimstack/aim:nightly --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
 else
-  if [[ "$AIM_VERSION" == *".dev"* ]]; then
-    AIM_VERSION=${AIM_VERSION::-8}
-  fi
   docker image build --no-cache	\
     -t aimstack/aim:$AIM_VERSION --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
 fi
