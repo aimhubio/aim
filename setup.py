188746d32b1c53e2d2486110cb93c4f5d9b6e45b
Karen Hambardzumyan <mahnerak@gmail.com>
2022-03-09 04:28:33 +0400

    Non-hardcoded pre-requirements install

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ SETUP_REQUIRED = [
 # What packages are required for this module to be executed?
 REQUIRED = [
     'aimrecords==0.0.7',
-    'aimrocks==0.1.1rc3',
+    'aimrocks==0.1.2',
     'cachetools>=4.0.0',
     'click>=7.0',
     'cryptography>=3.0',
