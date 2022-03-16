7acc7e95065a103fe19cae3f1df87264662857cc
Arthur <48801049+devfox-se@users.noreply.github.com>
2022-03-16 13:44:56 +0400

    added error response handler and updated messages

--- aim/web/api/runs/object_views.py
+++ aim/web/api/runs/object_views.py
@@ -31,7 +31,10 @@ class CustomObjectApiConfig:
     @staticmethod
     def check_density(density):
         if density <= 0:
-            raise HTTPException(status_code=400, detail=f'Invalid density value: \'{density}\'. Density must be > 0.')
+            raise HTTPException(status_code=400, detail={
+                'message': f'Invalid density value: \'{density}\'.',
+                'detail': 'Density must be > 0.'
+            })
 
     @classmethod
     def register_endpoints(cls, router):
