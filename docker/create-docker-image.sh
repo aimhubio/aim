ad30bca1c52de9df87f80917a18ad4c5d33b46f9
Albert Torosyan <torosyanalbert@gmail.com>
2022-03-23 15:39:16 +0400

    [fix] Retry docker image build when PyPI package is newly pushed

--- docker/create-docker-image.sh
+++ docker/create-docker-image.sh
@@ -2,10 +2,18 @@
 
 if [ "$UPDATE_TAG" = "latest" ] || [ "$UPDATE_TAG" = "nightly" ]
 then
-  docker image build --no-cache	 \
-    -t aimstack/aim:$AIM_VERSION -t aimstack/aim:$UPDATE_TAG --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
+  for i in {1..5}
+  do
+    docker image build --no-cache	 \
+      -t aimstack/aim:$AIM_VERSION -t aimstack/aim:$UPDATE_TAG --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile . \
+      && break || echo "retry attempt ${i}" && sleep 120
+  done
 else
-  docker image build --no-cache	\
-    -t aimstack/aim:$AIM_VERSION --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile .
+  for i in {1..5}
+  do
+    docker image build --no-cache	\
+      -t aimstack/aim:$AIM_VERSION --build-arg AIM_VERSION=$AIM_VERSION -f Dockerfile . \
+      && break || echo "retry attempt ${i}" && sleep 120
+  done
 fi
 docker image push --all-tags aimstack/aim
