aa3b07d2a23c0a8c8db45c858f56d8c180830249
Arthur <48801049+devfox-se@users.noreply.github.com>
2022-03-24 16:17:21 +0400

    keep 'tf' alias but show deprecated

--- aim/cli/convert/commands.py
+++ aim/cli/convert/commands.py
@@ -42,6 +42,20 @@ def convert_tensorflow(ctx, logdir, flat):
     parse_tb_logs(logdir, repo_inst, flat)
 
 
+@convert.command(name='tf')
+@click.pass_context
+@click.option('--logdir', required=True, type=click.Path(exists=True,
+                                                         readable=True,
+                                                         dir_okay=True,
+                                                         resolve_path=True))
+@click.option('--flat', '-f', required=False, is_flag=True, default=False)
+def convert_tensorflow(ctx, logdir, flat):
+    click.secho('WARN: Command \'tf\' is deprecated and will be removed in future release,'
+                ' please use \'tensorboard\' instead.', fg='red')
+    repo_inst = ctx.obj['repo_inst']
+    parse_tb_logs(logdir, repo_inst, flat)
+
+
 @convert.command(name='mlflow')
 @click.pass_context
 @click.option('--tracking_uri', required=False, default=None)
