e2fd68a84728bdc97bcb7b261bb141f12f997b75
Albert Torosyan <torosyanalbert@gmail.com>
2022-04-04 19:02:36 +0400

    [feat] Refactor objects import

--- aim/sdk/__init__.py
+++ aim/sdk/__init__.py
@@ -10,11 +10,7 @@ from aim.sdk.run import Run
 from aim.sdk.repo import Repo
 
 # pre-defined sequences and custom objects
-from aim.sdk.objects import Image
-from aim.sdk.objects import Audio
-from aim.sdk.objects import Figure
-from aim.sdk.objects import Distribution
-from aim.sdk.objects import Text
+from aim.sdk.objects import *
 
 from aim.sdk.sequences.image_sequence import Images
 from aim.sdk.sequences.audio_sequence import Audios
