4da937818519f91fe67902ab7c0e07ae114b7f90
Arthur <48801049+devfox-se@users.noreply.github.com>
2022-04-05 20:58:07 +0400

    added new setup.py for ui files

---
+++ aim/web/ui/setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup
+
+version_file = '../../VERSION'
+with open(version_file) as vf:
+    __version__ = vf.read().strip()
+
+# Package meta-data.
+NAME = 'aim-webui'
+DESCRIPTION = 'Aim Web UI'
+VERSION = __version__
+
+
+def package_files(directory):
+    paths = []
+    for (path, _, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join(path, filename))
+    return paths
+
+
+ui_files = package_files('build')
+
+setup(
+    name=NAME,
+    version=__version__,
+    description=DESCRIPTION,
+    packages=["aim.web.ui"],
+    package_dir={'aim.web.ui': '.'},
+    package_data={'aim.web.ui': ui_files}
+)
