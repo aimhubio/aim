5ae8f81c3f868aa58b85ba6992f1a737dcb08117
Karo <44981312+KaroMourad@users.noreply.github.com>
2022-04-09 02:25:17 +0400

    [feat] Refactor formatValue function
     - Supporting to arrays, objects, buffers

---
+++ aim/web/ui/src/utils/encoder/bytes_repr.ts
@@ -0,0 +1,47 @@
+const HEXDIGITS = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+];
+
+export function bytes__repr__(arr: Uint8Array, quote?: string): string {
+  if (!quote) {
+    quote = '"';
+  }
+  let tokens = [];
+  tokens.push('b');
+  tokens.push(quote);
+  for (let c of arr) {
+    if (c == quote.charCodeAt(0) || c == 92) {
+      tokens.push('\\');
+      tokens.push(String.fromCharCode(c));
+    } else if (c == 9) {
+      tokens.push('\\t');
+    } else if (c == 10) {
+      tokens.push('\\n');
+    } else if (c == 13) {
+      tokens.push('\\r');
+    } else if (c < 32 || c > 0x7f) {
+      tokens.push('\\x');
+      tokens.push(HEXDIGITS[(c & 0xf0) >> 4]);
+      tokens.push(HEXDIGITS[c & 0x0f]);
+    } else {
+      tokens.push(String.fromCharCode(c));
+    }
+  }
+  tokens.push(quote);
+  return tokens.join('');
+}
--- aim/web/ui/src/utils/formatValue.ts
+++ aim/web/ui/src/utils/formatValue.ts
@@ -1,54 +1,79 @@
-export function formatValue(value: any, undefinedValue: any = '-') {
-  let formattedValue;
+import { bytes__repr__ } from './encoder/bytes_repr';
 
-  if (typeof value === 'number') {
-    formattedValue = replacer(value, undefinedValue);
-  } else if (
-    value === null ||
-    value === undefined ||
-    typeof value == 'boolean'
-  ) {
-    formattedValue = replacer(value, undefinedValue);
-  } else {
-    formattedValue = JSON.stringify(value, (key, node) =>
-      // TODO: remove replacer by implementing custom stringify method
-      replacer(node, undefinedValue),
-    );
+export function formatValue(value: any, undefinedValue: any = '-'): any {
+  let formatted = replacer(value, undefinedValue);
 
-    formattedValue
-      .replaceAll('"__None__"', 'None')
-      .replaceAll('"__True__"', 'True')
-      .replaceAll('"__False__"', 'False')
-      .replaceAll('__None__', 'None')
-      .replaceAll('__True__', 'True')
-      .replaceAll('__False__', 'False');
+  // stringify 'object' type
+  if (typeof formatted === 'object') {
+    formatted = JSON.stringify(formatted, (key, node) => {
+      if (typeof node === 'string') {
+        // remove double-strings from 'node'
+        return node.replaceAll('"', '');
+      }
+      return node;
+    });
   }
-
-  return formattedValue;
+  console.log('value: ', value, 'formatted: ', formatted);
+  return formatString(formatted);
 }
 
-function replacer(value: any, undefinedValue: any = '-') {
-  if (value === undefined) {
-    return undefinedValue;
+function replacer(value: any, undefinedValue: any = '-'): any {
+  switch (typeof value) {
+    case 'number':
+      if (isNaN(value)) {
+        return 'NaN';
+      }
+      if (!isFinite(value)) {
+        return value === Infinity ? 'Inf' : '-Inf';
+      }
+      return formatNumber(value);
+    case 'string':
+      return JSON.stringify(value);
+    case 'boolean':
+      return value ? 'True' : 'False';
+    case 'undefined':
+      return undefinedValue;
+    case 'object':
+      if (value === null) {
+        return 'None';
+      } else if (value instanceof ArrayBuffer) {
+        return bytes__repr__(new Uint8Array(value), "'");
+      } else if (Array.isArray(value)) {
+        const arr = [];
+        for (let v of [...value]) {
+          arr.push(replacer(v, undefinedValue));
+        }
+        return arr;
+      } else {
+        const obj: Record<string, unknown> = {};
+        for (let key of Object.keys({ ...value })) {
+          obj[key] = replacer(value[key], undefinedValue);
+        }
+        return obj;
+      }
+    default:
+      return value;
   }
-  if (value === null) {
-    return 'None';
-  }
-  if (value === true) {
-    return 'True';
-  }
-  if (value === false) {
-    return 'False';
-  }
-  if (typeof value === 'number') {
-    if (isNaN(value)) return undefinedValue;
-    return formatNumber(value);
-  }
-
-  return value;
 }
 
 // TODO: implement proper formatting for numeric values
-function formatNumber(value: number) {
+function formatNumber(value: number): number {
   return value;
+}
+
+const formatsToReplace: [string, string][] = [
+  ['"True"', 'True'],
+  ['"False"', 'False'],
+  ['"NaN"', 'NaN'],
+  ['"Inf"', 'Inf'],
+  ['"-Inf"', '-Inf'],
+];
+
+function formatString(formattedValue: any) {
+  if (typeof formattedValue === 'string') {
+    formatsToReplace.forEach((f) => {
+      formattedValue = formattedValue.replaceAll(f[0], f[1]);
+    });
+  }
+  return formattedValue;
 }
