410576ee4e941bad13264db86c71a65f201cf00f
Albert Torosyan <torosyanalbert@gmail.com>
2022-04-11 10:29:30 +0400

    [feat] Add path to create_blob methods

--- aim/sdk/objects/blob_utils.py
+++ aim/sdk/objects/blob_utils.py
@@ -7,14 +7,16 @@ from aim.storage.utils import BLOB, ExtBLOB
 USE_EXT_BLOBS = os.environ.get('__AIM_USE_EXTERNAL_BLOBS__')
 
 
-def _make_ext_blob(data: object, object_class: str = 'object'):
-    now_ = datetime.datetime.utcnow().strftime('%Y.%m.%d-%H:%M')
-    hash_ = uuid.uuid4().hex[:16]
-    ext_path = f'{object_class}/{now_}/{hash_}-{object_class}'
-    return ExtBLOB(data=data, ext_path=ext_path)
+def _make_ext_blob(data: object, object_class: str = 'object', path: str = None):
+    if not path:
+        now_ = datetime.datetime.utcnow().strftime('%Y.%m.%d-%H:%M')
+        hash_ = uuid.uuid4().hex[:16]
+        path = f'{object_class}/{now_}/{hash_}-{object_class}'
+
+    return ExtBLOB(data=data, ext_path=path)
 
 
-def _make_blob(data: object, object_class: str = 'object'):
+def _make_blob(data: object, object_class: str = 'object', path: str = None):
     return BLOB(data=data)
 
 
