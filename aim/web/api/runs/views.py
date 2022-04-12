231f4066628419fdce8cb1b6f3fcc0c64c477221
Arthur <48801049+devfox-se@users.noreply.github.com>
2022-04-12 14:08:41 +0400

    [fix] include notes into run props only for api call (#1648)

--- aim/web/api/runs/views.py
+++ aim/web/api/runs/views.py
@@ -106,6 +106,10 @@ async def run_params_api(run_id: str, sequence: Optional[Tuple[str, ...]] = Quer
         'traces': run.collect_sequence_info(sequence, skip_last_value=True),
         'props': get_run_props(run)
     }
+
+    response['props'].update({
+        'notes': [{'id': note.id} for note in run.props.notes_obj]
+    })
     return JSONResponse(response)
 
 
