5c460d0175986efff4fec78ae7273d0bdf202a5e
Karan Gupta <49341298+karan2801@users.noreply.github.com>
2022-06-02 13:04:47 +0530

    [feat] Add cloud storage backups for aim runs to aws s3
    * Added cloud storage backups to AWS for aim repo runs
    
    * Indendation Fixes
    
    * Added changes to Changelog.md
    
    * Modified cloud backup feature from only repo runs to whole project
    
    * Create aws s3 storage for .aim repo
    
    * Improve error logging for aim runs upload command
    
    * Comined enhancements in changelog
    
    * Updated docs to account for upload command
    
    * Updated docs with upload command

--- aim/cli/runs/utils.py
+++ aim/cli/runs/utils.py
@@ -1,7 +1,11 @@
 import fnmatch
 import os
 
-from typing import List
+from typing import List, Tuple
+import io
+import zipfile
+import boto3
+from datetime import datetime
 
 
 def list_repo_runs(repo_path: str) -> List[str]:
@@ -26,3 +30,37 @@ def match_runs(repo_path: str, hashes: List[str]) -> List[str]:
             matched_hashes.add(run_hash)
 
     return list(matched_hashes)
+
+
+def make_zip_archive(repo_path: str) -> io.BytesIO:
+    aim_dir = os.path.join(repo_path, '.aim')
+    zip_buf = io.BytesIO()
+    zipf = zipfile.ZipFile(zip_buf, 'w', zipfile.ZIP_DEFLATED)
+    len_dir_path = len(aim_dir)
+    for root, _, files in os.walk(aim_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            zipf.write(file_path, file_path[len_dir_path:])
+    zipf.close()
+    return zip_buf
+
+
+def upload_repo_runs(buffer: io.BytesIO, bucket_name: str)->Tuple[bool, str]:
+    try:
+        s3_client = boto3.client('s3')
+        buckets = s3_client.list_buckets()
+        bucket_names = []
+        for bucket in buckets['Buckets']:
+            bucket_names.append(bucket["Name"])
+        if bucket_name not in bucket_names:
+            bucket = s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            bucket = next((bucket for bucket in buckets['Buckets'] if bucket['Name'] == bucket_name), None)
+
+        key = f'aim-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.zip'
+        s3_client.upload_fileobj(buffer, bucket_name, key)
+        return True, key
+    except Exception as e:
+        return False, e
+
+       
