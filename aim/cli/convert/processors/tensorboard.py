5cc91657521353ecdbd5c8ad3b6c5dfd064d9ce5
Erik Arakelyan <erikarakelyan1997@gmail.com>
2022-06-07 22:26:08 +0400

    Updated to new tracker 3.9 -> 3.10

--- aim/cli/convert/processors/tensorboard.py
+++ aim/cli/convert/processors/tensorboard.py
@@ -228,7 +228,7 @@ def parse_tb_logs(tb_logs, repo_inst, flat=False, no_cache=False):
                             'step': step,
                             'timestamp': timestamp
                         }
-                        run._track_impl(track_val, timestamp, tag, step, context=event_context)
+                        run.track(track_val, tag, step, context=event_context)
 
                     if fail_count:
                         click.echo(f'Failed to process {fail_count} entries. First exception: {_err_info}', err=True)
