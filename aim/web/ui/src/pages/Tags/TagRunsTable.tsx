4cbd9b0857202077f44555b52171a926de53b95a
Vardges Hovhannisyan <43929589+VkoHov@users.noreply.github.com>
2022-06-13 19:30:26 +0400

    [feat] Add the name, status and duration of the run in the runs table of the tags page (#1858)

--- aim/web/ui/src/pages/Tags/TagRunsTable.tsx
+++ aim/web/ui/src/pages/Tags/TagRunsTable.tsx
@@ -1,42 +1,75 @@
 import React, { memo, useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import moment from 'moment';
+import _ from 'lodash-es';
+
+import { Tooltip } from '@material-ui/core';
 
 import Table from 'components/Table/Table';
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
+import StatusLabel from 'components/StatusLabel';
 
-import { ITagRun, ITagRunsTableProps } from 'types/pages/tags/Tags';
+import { TABLE_DATE_FORMAT } from 'config/dates/dates';
+
+import { ITagRun } from 'types/pages/tags/Tags';
+
+import { processDurationTime } from 'utils/processDurationTime';
 
 function TagRunsTable({
-  // eslint-disable-next-line react/prop-types
   runsList,
-}: ITagRunsTableProps): React.FunctionComponentElement<React.ReactNode> {
+}: {
+  runsList: ITagRun[];
+}): React.FunctionComponentElement<React.ReactNode> {
   const tableRef = useRef<any>({});
   const tableColumns = [
     {
-      dataKey: 'runs',
-      key: 'runs',
-      title: 'Runs',
-      width: 400,
+      dataKey: 'name',
+      key: 'name',
+      title: 'Name',
+      width: 0,
+      flexGrow: 1,
       cellRenderer: function cellRenderer({
         cellData,
       }: {
-        cellData: { name: string; id: string };
+        cellData: { name: string; id: string; active: boolean };
       }) {
         return (
-          <NavLink to={`/runs/${cellData.id}`}>
-            <p className='TagsTable__runName'>{cellData.name}</p>
-          </NavLink>
+          <>
+            <Tooltip title={cellData.active ? 'In Progress' : 'Finished'}>
+              <div>
+                <StatusLabel
+                  className='Table__status_indicator'
+                  status={cellData.active ? 'success' : 'alert'}
+                />
+              </div>
+            </Tooltip>
+            <Tooltip title={cellData.name}>
+              <div>
+                <NavLink to={`/runs/${cellData.id}`}>
+                  <p className='TagsTable__runName'>{cellData.name}</p>
+                </NavLink>
+              </div>
+            </Tooltip>
+          </>
         );
       },
     },
     {
-      dataKey: 'createdDate',
-      key: 'createdDate',
-      title: 'Created at',
-      width: 400,
+      dataKey: 'date',
+      key: 'date',
+      title: 'Date',
+      width: 200,
       cellRenderer: function cellRenderer({ cellData }: { cellData: string }) {
         return <p className='TagsTable__runCreatedDate'>{cellData}</p>;
+      },
+    },
+    {
+      dataKey: 'duration',
+      key: 'duration',
+      title: 'Duration',
+      width: 200,
+      cellRenderer: function cellRenderer({ cellData }: { cellData: string }) {
+        return <p className='TagsTable__runDuration'>{cellData}</p>;
       },
     },
   ];
@@ -46,8 +79,16 @@ function TagRunsTable({
       tableRef?.current?.updateData({
         // eslint-disable-next-line react/prop-types
         newData: runsList.map((run: ITagRun) => ({
-          runs: { name: run.run_id, id: run.run_id },
-          createdDate: moment(run.creation_time).format('DD-MM-YY HH:mm'),
+          name: {
+            name: run.name,
+            id: run.run_id,
+            active: _.isNil(run.end_time),
+          },
+          date: moment(run.creation_time * 1000).format(TABLE_DATE_FORMAT),
+          duration: processDurationTime(
+            run.creation_time * 1000,
+            run.end_time ? run.end_time * 1000 : Date.now(),
+          ),
           key: run.run_id,
         })),
         newColumns: tableColumns,
@@ -69,6 +110,7 @@ function TagRunsTable({
           rowHeight={32}
           headerHeight={32}
           height='calc(100% - 10px)'
+          disableRowClick
         />
       </div>
     </ErrorBoundary>
