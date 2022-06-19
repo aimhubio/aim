e31b4929ada4628c091819a67cbc94e79c54f7da
VkoHov <vkohovhannisyan96@gmail.com>
2022-06-19 21:59:24 +0400

    [fix] Seperate the active indicatore point filtering logic

--- aim/web/ui/src/utils/d3/drawLines.ts
+++ aim/web/ui/src/utils/d3/drawLines.ts
@@ -76,9 +76,11 @@ function drawLines(args: IDrawLinesArgs): void {
       .attr('d', lineGenerator(xScale, yScale, curveInterpolation));
 
     if (!readOnly) {
+      const filterdData =
+        data?.filter((d: IProcessedData) => d?.run?.props?.active) ?? [];
       linesNodeRef.current
         ?.selectAll('.inProgressLineIndicator')
-        .data(data.filter((d: IProcessedData) => d?.run?.props?.active))
+        .data(filterdData)
         .join('circle')
         .attr('class', 'inProgressLineIndicator')
         .attr('id', (d: IProcessedData) => `inProgressLineIndicator-${d.key}`)
