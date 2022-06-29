218a4a3463c7f9bd3c59221f02fbd22ab2c89e56
Arsen Alkamyan <alkamyan97@gmail.com>
2022-06-30 01:59:33 +0400

    [refactor] Refactor Icon.scss and ProgressBar files

--- aim/web/ui/src/components/ProgressBar/ProgressBar.tsx
+++ aim/web/ui/src/components/ProgressBar/ProgressBar.tsx
@@ -73,7 +73,6 @@ function ProgressBar({
             <Text
               className='ProgressBar__container__info__matched'
               size={14}
-              color='success'
               weight={600}
             >
               {matched} matched run(s)
