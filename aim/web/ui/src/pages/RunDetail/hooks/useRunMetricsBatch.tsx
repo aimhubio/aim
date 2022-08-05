93330c3c61139e25b3da964d2ad2de7ff2429e47
Ruben Karapetyan <karapetyanrouben@gmail.com>
2022-08-05 17:15:05 +0400

    Remove unused items

--- aim/web/ui/src/pages/RunDetail/hooks/useRunMetricsBatch.tsx
+++ aim/web/ui/src/pages/RunDetail/hooks/useRunMetricsBatch.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import _ from 'lodash-es';
 
 import runDetailAppModel from 'services/models/runs/runDetailAppModel';
 
-function useRunMetricsBatch({ runBatch, runTraces, runHash }: any) {
+function useRunMetricsBatch({ runTraces, runHash }: any) {
   React.useEffect(() => {
     const runsBatchRequestRef = runDetailAppModel.getRunMetricsBatch(
       runTraces.metric,
