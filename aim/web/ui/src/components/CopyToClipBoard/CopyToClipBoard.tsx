5a9049f8bfdfa1e9edeb583399be4434302d1e74
Arsen Alkamyan <alkamyan97@gmail.com>
2022-08-18 17:40:50 +0400

    [feat] Add useCopy custom hook and onCopyToClipBord helper function

--- aim/web/ui/src/components/CopyToClipBoard/CopyToClipBoard.tsx
+++ aim/web/ui/src/components/CopyToClipBoard/CopyToClipBoard.tsx
@@ -5,6 +5,8 @@ import { Tooltip } from '@material-ui/core';
 import { Button, Icon } from 'components/kit';
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 
+import { useCopy } from 'hooks/useCopy/useCopy';
+
 import { ICopyToClipBoardProps } from 'types/components/CopyToClipBoard/CopyToClipBoard';
 
 function CopyToClipboard({
@@ -12,54 +14,23 @@ function CopyToClipboard({
   showSuccessDelay = 1500,
   className = '',
 }: ICopyToClipBoardProps): React.FunctionComponentElement<ICopyToClipBoardProps> {
-  const [showCopiedIcon, setShowCopiedIcon] = React.useState<boolean>(false);
+  const { onCopy, copied, setCopied } = useCopy(contentRef);
 
   React.useEffect(() => {
-    if (showCopiedIcon) {
+    if (copied) {
       setTimeout(() => {
-        setShowCopiedIcon(false);
+        setCopied(false);
       }, showSuccessDelay);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [showCopiedIcon]);
-
-  const onCopy = React.useCallback(() => {
-    const text: string = contentRef.current?.innerText?.trim('');
-    if (text && !showCopiedIcon) {
-      if (navigator.clipboard && window.isSecureContext) {
-        navigator.clipboard
-          ?.writeText(text)
-          ?.then(function () {
-            setShowCopiedIcon(true);
-          })
-          ?.catch();
-      } else {
-        const textArea: HTMLTextAreaElement =
-          document.createElement('textarea');
-        textArea.value = text;
-
-        // make the textarea out of viewport
-        textArea.style.position = 'fixed';
-        textArea.style.left = '-400vw';
-        textArea.style.top = '-400vh';
-        document.body.appendChild(textArea);
-        textArea.focus();
-        textArea.select();
-        try {
-          if (document.execCommand('copy')) {
-            setShowCopiedIcon(true);
-          }
-        } catch (err) {}
-      }
-    }
-  }, [contentRef, showCopiedIcon]);
+  }, [copied]);
 
   return (
     <ErrorBoundary>
-      <Tooltip title={showCopiedIcon ? 'Copied!' : 'Copy to clipboard'}>
+      <Tooltip title={copied ? 'Copied!' : 'Copy to clipboard'}>
         <span className={className} onClick={onCopy}>
           <Button withOnlyIcon color='secondary' size='medium'>
-            {showCopiedIcon ? <Icon name='check' /> : <Icon name='copy' />}
+            {copied ? <Icon name='check' /> : <Icon name='copy' />}
           </Button>
         </span>
       </Tooltip>
