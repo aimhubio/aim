0a2f5aceaa0a3a7e063330f5a48ffcb9f41096a0
Arsen Alkamyan <44676488+arsengit@users.noreply.github.com>
2022-09-06 18:53:13 +0400

    [feat] Add quick links to explore runs (#2140)
    * [feat] Add quick links to explore runs
    
    * [feat] Add ListItem component
    
    * [feat] Add types for ListItem component

--- aim/web/ui/src/pages/Home/components/HomeLeft/HomeLeft.tsx
+++ aim/web/ui/src/pages/Home/components/HomeLeft/HomeLeft.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { Text } from 'components/kit';
+
+import QuickLinks from './QuickLinks/QuickLinks';
+
 import './HomeLeft.scss';
 
 function HomeLeft(): React.FunctionComponentElement<React.ReactNode> {
-  return <aside className='HomeLeft'>left side</aside>;
+  return (
+    <aside className='HomeLeft'>
+      <Text className='HomeLeft__title' tint={100} size={24} weight={600}>
+        Explore
+      </Text>
+      <QuickLinks />
+    </aside>
+  );
 }
 
 export default HomeLeft;
