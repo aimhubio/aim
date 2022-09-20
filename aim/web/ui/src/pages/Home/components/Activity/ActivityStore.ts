1d18ba2a47285e36d15fc6b33237ae78616432df
Arsen Alkamyan <alkamyan97@gmail.com>
2022-09-20 19:39:51 +0400

    [feat] Rename activity to projectContributions

--- aim/web/ui/src/pages/Home/components/Activity/ActivityStore.ts
+++
@@ -1,9 +0,0 @@
-import { getActivity, GetActivityResult } from 'modules/core/api/projectApi';
-import createResource from 'modules/core/utils/createResource';
-
-function createActivityEngine() {
-  const { fetchData, state } = createResource<GetActivityResult>(getActivity);
-  return { fetchActivity: fetchData, activityState: state };
-}
-
-export default createActivityEngine();
--- aim/web/ui/src/pages/Home/components/ProjectContributions/ProjectContributions.tsx
+++ aim/web/ui/src/pages/Home/components/ProjectContributions/ProjectContributions.tsx
@@ -10,16 +10,12 @@ import { ANALYTICS_EVENT_KEYS } from 'config/analytics/analyticsKeysMap';
 
 import { trackEvent } from 'services/analytics';
 
-import { IActivityProps } from 'types/pages/home/components/Activity/Activity';
+import useProjectContributions from './useProjectContributions';
 
-import useActivity from './useActivity';
+import './ProjectContributions.scss';
 
-import './Activity.scss';
-
-function Activity({
-  activityData,
-}: IActivityProps): React.FunctionComponentElement<React.ReactNode> {
-  const { activityStore } = useActivity();
+function ProjectContributions(): React.FunctionComponentElement<React.ReactNode> {
+  const { projectContributionsStore } = useProjectContributions();
   function shiftDate(date: any, numDays: any) {
     const newDate = new Date(date);
     newDate.setDate(newDate.getDate() + numDays);
@@ -28,28 +24,28 @@ function Activity({
   let today = new Date();
   return (
     <ErrorBoundary>
-      <Grid className='Activity' container spacing={1}>
+      <Grid className='ProjectContributions' container spacing={1}>
         <Grid item>
           <Text component='h2' size={24} weight={600} tint={100}>
             Statistics
           </Text>
-          <div className='Activity__Statistics__card'>
+          <div className='ProjectContributions__Statistics__card'>
             <Text size={16} component='span' color='secondary'>
               Experiments
             </Text>
             <Text component='strong' size={36} weight={600} color='secondary'>
-              {activityData?.num_experiments ?? (
-                <CircularProgress className='Activity__loader' />
+              {projectContributionsStore.data?.num_experiments ?? (
+                <CircularProgress className='ProjectContributions__loader' />
               )}
             </Text>
           </div>
-          <div className='Activity__Statistics__card'>
+          <div className='ProjectContribution__Statistics__card'>
             <Text size={16} component='span' color='secondary'>
               Runs
             </Text>
             <Text component='strong' size={36} weight={600} color='secondary'>
-              {activityStore.data?.num_runs ?? (
-                <CircularProgress className='Activity__loader' />
+              {projectContributionsStore.data?.num_runs ?? (
+                <CircularProgress className='ProjectContributions__loader' />
               )}
             </Text>
           </div>
@@ -58,16 +54,18 @@ function Activity({
           <Text component='h2' size={24} weight={600} tint={100}>
             Activity
           </Text>
-          <div className='Activity__HeatMap'>
+          <div className='ProjectContributions__HeatMap'>
             <HeatMap
               startDate={shiftDate(today, -10 * 30)}
               endDate={today}
               onCellClick={() => {
                 trackEvent(ANALYTICS_EVENT_KEYS.home.activityCellClick);
               }}
-              data={Object.keys(activityData?.activity_map ?? {}).map((k) => [
+              data={Object.keys(
+                projectContributionsStore.data?.activity_map ?? {},
+              ).map((k) => [
                 new Date(k),
-                activityData.activity_map[k],
+                projectContributionsStore.data?.activity_map[k],
               ])}
             />
           </div>
@@ -76,4 +74,4 @@ function Activity({
     </ErrorBoundary>
   );
 }
-export default React.memo(Activity);
+export default React.memo(ProjectContributions);
