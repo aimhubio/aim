a3fd3eb09d4a6ae87e0a819f78fe739511ca4769
Ruben Karapetyan <karapetyanrouben@gmail.com>
2022-09-20 11:55:49 +0400

    [feat] Process data with python in browser

--- aim/web/ui/src/pages/CustomExplorer/ui.tsx
+++ aim/web/ui/src/pages/CustomExplorer/ui.tsx
@@ -2,15 +2,12 @@
 import * as React from 'react';
 
 import { IQueryFormProps, IUIConfig } from 'modules/BaseExplorer/types';
-import {
-  Grouping,
-  QueryForm,
-  Visualizer,
-} from 'modules/BaseExplorer/components';
+import { Grouping, QueryForm } from 'modules/BaseExplorer/components';
 import Controls from 'modules/BaseExplorer/components/Controls';
 
-import { AlignmentOptionsEnum } from 'utils/d3';
 import { filterMetricsData } from 'utils/filterMetricData';
+import { AlignmentOptionsEnum } from 'utils/d3';
+import { encode } from 'utils/encoder/encoder';
 
 const ui: IUIConfig = {
   defaultBoxConfig: {
@@ -22,70 +19,136 @@ const ui: IUIConfig = {
     queryForm: React.memo((props: IQueryFormProps) => (
       <QueryForm engine={props.engine} hasAdvancedMode />
     )),
-    visualizations: [Visualizer],
-    grouping: Grouping,
-    box: function CustomeMetricVisualizer(props: any) {
-      let data = props.data.data;
-      let element = React.useRef<HTMLIFrameElement>(null);
-
-      const { values, steps } = filterMetricsData(
-        data,
-        AlignmentOptionsEnum.STEP,
-      );
-
-      React.useEffect(() => {
-        if (element.current) {
-          let doc = element.current.contentWindow!.document;
-          doc.open();
-          doc.write(
-            `
-            <html>
-            <head>
-              <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
-              <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
-              <py-env>
-                - numpy
-                - matplotlib
-              </py-env>
-            </head>
-            <body>
-              <div id="plot"></div>
-              <py-script output="plot">
-                import matplotlib.pyplot as plt
-                import numpy as np
-          
-                x = np.random.randn(1000)
-                y = np.random.randn(1000)
-          
-                fig = plt.figure()
-                ax = fig.add_subplot(1, 1, 1)
-                ax.plot([${steps}], [${values}], color='tab:blue')
-                fig
-              </py-script>
-            </body>
-            </html>
-            `,
+    visualizations: [
+      function CustomVisualizer(props: any) {
+        let element = React.useRef<HTMLIFrameElement>(null);
+        const {
+          engine: { useStore, dataSelector },
+        } = props;
+        const data = useStore(dataSelector)?.map((item: any) => {
+          const { values, steps, epochs, timestamps } = filterMetricsData(
+            item.data,
+            AlignmentOptionsEnum.STEP,
           );
-          doc.close();
-        }
-      }, []);
+          return {
+            name: item.data.name,
+            context: item.data.context,
+            values,
+            steps,
+            epochs,
+            timestamps,
+          };
+        });
 
-      return (
-        <div
-          style={{
-            width: '100%',
-            height: '100%',
-            position: 'relative',
-          }}
-        >
-          <iframe
-            ref={element}
-            src='about:blank'
-            style={{ width: '100%', height: '100%', border: 'none' }}
-          />
-        </div>
-      );
-    },
+        React.useEffect(() => {
+          if (element.current && data && data.length > 0) {
+            const metrics = JSON.stringify(data);
+            let doc = element.current.contentWindow!.document;
+            doc.open();
+            doc.write(
+              `
+              <html>
+              <head>
+                <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
+                <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+                <style>
+                  body {
+                    background-color: #eee;
+                    height: 100%;
+                  }
+                  .container {
+                    height: 100%;
+                    display: flex;
+                  }
+                  .box {
+                    background-color: #fff;
+                    border: 1px solid #ccc;
+                    padding: 10px;
+                    margin: 10px 5px;
+                    overflow: auto;
+                  }
+                  .repl {
+                    flex: 1;
+                  }
+                  .result {
+                    display: flex;
+                    flex-direction: column;
+                    flex: 1;
+                  }
+                  .output {
+                    flex: 0.65;
+                  }
+                  .errors {
+                    flex: 0.35;
+                    max-height: 300px;
+                  }
+                  #output, #errors {
+                    height: 70%;
+                  }
+                </style>
+                <py-env>
+                  - numpy
+                  - matplotlib
+                </py-env>
+              </head>
+              <body>
+                <py-script>
+                  import js
+                  import json
+                  metrics = json.loads('${metrics}')
+                </py-script>
+                <div class="container">
+                  <div class="repl box">
+                    <py-repl std-out="output" std-err="errors">
+                      import matplotlib.pyplot as plt
+                      
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+for i, metric in enumerate(metrics):
+  color = (i / 10 % 1, i / 30 % 1, i / 50 % 1)
+  ax.plot(list(metric["steps"].values()), list(metric["values"].values()), color=color)
+fig
+                    </py-repl>
+                  </div>
+                  <div class="result">
+                    <div class="output box">
+                      <b>Output</b><hr>
+                      <div id="output"></div>
+                    </div>
+                    <div class="errors box">
+                      <b>Errors and Warnings</b><hr>
+                      <div id="errors"></div>
+                    </div>
+                  </div>
+                </div>
+              </body>
+              </html>
+              `,
+            );
+            doc.close();
+          }
+        }, [data]);
+
+        return (
+          <div
+            style={{
+              width: '100%',
+              height: '100%',
+              position: 'relative',
+            }}
+          >
+            <iframe
+              key={encode(data)}
+              ref={element}
+              src='about:blank'
+              style={{ width: '100%', height: '100%', border: 'none' }}
+            />
+          </div>
+        );
+      },
+    ],
+    grouping: Grouping,
+    box: () => null,
     controls: Controls,
   },
 };
