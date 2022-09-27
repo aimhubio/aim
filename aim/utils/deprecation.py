ab032dc84bc5c913547c4b478f2ed095ef03ebcf
Albert Torosyan <torosyanalbert@gmail.com>
2022-09-27 12:00:13 +0400

    [deprecation] Mark Python 3.6 as deprecated

--- aim/utils/deprecation.py
+++ aim/utils/deprecation.py
@@ -3,5 +3,13 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def python_version_deprecation_check():
+    import sys
+    version_info = sys.version_info
+    if version_info.major == 3:
+        deprecation_warning(remove_version='3.16', msg='Python 3.6 has reached EOL. '
+                                                       'Aim support for Python 3.6 is deprecated!')
+
+
 def deprecation_warning(*, remove_version: str, msg: str):
-    logger.warning(f'{msg} This functionality will be removed in version {remove_version}.')
+    logger.warning(f'{msg} This functionality will be removed in Aim version {remove_version}.')
