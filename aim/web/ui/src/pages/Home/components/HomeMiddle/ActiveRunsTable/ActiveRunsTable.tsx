9cce049cb6e86fd97feefb3ce79170e4a7214aee
Ruben Karapetyan <karapetyanrouben@gmail.com>
2022-09-29 22:09:52 +0400

    Add spinner for table loading state

--- aim/web/ui/src/pages/Home/components/HomeMiddle/ActiveRunsTable/ActiveRunsTable.tsx
+++ aim/web/ui/src/pages/Home/components/HomeMiddle/ActiveRunsTable/ActiveRunsTable.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 
-import { Text } from 'components/kit';
+import { Spinner, Text } from 'components/kit';
 import Table from 'components/Table/Table';
 
 import { RowHeightSize } from 'config/table/tableConfigs';
@@ -49,31 +49,36 @@ function ActiveRunsTable() {
       </div>
       <div
         className={classNames('ActiveRunsTable__table', {
+          'ActiveRunsTable__table--loading': loading,
           'ActiveRunsTable__table--empty': tableData.length === 0,
         })}
       >
-        <Table
-          custom
-          topHeader
-          multiSelect
-          noColumnActions
-          hideHeaderActions
-          showRowClickBehaviour={false}
-          showResizeContainerActionBar={false}
-          appName={AppNameEnum.RUNS} // @TODO: change to Dashboard
-          ref={tableRef}
-          columns={tableColumns}
-          data={tableData}
-          isLoading={loading}
-          height='100%'
-          rowHeight={RowHeightSize.sm}
-          illustrationConfig={{
-            size: 'large',
-            title: 'No active runs',
-          }}
-          selectedRows={selectedRows}
-          onRowSelect={onRowSelect}
-        />
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Table
+            custom
+            topHeader
+            multiSelect
+            noColumnActions
+            hideHeaderActions
+            showRowClickBehaviour={false}
+            showResizeContainerActionBar={false}
+            appName={AppNameEnum.RUNS} // @TODO: change to Dashboard
+            ref={tableRef}
+            columns={tableColumns}
+            data={tableData}
+            isLoading={loading}
+            height='100%'
+            rowHeight={RowHeightSize.sm}
+            illustrationConfig={{
+              size: 'medium',
+              title: 'No active runs',
+            }}
+            selectedRows={selectedRows}
+            onRowSelect={onRowSelect}
+          />
+        )}
       </div>
     </div>
   );
