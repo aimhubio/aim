783d9c3eadfe4cd4f813ddcec84f15efe647ca38
Karo <44981312+KaroMourad@users.noreply.github.com>
2022-09-30 20:23:54 +0400

    [feat] Add ability to navigate to the explorers by statistic cards

--- aim/web/ui/src/components/StatisticsCard/StatisticsCard.tsx
+++ aim/web/ui/src/components/StatisticsCard/StatisticsCard.tsx
@@ -1,66 +1,78 @@
 import * as React from 'react';
 import classNames from 'classnames';
+import { useHistory } from 'react-router-dom';
+
+import { Tooltip } from '@material-ui/core';
 
 import { Icon, Text } from 'components/kit';
 
 import hexToRgbA from 'utils/hexToRgbA';
 
-import { IStatisticsCardProps } from '.';
+import { IStatisticsCardProps } from './index';
 
 import './StatisticsCard.scss';
 
 function StatisticsCard({
   label,
+  title,
   count,
   icon,
   iconBgColor = '#fff',
   cardBgColor = hexToRgbA(iconBgColor, 0.1),
   onMouseOver,
   onMouseLeave,
+  navLink,
   highlighted,
+  disabled = false,
 }: IStatisticsCardProps) {
+  const history = useHistory();
   const onSafeMouseOver = React.useCallback(
     (id: string) => {
-      if (typeof onMouseOver === 'function') {
+      if (!disabled && typeof onMouseOver === 'function') {
         onMouseOver(id);
       }
     },
     [onMouseOver],
   );
   return (
-    <div
-      onMouseLeave={onMouseLeave}
-      onMouseOver={() => onSafeMouseOver(label)}
-      className={classNames('StatisticsCard', { highlighted })}
-      style={{ backgroundColor: cardBgColor }}
-    >
-      {icon && (
-        <div
-          className='StatisticsCard__iconWrapper'
-          style={{ backgroundColor: iconBgColor }}
-        >
-          <Icon className='StatisticsCard__iconWrapper__icon' name={icon} />
+    <Tooltip title={title || ''}>
+      <div
+        onClick={() => !disabled && navLink && history.push(navLink)}
+        onMouseLeave={onMouseLeave}
+        onMouseOver={() => onSafeMouseOver(label)}
+        className={classNames('StatisticsCard', {
+          highlighted: navLink && highlighted,
+        })}
+        style={{
+          backgroundColor: navLink && highlighted ? iconBgColor : cardBgColor,
+        }}
+      >
+        {icon && (
+          <div
+            className='StatisticsCard__iconWrapper'
+            style={{
+              backgroundColor: navLink && highlighted ? '#fff' : iconBgColor,
+            }}
+          >
+            <Icon className='StatisticsCard__iconWrapper__icon' name={icon} />
+          </div>
+        )}
+        <div className='StatisticsCard__info'>
+          <Text className='StatisticsCard__info__label' size={10} weight={600}>
+            {label}
+          </Text>
+          <Tooltip title={`${count}`}>
+            <Text
+              className='StatisticsCard__info__count'
+              size={16}
+              weight={600}
+            >
+              {count}
+            </Text>
+          </Tooltip>
         </div>
-      )}
-      <div className='StatisticsCard__info'>
-        <Text
-          className='StatisticsCard__info__label'
-          size={10}
-          weight={600}
-          title={`${label}`}
-        >
-          {label}
-        </Text>
-        <Text
-          className='StatisticsCard__info__count'
-          size={16}
-          weight={600}
-          title={`${count}`}
-        >
-          {count}
-        </Text>
       </div>
-    </div>
+    </Tooltip>
   );
 }
 
--- aim/web/ui/src/pages/Home/components/ProjectStatistics/ProjectStatistics.d.ts
+++ aim/web/ui/src/pages/Home/components/ProjectStatistics/ProjectStatistics.d.ts
@@ -5,4 +5,6 @@ export interface IProjectStatistic {
   count: number;
   iconBgColor: string;
   icon?: IconName;
+  navLink?: string;
+  title?: string;
 }
