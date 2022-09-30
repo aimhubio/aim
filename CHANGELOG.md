9edaea4d01be62f28a30148bbb50b0d1ea9b29da
Albert Torosyan <32957250+alberttorosyan@users.noreply.github.com>
2022-09-30 14:46:10 +0400

    [deprecation] Mark Python 3.6 as deprecated (#2203)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@
 - Replace the spinner loader with a lighter one (VkoHov)
 - Add fast.ai integration (tmynn)
 - Add command for dangling params cleanup (mihran113)
+- Deprecate Python 3.6 (alberttorosyan)
 
 ### Fixes:
 
--- aim/__init__.py
+++ aim/__init__.py
@@ -2,3 +2,7 @@ import aimrocks
 from aim.sdk import *
 from aim.ext.notebook.notebook import load_ipython_extension
 from aim.cli.manager.manager import run_process
+
+from aim.utils.deprecation import python_version_deprecation_check
+
+python_version_deprecation_check()
