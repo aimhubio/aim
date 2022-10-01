e61aeb6a8ef7f37b3d7cb2b9e9358f4af125abb9
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 06:10:24 +0000

    Adding tarfile member sanitization to extractall()

--- aim/sdk/utils.py
+++ aim/sdk/utils.py
@@ -133,4 +133,23 @@ def restore_run_backup(repo, run_hash):
         shutil.rmtree(f'meta/chunks/{run_hash}', ignore_errors=True)
         shutil.rmtree(f'seqs/chunks/{run_hash}', ignore_errors=True)
         with tarfile.open(run_bcp_file, 'r:gz') as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar)
