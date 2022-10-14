21a48703331cc93549d6534af8442a14a3444138
Arsen Alkamyan <alkamyan97@gmail.com>
2022-10-14 17:15:25 +0400

    [fix] Move types dependencies to dev dependency

--- aim/web/ui/.storybook/main.js
+++ aim/web/ui/.storybook/main.js
@@ -1,13 +1,10 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+};
