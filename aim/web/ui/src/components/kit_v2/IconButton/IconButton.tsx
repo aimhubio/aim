97e1fe6c747c94dd5953bf5e485b06ac89811b31
Arsen Alkamyan <alkamyan97@gmail.com>
2022-10-20 20:25:32 +0400

    [feat] Add IconButton component

---
+++ aim/web/ui/src/components/kit_v2/IconButton/IconButton.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import Icon from 'components/kit/Icon';
+
+import { styled } from 'config/stitches/stitches.config';
+
+import { getButtonStyles } from '../utils/getButtonStyles';
+
+import { IIconButtonProps } from './IconButton.d';
+
+const Container = styled('button', {
+  all: 'unset',
+  display: 'inline-flex',
+  width: 'fit-content',
+  ai: 'center',
+  jc: 'center',
+  fontWeight: '$2',
+  cursor: 'pointer',
+  borderRadius: '$1',
+  transition: 'all 0.2s ease-in-out',
+  fontSize: '$2',
+  variants: {
+    size: {
+      medium: {
+        size: '$1',
+        fontSize: '$2',
+      },
+      large: {
+        size: '$2',
+        fontSize: '$3',
+      },
+      xLarge: {
+        size: '$3',
+        fontSize: '$3',
+      },
+    },
+    variant: {
+      contained: {},
+      outlined: {},
+      text: {},
+    },
+  },
+});
+
+function IconButton({
+  icon,
+  size = 'medium',
+  color = 'primary',
+  variant = 'contained',
+  disabled = false,
+  ...props
+}: IIconButtonProps) {
+  return (
+    <Container
+      {...props}
+      css={{ ...getButtonStyles(color, variant, disabled) }}
+      size={size}
+      variant={variant}
+    >
+      <Icon name={icon} />
+    </Container>
+  );
+}
+
+export default React.memo(IconButton);
