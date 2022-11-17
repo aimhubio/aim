9f5a6b4ee2015b538050c93b9fb5718b82fbccbb
Albert Torosyan <torosyanalbert@gmail.com>
2022-11-17 21:32:11 +0400

    [feat] Implement LogRecord hash to deduplicate messages

--- aim/sdk/logging/log_record.py
+++ aim/sdk/logging/log_record.py
@@ -1,8 +1,11 @@
 import time
 
+from inspect import getframeinfo, currentframe
 from typing import Optional, Union, Tuple
 
 from aim.storage.object import CustomObject
+from aim.storage.hashing import hash_auto
+
 from aim.sdk.sequence import Sequence
 
 
@@ -10,13 +13,23 @@ from aim.sdk.sequence import Sequence
 class LogRecord(CustomObject):
     AIM_NAME = 'aim.log_record'
 
-    def __init__(self, message: str, level: int, timestamp: Optional[float] = None, **kwargs):
+    def __init__(self,
+                 message: str,
+                 level: int,
+                 timestamp: Optional[float] = None,
+                 logger_info: Optional[Tuple[str, int]] = None,
+                 **kwargs):
         super().__init__()
         self.storage['message'] = message
         self.storage['log_level'] = level
         self.storage['timestamp'] = timestamp or time.time()
         for k, v in kwargs:
             self.storage['extra_args'].set(k, v, strict=False)
+
+        if not logger_info:
+            frame_info = getframeinfo(currentframe().f_back)
+            logger_info = (frame_info.filename, frame_info.lineno)
+        self.storage['__logger_info'] = logger_info
 
     @property
     def message(self):
@@ -25,6 +38,9 @@ class LogRecord(CustomObject):
     @property
     def level(self):
         return self.storage['log_level']
+
+    def __hash__(self):
+        return hash_auto((self.storage['__logger_info'], self.storage['log_level']))
 
 
 class LogRecords(Sequence):
