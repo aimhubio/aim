7e7d07413f5ac64765d90af41060fe9527a8df76
Karo <44981312+KaroMourad@users.noreply.github.com>
2022-12-08 18:13:31 +0400

    [feat] Add warning and info icons and replace old icons

--- aim/web/ui/src/modules/BaseExplorer/components/ExplorerNotifications/config.tsx
+++ aim/web/ui/src/modules/BaseExplorer/components/ExplorerNotifications/config.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import * as React from 'react';
 
-import { ReactComponent as SuccessIcon } from 'assets/icons/successIcon.svg';
-import { ReactComponent as ErrorIcon } from 'assets/icons/errorIcon.svg';
+import { ReactComponent as SuccessIcon } from 'assets/icons/notifications/success.svg';
+import { ReactComponent as ErrorIcon } from 'assets/icons/notifications/error.svg';
+import { ReactComponent as InfoIcon } from 'assets/icons/notifications/info.svg';
+import { ReactComponent as WarningIcon } from 'assets/icons/notifications/warning.svg';
 
 const NotificationIcons: Record<string, React.ReactNode> = {
   success: <SuccessIcon />,
   error: <ErrorIcon />,
-  warning: null, // @TODO: add warning icon
-  info: null, // @TODO: add info icon
+  warning: <WarningIcon />,
+  info: <InfoIcon />,
 };
 
 export const getNotificationIcon = (iconName: string) => {
