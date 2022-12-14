e573f2029306f3fff529e883b16480a89ff48250
Arsen Alkamyan <alkamyan97@gmail.com>
2022-12-14 20:59:29 +0400

    [fix] Remove TreeSelect component

--- aim/web/ui/src/pages/Tags/Tags.tsx
+++ aim/web/ui/src/pages/Tags/Tags.tsx
@@ -1,15 +1,10 @@
 import React, { memo, useEffect, useState } from 'react';
-import { Select } from 'antd';
 
 import { Paper, Tab, Tabs } from '@material-ui/core';
 
 import TabPanel from 'components/TabPanel/TabPanel';
 import NotificationContainer from 'components/NotificationContainer/NotificationContainer';
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
-import TreeSelect from 'components/kit_v2/TreeSelect/TreeSelect';
-import Tree from 'components/kit_v2/Tree/Tree';
-import Popover from 'components/kit_v2/Popover';
-import Button from 'components/kit_v2/Button';
 
 import { ANALYTICS_EVENT_KEYS } from 'config/analytics/analyticsKeysMap';
 
@@ -20,25 +15,7 @@ import { ITagsProps } from 'types/pages/tags/Tags';
 import TagsList from './TagsList';
 
 import './Tags.scss';
-const dig = (path = '0', level = 3) => {
-  const list = [];
-  for (let i = 0; i < 10; i += 1) {
-    const key = `${path}-${i}`;
-    const treeNode: any = {
-      title: key,
-      key,
-    };
 
-    if (level > 0) {
-      treeNode.children = dig(key, level - 1);
-    }
-
-    list.push(treeNode);
-  }
-  return list;
-};
-
-const treeData = dig();
 function Tags({
   tagsListData,
   isTagsDataLoading,
@@ -67,13 +44,9 @@ function Tags({
     setTagsList(tagsListData?.filter((tag) => !tag.archived) || []);
   }, [tagsListData]);
 
-  console.log(treeData);
   return (
     <ErrorBoundary>
-      <div style={{ margin: 100 }}>
-        <TreeSelect />
-      </div>
-      {/* <section className='Tags container'>
+      <section className='Tags container'>
         <Paper className='Tags__tabsContainer'>
           <Tabs
             value={value}
@@ -117,14 +90,7 @@ function Tags({
             data={notifyData}
           />
         )}
-      </section> */}
-
-      <div style={{ margin: 200 }}>
-        <Popover
-          trigger={<Button>Open Tree</Button>}
-          content={<Tree data={treeData} height={300} checkable />}
-        />
-      </div>
+      </section>
     </ErrorBoundary>
   );
 }
