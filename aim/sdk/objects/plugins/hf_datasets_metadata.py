e8c11fe7b6ec8cbdef58ebb34409bd5c13129e42
tmynn <hovhannes.tamoyan@gmail.com>
2023-01-11 14:59:16 +0000

    [fix] Make get value actions safe

--- aim/sdk/objects/plugins/hf_datasets_metadata.py
+++ aim/sdk/objects/plugins/hf_datasets_metadata.py
@@ -1,5 +1,8 @@
 from datasets import DatasetDict
 from aim.storage.object import CustomObject
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 
 @CustomObject.alias("hf_datasets.metadata")
@@ -17,35 +20,53 @@ class HFDatasetsData(CustomObject):
         dataset_info = vars(dataset[list(dataset.keys())[0]]._info)
 
         return {
-            "description": dataset_info["description"],
-            "citation": dataset_info["citation"],
-            "homepage": dataset_info["homepage"],
-            "license": dataset_info["license"],
-            "features": [
-                {feature: str(dataset_info["features"][feature])}
-                for feature in dataset_info["features"].keys()
-            ],
-            "post_processed": str(dataset_info["post_processed"]),
-            "supervised_keys": str(dataset_info["supervised_keys"]),
-            "task_templates": [
-                str(template) for template in dataset_info["task_templates"]
-            ],
-            "builder_name": dataset_info["builder_name"],
-            "config_name": dataset_info["config_name"],
-            "version": str(dataset_info["version"]),
-            "splits": [
+            "description": dataset_info.get("description"),
+            "citation": dataset_info.get("citation"),
+            "homepage": dataset_info.get("homepage"),
+            "license": dataset_info.get("license"),
+            "features": self._get_features(dataset_info),
+            "post_processed": str(dataset_info.get("post_processed")),
+            "supervised_keys": str(dataset_info.get("supervised_keys")),
+            "task_templates": self._get_task_templates(dataset_info),
+            "builder_name": dataset_info.get("builder_name"),
+            "config_name": dataset_info.get("config_name"),
+            "version": str(dataset_info.get("version")),
+            "splits": self._get_splits(dataset_info),
+            "download_checksums": dataset_info.get("download_checksums"),
+            "download_size": dataset_info.get("download_size"),
+            "post_processing_size": dataset_info.get("post_processing_size"),
+            "dataset_size": dataset_info.get("dataset_size"),
+            "size_in_bytes": dataset_info.get("size_in_bytes"),
+        }
+
+    def _get_features(self, dataset_info):
+        try:
+            return [
+                {feature: str(dataset_info.get("features")[feature])}
+                for feature in dataset_info.get("features").keys()
+            ]
+        except LookupError:
+            logger.warning(f'Failed to get features information')
+
+    def _get_task_templates(self, dataset_info):
+        try:
+            return [
+                str(template) for template in dataset_info.get("task_templates")
+            ]
+        except LookupError:
+            logger.warning(f'Failed to get task templates information')
+
+    def _get_splits(self, dataset_info):
+        try:
+            return [
                 {
                     subset: {
-                        "num_bytes": dataset_info["splits"][subset].num_bytes,
-                        "num_examples": dataset_info["splits"][subset].num_examples,
-                        "dataset_name": dataset_info["splits"][subset].dataset_name,
+                        "num_bytes": dataset_info.get("splits")[subset].num_bytes,
+                        "num_examples": dataset_info.get("splits")[subset].num_examples,
+                        "dataset_name": dataset_info.get("splits")[subset].dataset_name,
                     }
                 }
-                for subset in dataset_info["splits"]
-            ],
-            "download_checksums": dataset_info["download_checksums"],
-            "download_size": dataset_info["download_size"],
-            "post_processing_size": dataset_info["post_processing_size"],
-            "dataset_size": dataset_info["dataset_size"],
-            "size_in_bytes": dataset_info["size_in_bytes"],
-        }
+                for subset in dataset_info.get("splits")
+            ]
+        except LookupError:
+            logger.warning(f'Failed to get splits information')
