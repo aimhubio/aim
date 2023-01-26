891e95d57cadebfdd97cfb7e286fc7183fc0fee7
Ruben Karapetyan <karapetyanrouben@gmail.com>
2023-01-26 21:20:08 +0400

    Change copy icon size

--- aim/web/ui/src/pages/RunDetail/RunOverviewTab/components/RunOverviewSidebar/RunOverviewSidebar.tsx
+++ aim/web/ui/src/pages/RunDetail/RunOverviewTab/components/RunOverviewSidebar/RunOverviewSidebar.tsx
@@ -149,7 +149,7 @@ function RunOverviewSidebar({
             </div>
             <CopyToClipBoard
               className='RunOverviewSidebar__section__info__listItem__copyRunHashButton'
-              iconSize='xxSmall'
+              iconSize='small'
               copyContent={runHash ?? ''}
             />
           </div>
