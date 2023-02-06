aebe2687680e46162593152b69a015cfcb8ba0a5
mihran113 <vanyanmihran@gmail.com>
2023-02-06 12:17:28 +0400

    [fix] Sanity checks

--- aim/sdk/adapters/hugging_face.py
+++ aim/sdk/adapters/hugging_face.py
@@ -143,7 +143,7 @@ class AimCallback(TrainerCallback):
                 matching_substring = string1[match.a:match.a + match.size]
                 substring_counts[matching_substring] += 1
 
-        return max(substring_counts, key= lambda x: substring_counts[x]).rstrip('_')
+        return max(substring_counts, key=lambda x: substring_counts[x]).rstrip('_')
 
     def __del__(self):
         self.close()
