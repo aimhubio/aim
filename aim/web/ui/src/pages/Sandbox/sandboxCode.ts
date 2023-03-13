c0b9f3eacfba2b63cc59f64a241b94b4cdb11f02
Ruben Karapetyan <karapetyanrouben@gmail.com>
2023-03-13 19:18:49 +0400

    Tune styles for board cards

--- aim/web/ui/src/pages/Sandbox/sandboxCode.ts
+++ aim/web/ui/src/pages/Sandbox/sandboxCode.ts
@@ -1,10 +1,22 @@
-export const initialCode = `metrics = Metric.query('metric.name == "loss"')
+export const initialCode = `#####
+# Filter data with a query condition 
+# Example
+metrics = Metric.query('metric.name == "loss"')
+#####
 
+#####
+# Render your data with viz component
+# Example
 line_chart = LineChart(metrics, x='steps', y='values',
-                    color=["run.name"],
-                    stroke_style=["metric.context"])
+                   color=["run.name"],
+                   stroke_style=["metric.context"])
+#####
 
+#####
+# Use component state to make your board interactive
+# Example
 if line_chart["focused_line_data"] != None:
     run = line_chart["focused_line_data"]["run"]
     JSON(run)
+#####
 `;
