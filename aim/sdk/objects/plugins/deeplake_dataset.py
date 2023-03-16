b2898f07ea67bdba8f46c7d2a9ce94c19c5743fc
Bernhard Conzelmann <25883607+drahnreb@users.noreply.github.com>
2023-03-16 01:28:03 +0100

    [feat] add activeloop deeplake dataset

---
+++ aim/sdk/objects/plugins/deeplake_dataset.py
@@ -0,0 +1,58 @@
+from aim.storage.object import CustomObject
+import deeplake
+
+
+@CustomObject.alias('deeplake.dataset')
+class DeeplakeDataset(CustomObject):
+    """
+    Track Activeloop Deeplake Dataset with versioning.
+
+    .. code-block:: python
+
+        import deeplake
+
+        from aim.sdk.objects.plugins.deeplake_dataset import DeeplakeDataset
+        from aim.sdk import Run
+
+        # create dataset object
+        ds = deeplake.dataset('hub://activeloop/cifar100-test')
+
+        # log dataset metadata
+        run = Run(system_tracking_interval=None)
+        run['deeplake_ds'] = DeeplakeDataset(ds)
+    """
+    AIM_NAME = 'deeplake.dataset'
+
+    def __init__(self, dataset: deeplake.Dataset):
+        super().__init__()
+        self.storage['dataset'] = {
+            'source': 'deeplake',
+            'meta': self._get_ds_meta(dataset)
+        }
+
+    def _get_ds_meta(self, ds: deeplake.Dataset):
+        return {
+            "path": ds.path,
+            "commit_id": ds.commit_id,
+            "branch": ds.branch,
+            "has_head_changes": ds.has_head_changes,
+            "info": ds.info,  # Info might contain keys such as "description" and "title"
+            "num_samples": len(ds),
+            "tensors": {group: self._tensor_meta(tensor) for group, tensor in ds.tensors.items()},
+            "size_approx": ds.size_approx(),
+            "deeplake_version": ds.meta.version
+        }
+
+    def _tensor_meta(self, tensor: deeplake.Tensor):
+        meta = tensor.meta
+        return {
+            "name": tensor.key,
+            "num_samples": len(tensor),
+            "htype": tensor.htype,
+            "dtype": str(tensor.dtype) if tensor.dtype else None,
+            "compression_type": "sample_compression"
+            if meta.sample_compression
+            else ("chunk_compression" if meta.chunk_compression else None),
+            "compression_format": meta.sample_compression or meta.chunk_compression,
+            "info": tensor.info,
+        }
