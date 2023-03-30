00685c5f2b71889e4f7136f32a056c6d20b4267f
Dongki Lee <123964440+asynclee@users.noreply.github.com>
2023-03-30 16:54:37 +0900

    fix typo

--- aim/web/ui/src/components/MediaPanel/MediaPanel.tsx
+++ aim/web/ui/src/components/MediaPanel/MediaPanel.tsx
@@ -217,7 +217,7 @@ function MediaPanel({
         const processingBlobUriArray = Object.assign([], blobUriArray.current);
         blobUriArray.current = [];
         processedBlobUriArray.current = [
-          ...new Set([...processedBlobUriArray, ...processingBlobUriArray]),
+          ...new Set([...processedBlobUriArray.current, ...processingBlobUriArray]),
         ];
         requestRef.current = getBlobsData(processingBlobUriArray);
         requestRef.current.call().catch(() => {
