30e48f1ce21cb854ca8b28b1cef129737f080caf
Albert Torosyan <torosyanalbert@gmail.com>
2023-04-19 16:41:08 +0400

    [feat] Add top-level imports for core SDK classes

---
+++ aim/sdk/core/__init__.py
@@ -0,0 +1,11 @@
+from aim.sdk.core.object import Object
+from aim.sdk.core.sequence import Sequence
+from aim.sdk.core.container import Container
+from aim.sdk.core.repo import Repo
+
+__all__ = ['Object', 'Sequence', 'Container', 'Repo']
+__aim_types__ = [Sequence, Container, Object]
+
+from aim.sdk.core.type_utils import _register
+
+_register()
