7343d327670afbbe70559a0e7d89eea95c40ecf5
mihran113 <vanyanmihran@gmail.com>
2023-05-02 15:07:47 +0400

    [fix] Notebook extension start-up (#2696)

--- aim/cli/manager/manager.py
+++ aim/cli/manager/manager.py
@@ -27,12 +27,28 @@ class ManagerActionResult:
 
 
 def run_up(args):
+    def check_startup_success():
+        import requests
+        server_path = 'http://{}:{}{}'.format(args['--host'], args['--port'], args['--base-path'])
+        status_api = f'{server_path}/api/projects/status'
+        retry_count = 5
+        sleep_interval = 1
+        for _ in range(retry_count):
+            try:
+                response = requests.get(status_api)
+                if response.status_code == 200:
+                    return True
+            except Exception:
+                pass
+            sleep_interval += 1
+            time.sleep(sleep_interval)
+
+        return False
+
     args_list = ['--log-level=error']
     for p in args.keys():
         if p != '--proxy-url':
             args_list.append(p + '=' + args[p])
-
-    success_msg = 'Open http://{}:{}{}'.format(args['--host'], args['--port'], args['--base-path'])
 
     child_process = subprocess.Popen(
         ['aim', UP_NAME] + args_list + ['--force-init'],
@@ -45,30 +61,26 @@ def run_up(args):
         'host': 'http://' + args['--host']
     }
 
-    time.sleep(2)
+    if check_startup_success():
+        return ManagerActionResult(
+            ManagerActionStatuses.Succeed,
+            info
+        )
+
     for line in child_process.stderr:
-        # @TODO improve this solution
-        #  The child process `aim cli` has an incompatible things inside
-        #  that's the reason to find this way to get result from the process
         if ERROR_MSG_PREFIX in line.decode():
             return ManagerActionResult(
                 ManagerActionStatuses.Failed,
                 {'message': line.decode()}
             )
-        else:
-            if success_msg in line.decode():
-                return ManagerActionResult(
-                    ManagerActionStatuses.Succeed,
-                    info
-                )
-            else:
-                return ManagerActionResult(
-                    ManagerActionStatuses.Failed,
-                    {
-                        'message': '\nPerhaps this is a bug from aim side.'
-                                   '\nPlease open an issue https://github.com/aimhubio/aim/issues.'
-                    }
-                )
+
+    return ManagerActionResult(
+        ManagerActionStatuses.Failed,
+        {
+            'message': '\nPerhaps this is a bug from aim side.'
+                       '\nPlease open an issue https://github.com/aimhubio/aim/issues.'
+        }
+    )
 
 
 def run_version(args):
