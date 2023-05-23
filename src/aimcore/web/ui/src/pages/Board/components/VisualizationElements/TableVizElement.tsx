be63fc1545c4f8c2eeebbd80cc997d94f0eac67f
Ruben Karapetyan <karapetyanrouben@gmail.com>
2023-05-24 00:00:06 +0400

    Remove unused table props

--- src/aimcore/web/ui/src/pages/Board/components/VisualizationElements/TableVizElement.tsx
+++ src/aimcore/web/ui/src/pages/Board/components/VisualizationElements/TableVizElement.tsx
@@ -3,10 +3,9 @@ import { Table } from 'components/kit_v2';
 function TableVizElement(props: any) {
   return (
     <Table
-      {...props.options}
+      data={props.data}
       onRowFocus={props.callbacks.on_row_focus}
       onRowSelect={props.callbacks.on_row_select}
-      data={props.data}
     />
   );
 }
