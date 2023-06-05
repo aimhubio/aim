beac4b7cf1c46401ebb4511afba5d6bc64404ba1
Arsen Alkamyan <alkamyan97@gmail.com>
2023-06-05 13:58:23 +0400

    [feat] Add charts.py board

--- src/aimcore/web/ui/src/pages/Board/components/VisualizationElements/NivoLineChartVizElement.tsx
+++ src/aimcore/web/ui/src/pages/Board/components/VisualizationElements/NivoLineChartVizElement.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 import { ResponsiveLine } from '@nivo/line';
 
+import { Box, Text } from 'components/kit_v2';
+
 function NivoLineChartVizElement(props: any) {
   const modifyData = React.useMemo(() => {
-    return props.data.map((item: any) => {
+    let maxPointsCount = 0;
+    const data = props.data.map((item: any) => {
       return {
         id: item.name,
         color: item.color,
         data: item.data.xValues.map((dataItem: number, index: number) => {
+          if (item.data.yValues.length > maxPointsCount) {
+            maxPointsCount = item.data.yValues[index].length;
+          }
           return {
             x: dataItem,
             y: item.data.yValues[index],
@@ -16,13 +22,19 @@ function NivoLineChartVizElement(props: any) {
         }),
       };
     });
+    return {
+      data,
+      maxPointsCount,
+    };
   }, [props.data]);
+
   return (
     <div className='VizComponentContainer'>
       <ResponsiveLine
-        data={modifyData}
-        enablePoints={false}
+        data={modifyData.data}
+        enablePoints={modifyData.maxPointsCount < 100}
         lineWidth={1}
+        colors={(d) => d.color}
         margin={{ top: 50, right: 90, bottom: 50, left: 60 }}
         xScale={{
           type: 'linear',
@@ -32,7 +44,38 @@ function NivoLineChartVizElement(props: any) {
           type: 'linear',
           reverse: false,
         }}
-        yFormat=' >-.2f'
+        enableSlices='x'
+        sliceTooltip={({ slice }) => {
+          return (
+            <Box
+              key={slice.x}
+              css={{
+                background: 'white',
+                br: '$3',
+                padding: '$6 $8',
+                border: '1px solid #ccc',
+              }}
+            >
+              <Text weight='$4' as='strong'>
+                x: {slice.id}
+              </Text>
+              {slice.points.map((point) => (
+                <Box
+                  key={point.id}
+                  css={{
+                    padding: '3px 0',
+                  }}
+                >
+                  <Text css={{ mr: '$5' }} color={point.serieColor}>
+                    {point.serieId}:
+                  </Text>
+                  <Text>{point.data.y}</Text>
+                </Box>
+              ))}
+            </Box>
+          );
+        }}
+        enableCrosshair={true}
         axisTop={null}
         axisRight={null}
         axisBottom={{
@@ -55,7 +98,7 @@ function NivoLineChartVizElement(props: any) {
         useMesh={true}
         legends={[
           {
-            anchor: 'bottom-right',
+            anchor: 'top-right',
             direction: 'column',
             justify: false,
             translateX: 100,
@@ -65,7 +108,7 @@ function NivoLineChartVizElement(props: any) {
             itemWidth: 80,
             itemHeight: 20,
             itemOpacity: 0.75,
-            symbolSize: 10,
+            symbolSize: 8,
             symbolShape: 'circle',
             symbolBorderColor: 'rgba(0, 0, 0, .5)',
             effects: [
