801a0ea9da491d1e747f47cacb0450349a5639e9
Ruben Karapetyan <karapetyanrouben@gmail.com>
2023-06-05 21:21:02 +0400

    Wrap code block with box to improve spacings

--- src/aimcore/web/ui/src/pages/Board/components/VisualizationElements/CodeVizElement.tsx
+++ src/aimcore/web/ui/src/pages/Board/components/VisualizationElements/CodeVizElement.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { Box } from 'components/kit_v2';
 import CodeBlock from 'components/CodeBlock/CodeBlock';
 
 function CodeVizElement(props: any) {
-  return <CodeBlock code={`${props.data}`} language={props.options.language} />;
+  return (
+    <Box>
+      <CodeBlock code={`${props.data}`} language={props.options.language} />
+    </Box>
+  );
 }
 
 export default CodeVizElement;
