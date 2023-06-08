55f41c50b8a79af7b3a434525ec2f726c9e2e2e7
Ruben Karapetyan <karapetyanrouben@gmail.com>
2023-06-08 23:03:25 +0400

    [feat] Add a demo board for data display elements (#2827)

---
+++ pkgs/aimstack/demo/boards/data_display_elements.py
@@ -0,0 +1,49 @@
+# Header
+ui.header('Header')
+
+# Sub header
+ui.sub_header('Sub header')
+
+# Text element
+ui.text('This is a text element')
+
+# JSON
+ui.text('JSON')
+ui.json({
+    'key': 'value',
+    'key2': 10,
+    'key3': [1, 2, 3],
+    'key4': {
+        'nested_key': 'value'
+    }
+})
+
+# Table
+ui.text('Table')
+ui.table({
+    'col1': [1, 2, 3],
+    'col2': [4, 5, 6],
+    'col3': [7, 8, 9]
+})
+
+ui.text('Table with custom column renderer')
+ui.table({
+    'col1': [1, 2, 3],
+    'col2': [4, 5, 6],
+    'col3': [7, 8, 9]
+}, {
+    'col1': lambda val: ui.number_input(value=val),
+})
+
+ui.html('<p>This is <strong style="color: #396">HTML</strong> renderer!</p>')
+
+ui.link('External link to Aim GitHub page',
+        'https://github.com/aimhubio/aim', new_tab=True)
+
+ui.code("""# This is a code block
+from aim import Metric
+
+metric = Metric.filter()
+
+ui.line_chart(metric, x='steps', y='values')
+""")
