145e6992ab82a697b076299510e00daaed0b0561
Ruben Karapetyan <karapetyanrouben@gmail.com>
2023-06-16 20:00:52 +0400

    [feat] Add sequence viz components section to docs package

--- pkgs/aimstack/docs/boards/ui/layout_containers/form.py
+++ pkgs/aimstack/docs/boards/ui/layout_containers/form.py
@@ -1,4 +1,4 @@
-ui.header('ui.rows')
+ui.header('ui.form')
 ui.text("A form is a container that visually groups other elements and widgets together, and contains a Submit button. When the form's Submit button is pressed, all elements values inside the form will be appliead to the board's state.")
 ui.text('To add elements to the returned container, you can use "with" notation or just call methods directly on the returned object.')
 
@@ -23,7 +23,7 @@ ui.subheader('Example')
 ui.code("""form = ui.form('Apply')
 
 with form:
-    val = ui.text_input('value')
+    val = ui.text_input(value='value')
 
 ui.text('The value below will be updated only after pressing the Apply button.')
 ui.text(val)""")
---
+++ pkgs/aimstack/docs/boards/ui/sequence_viz/figures.py
@@ -0,0 +1,22 @@
+ui.header('ui.figures')
+ui.text('Display an figures list component.')
+
+ui.subheader('Signature')
+ui.code("ui.figures(data)")
+
+ui.subheader('Parameters')
+ui.table({
+    'name': ['data'],
+    'type': ['List[dict]'],
+    'default': ['', '{}'],
+    '': [
+        'The data for the FiguresList component'
+    ],
+})
+
+ui.subheader('Example')
+ui.code("""from asp import Figures
+
+figures = Figures.filter()
+
+ui.figures(images)""")
