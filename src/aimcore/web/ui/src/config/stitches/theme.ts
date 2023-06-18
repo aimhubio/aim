c70f0a084edaf2ac9a259f851f3b2394908be9b8
Arsen Alkamyan <alkamyan97@gmail.com>
2023-06-19 00:25:56 +0400

    [feat] Update shadows for ui kit v2

--- src/aimcore/web/ui/src/config/stitches/theme.ts
+++ src/aimcore/web/ui/src/config/stitches/theme.ts
@@ -494,4 +494,13 @@ export const lightTheme: ConfigType.Theme = {
   transitions: {
     main: '0.2s ease-out',
   },
+  shadows: {
+    moderate:
+      '0px 1px 2px rgba(0, 0, 0, 0.3), 0px 1px 3px 1px rgba(0, 0, 0, 0.15)',
+    intense:
+      '0px 6px 10px 4px rgba(0, 0, 0, 0.15), 0px 2px 3px rgba(0, 0, 0, 0.3)',
+    subtle: '0px 0px 0px 3px rgba(0, 0, 0, 0.08)',
+    strong:
+      '0px 4px 8px 3px rgba(0, 0, 0, 0.15), 0px 1px 3px rgba(0, 0, 0, 0.3)',
+  },
 };
