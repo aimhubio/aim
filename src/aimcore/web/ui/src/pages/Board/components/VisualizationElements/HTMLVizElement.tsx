aed9dab111866953f89be843e4b443040c88f257
Arsen Alkamyan <alkamyan97@gmail.com>
2023-06-19 22:58:05 +0400

    [feat] Add html component nested tag styles

--- src/aimcore/web/ui/src/pages/Board/components/VisualizationElements/HTMLVizElement.tsx
+++ src/aimcore/web/ui/src/pages/Board/components/VisualizationElements/HTMLVizElement.tsx
@@ -2,9 +2,26 @@ import * as React from 'react';
 
 import { Box } from 'components/kit_v2';
 
+import { styled } from 'config/stitches';
+
+const HtmlVizElementContainer = styled(Box, {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$13',
+  '& h3': {
+    fontSize: '$6',
+    fontWeight: '$2',
+    color: '$textPrimary',
+  },
+  '& h2': {
+    fontSize: '$9',
+    fontWeight: '$2',
+  },
+});
+
 function HTMLVizElement(props: any) {
   return (
-    <Box
+    <HtmlVizElementContainer
       style={{ width: '100%' }}
       css={{
         color: '$textPrimary',
