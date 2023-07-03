e4ee4fdc65bd89825c23075be8b18a8f787641d8
Arsen Alkamyan <alkamyan97@gmail.com>
2023-07-03 15:41:41 +0400

    [feat] Update Breadcrumb colors

--- src/aimcore/web/ui/src/components/kit_v2/Breadcrumb/Breadcrumb.style.ts
+++ src/aimcore/web/ui/src/components/kit_v2/Breadcrumb/Breadcrumb.style.ts
@@ -3,17 +3,17 @@ import { NavLink } from 'react-router-dom';
 import { styled } from 'config/stitches';
 
 const BreadcrumbItem = styled(NavLink, {
-  color: '$textPrimary50',
+  color: '$text-default-text-muted',
   textDecoration: 'none',
   fontWeight: '$3',
   textTransform: 'uppercase',
   fontSize: '$3',
   transition: 'all 0.18s ease-out',
   '&:hover': {
-    color: '$textPrimary',
+    color: '$text-default-text-deep',
   },
   '&.active': {
-    color: '$textPrimary',
+    color: '$text-default-text-deep',
   },
 });
 
@@ -21,7 +21,7 @@ const BreadcrumbLastItem = styled('span', {
   fontWeight: '$3',
   textTransform: 'uppercase',
   fontSize: '$3',
-  color: '$textPrimary',
+  color: '$text-default-text-deep',
 });
 
 export { BreadcrumbItem, BreadcrumbLastItem };
