ed8e7c3f345e2784e7334a6a97d1f70410c1296c
donderom <donderom@gmail.com>
2023-07-04 20:35:49 +0300

    [refactor] Improve performance of dict flattening for board runs

--- pkgs/aimstack/asp/boards/runs.py
+++ pkgs/aimstack/asp/boards/runs.py
@@ -9,14 +9,14 @@ runs = Run.filter(query)
 
 
 def flatten(dictionary, parent_key='', separator='.'):
-    items = []
+    flattened = {}
     for key, value in dictionary.items():
-        new_key = parent_key + separator + key if parent_key else key
+        new_key = f"{parent_key}{separator}{key}" if parent_key else key
         if isinstance(value, MutableMapping):
-            items.extend(flatten(value, new_key, separator=separator).items())
+            flattened.update(flatten(value, new_key, separator=separator))
         else:
-            items.append((new_key, value))
-    return dict(items)
+            flattened[new_key] = value
+    return flattened
 
 
 @memoize
