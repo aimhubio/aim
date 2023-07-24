8b675782dbb5588e4412fb9a788e160fb70baf30
Arsen Alkamyan <alkamyan97@gmail.com>
2023-07-24 13:09:04 +0400

    [feat] Declare gtag as a global interface

--- src/aimcore/web/ui/src/services/googleAnalytics.ts
+++ src/aimcore/web/ui/src/services/googleAnalytics.ts
@@ -1,22 +1,28 @@
-//@ts-nocheck
 const isDev = process.env.NODE_ENV === 'development';
 
 const enabled = () => {
-  return !isDev && window.gtag;
+  if (!isDev && window.gtag !== undefined) {
+    return true;
+  } else {
+    console.warn('Google Analytics is not enabled.');
+    return false;
+  }
 };
 
-const pageView = (pageName) => {
-  if (!enabled()) return;
-  window.gtag('event', 'page_view', {
-    page_title: pageName,
-    page_path: window.location.pathname,
-    page_location: window.location.href,
-  });
+const pageView = (pageName: string) => {
+  if (enabled()) {
+    window.gtag('event', 'page_view', {
+      page_title: pageName,
+      page_path: window.location.pathname,
+      page_location: window.location.href,
+    });
+  }
 };
 
 const trackEvent = (eventName: string, properties = {}) => {
-  if (!enabled()) return;
-  window.gtag('event', eventName, properties);
+  if (enabled()) {
+    window.gtag('event', eventName, properties);
+  }
 };
 
 export { pageView, trackEvent };
