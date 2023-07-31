e6d2d997615b35ca6aa4af547312b5542f7efced
Karo <44981312+KaroMourad@users.noreply.github.com>
2023-07-31 12:32:22 +0400

    [feat] Add Images Explorer (#2947)

--- src/aimcore/web/ui/src/pages/Board/components/AudiosList.tsx
+++ src/aimcore/web/ui/src/pages/Board/components/AudiosList.tsx
@@ -12,12 +12,6 @@ function AudiosList(props: any) {
     ...audio.record,
   }));
 
-  const boxStyle = {
-    margin: '5px',
-    height: 50,
-    width: 'calc(100% - 10px)',
-    flex: 1,
-  };
   const boxKey = (item: any) =>
     `${item?.container?.hash}_${item.name}_${JSON.stringify(item.context)}_${
       item.step
@@ -27,7 +21,6 @@ function AudiosList(props: any) {
       {data.map((item: any) => (
         <AudioBox
           key={boxKey(item)}
-          style={boxStyle}
           blobData={item.blobs.data}
           format={item.format}
           caption={item.caption}
