ae4e0b3f5be83087297e71f697c6922a090648a3
tmynn <hovhannes.tamoyan@gmail.com>
2023-08-22 14:45:46 +0000

    [feat] Add Langchain in AI Systsems Tracking package. Support for LLMChain and Agent Chain

---
+++ pkgs/aimstack/ai-systems-tracking/langchain_logger/src/langchain_logger/__init__.py
@@ -0,0 +1,29 @@
+__aim_boards__ = "boards"
+
+from langchain_logger.logging.analytics import *
+from langchain_logger.logging.chat import *
+
+__all__ = [
+    "Session",
+    "SessionProd",
+    "SessionDev",
+    "Experiment",
+    "Release",
+    "MessagesSequence",
+    "UserActivity",
+    "UserActions",
+]
+
+__aim_types__ = [
+    Session,
+    SessionProd,
+    SessionDev,
+    Experiment,
+    Release,
+    MessagesSequence,
+    UserActivity,
+    UserActions,
+]
+
+for aim_type in __aim_types__:
+    setattr(aim_type, "__aim_package__", "langchain_logger")
