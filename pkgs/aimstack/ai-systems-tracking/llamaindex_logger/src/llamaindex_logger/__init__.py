52792e9a7c751d18a6ad1d6ff0422db6b1e694ec
tmynn <hovhannes.tamoyan@gmail.com>
2023-08-25 17:46:39 +0000

    [feat] Add llamaindex basic logger pkg

---
+++ pkgs/aimstack/ai-systems-tracking/llamaindex_logger/src/llamaindex_logger/__init__.py
@@ -0,0 +1,31 @@
+__aim_boards__ = "boards"
+
+from llamaindex_logger.logging.analytics import *
+from llamaindex_logger.logging.chat import *
+
+__all__ = [
+    "Session",
+    "SessionProd",
+    "SessionDev",
+    "Experiment",
+    "Release",
+    "MessagesSequence",
+    "ChunkSequence",
+    "UserActivity",
+    "UserActions",
+]
+
+__aim_types__ = [
+    Session,
+    SessionProd,
+    SessionDev,
+    Experiment,
+    Release,
+    MessagesSequence,
+    ChunkSequence,
+    UserActivity,
+    UserActions,
+]
+
+for aim_type in __aim_types__:
+    setattr(aim_type, "__aim_package__", "llamaindex_logger")
---
+++ pkgs/aimstack/ai-systems-tracking/llamaindex_logger/src/llamaindex_logger/boards/dev/experiment.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+
+from llamaindex_logger import Experiment
+
+# FIXIT:
+# There is LOTS of repeat non-ui code in these files.
+# To be moved to "server functions" once its available (to be shipped as part of stable)
+
+# FIXIT:
+# There is lots of repeat UI code that should be moved into template.
+# This is exactly the same page as the one in release.
+# In real world these would be different pages built on the same base.
+# We need to enable __init__.py in the UI folders so devs can precisely define what files get rendered and what become reusable templates.
+
+
+##################
+# Utils
+##################
+
+
+def get_experiments(query="", param=None):
+    sessions = Experiment.filter(query)
+    sessions = sorted(
+        sessions, key=lambda sess: sess["params"].get("started") or 0, reverse=True
+    )
+    if param is not None:
+        return [session.get(param) for session in sessions]
+    return sessions
+
+
+def get_experiment(session_hash):
+    sessions = Experiment.filter(f'c.hash == "{session_hash}"')
+    if sessions and len(sessions):
+        return sessions[0]
+    return None
+
+
+##################
+
+
+def experiment(exp_hash):
+    exp = get_experiment(exp_hash)
+    if not exp:
+        ui.text("Pick an experiment")
+        return
+
+    ui.header(f'Experiment "{exp_hash}"')
+
+    overview, docs, llm, tools, agent = ui.tabs(
+        ["Overview", "Documents", "LLM", "Tools", "Agent"]
+    )
+
+    overview.json(
+        {
+            "release": exp["params"].get("release"),
+            "version": exp["params"].get("version"),
+            "started": datetime.fromtimestamp(exp["params"].get("started")).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+            if exp["params"].get("started")
+            else "-",
+        }
+    )
+
+    docs.json(exp["params"].get("docs")) if exp["params"].get("docs") else None
+    llm.json(exp["params"].get("llm")) if exp["params"].get("llm") else None
+    tools.json(exp["params"].get("tools")) if exp["params"].get("tools") else None
+    agent.json(exp["params"].get("agent")) if exp["params"].get("agent") else None
+    
+    
+
+##################
+# Page
+##################
+
+try:
+    exp_hash = state["dev/experiment.py"]["experiment_hash"]
+except:
+    exp_hash = ""
+
+experiments = get_experiments("", "hash")
+
+if experiments:
+    default_exp = experiments.index(exp_hash) if exp_hash != "" else 0
+    exp_hash = ui.select(options=experiments, index=default_exp)
+    experiment(exp_hash)
