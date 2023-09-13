ac6a29b0ef9292eaae128279a36553c4c5239ca1
tmynn <hovhannes.tamoyan@gmail.com>
2023-09-13 20:22:04 +0000

    [feat] catboost_track update

--- docs/example_scripts/catboost_track.py
+++ docs/example_scripts/catboost_track.py
@@ -1,18 +1,12 @@
-"""
-Code snippet from Catboost's official doc has been used for this example:
-https://catboost.ai/en/docs/concepts/python-quickstart#classification
-"""
-
 import numpy as np
 from aimstack.ml.integrations.catboost import AimLogger
 from catboost import CatBoostClassifier, Pool
 
 # initialize data
 train_data = np.random.randint(0, 100, size=(100, 10))
-
 train_labels = np.random.randint(0, 2, size=(100))
 
-test_data = catboost_pool = Pool(train_data, train_labels)
+eval_dataset = Pool(train_data, train_labels)
 
 model = CatBoostClassifier(
     iterations=2, depth=2, learning_rate=1, loss_function="Logloss", verbose=True
@@ -26,7 +20,7 @@ model.fit(
     logging_level="Info",
 )
 # make the prediction using the resulting model
-preds_class = model.predict(test_data)
-preds_proba = model.predict_proba(test_data)
+preds_class = model.predict(eval_dataset)
+preds_proba = model.predict_proba(eval_dataset)
 print("class = ", preds_class)
 print("proba = ", preds_proba)
