55fb7067bd3e8c37817f1b8db15dbd78a79f9953
tmynn <hovhannes.tamoyan@gmail.com>
2023-09-13 21:44:51 +0000

    [feat] paddle update

--- docs/example_scripts/paddle_track.py
+++ docs/example_scripts/paddle_track.py
@@ -1,22 +1,18 @@
 import paddle
-import paddle.vision.transforms as T
 from aimstack.ml.integrations.paddle import AimCallback
-from paddle.static import InputSpec
+from paddle.vision.transforms import ToTensor
 
-inputs = [InputSpec([-1, 1, 28, 28], "float32", "image")]
-labels = [InputSpec([None, 1], "int64", "label")]
+train_dataset = paddle.vision.datasets.MNIST(mode="train", transform=ToTensor())
+test_dataset = paddle.vision.datasets.MNIST(mode="test", transform=ToTensor())
+lenet = paddle.vision.models.LeNet()
 
-transform = T.Compose([T.Transpose(), T.Normalize([127.5], [127.5])])
-train_dataset = paddle.vision.datasets.MNIST(mode="train", transform=transform)
-eval_dataset = paddle.vision.datasets.MNIST(mode="test", transform=transform)
+model = paddle.Model(lenet)
 
-net = paddle.vision.models.LeNet()
-model = paddle.Model(net, inputs, labels)
-
-optim = paddle.optimizer.Adam(0.001, parameters=net.parameters())
 model.prepare(
-    optimizer=optim, loss=paddle.nn.CrossEntropyLoss(), metrics=paddle.metric.Accuracy()
+    optimizer=paddle.optimizer.Adam(0.001, parameters=lenet.parameters()),
+    loss=paddle.nn.CrossEntropyLoss(),
+    metrics=paddle.metric.Accuracy(),
 )
 
 callback = AimCallback(experiment_name="paddle_test")
-model.fit(train_dataset, eval_dataset, batch_size=64, callbacks=callback)
+model.fit(train_dataset, test_dataset, batch_size=64, callbacks=callback)
