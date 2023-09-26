d266f54849a549e0383a670336a50ff02715efb2
Ruben Karapetyan <karapetyanrouben@gmail.com>
2023-09-26 14:16:06 +0400

    Use defaultdict for table columns

--- pkgs/aimstack/asp/boards/runs.py
+++ pkgs/aimstack/asp/boards/runs.py
@@ -1,4 +1,5 @@
 from asp import Run
+from collections import defaultdict
 
 search_signal = "search"
 
@@ -22,7 +23,7 @@ def flatten(dictionary, parent_key='', separator='.'):
 
 @memoize
 def get_table_data(data=[], page_size=10, page_num=1, should_fold=True):
-    table_data = {}
+    table_data = defaultdict(list)
     exclude_keys = ['type', 'container_type', 'container_full_type']
 
     runs = data[(page_num - 1) * page_size:page_num * page_size]
@@ -33,9 +34,7 @@ def get_table_data(data=[], page_size=10, page_num=1, should_fold=True):
             if key in exclude_keys:
                 continue
             else:
-                if key not in table_data:
-                    table_data[key] = [None] * i
-                elif len(table_data[key]) < i:
+                if len(table_data[key]) < i:
                     table_data[key].extend([None] * (i - len(table_data[key])))
             
                 table_data[key].append(f'{value}')
