f02f32b514645c540902c7ea06ebe7787bfb4d13
Ruben Karapetyan <karapetyanrouben@gmail.com>
2023-09-27 18:29:24 +0400

    [feat] Group apps by category

--- src/aimcore/web/ui/src/pages/Apps/Apps.tsx
+++ src/aimcore/web/ui/src/pages/Apps/Apps.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as _ from 'lodash-es';
 
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
-import { Box, Input, Text } from 'components/kit_v2';
+import { Box, Input, Separator, Text } from 'components/kit_v2';
 import BusyLoaderWrapper from 'components/BusyLoaderWrapper/BusyLoaderWrapper';
 import Illustration, { ILLUSTRATION_TYPES } from 'components/Illustration';
 
@@ -17,6 +17,23 @@ function Apps(): React.FunctionComponentElement<React.ReactNode> {
   const { packages, isLoading } = usePyodide();
   const [searchValue, setSearchValue] = React.useState<string>('');
 
+  const categories = React.useMemo(() => {
+    if (_.isEmpty(packages)) return {};
+
+    const result: Record<string, any> = {};
+
+    for (let pkg in packages) {
+      const { category } = packages[pkg];
+
+      if (!result.hasOwnProperty(category)) {
+        result[category] = [];
+      }
+      result[category ?? 'Unknown'].push(packages[pkg]);
+    }
+
+    return result;
+  }, [packages]);
+
   return (
     <ErrorBoundary>
       <TopBar>
@@ -24,7 +41,7 @@ function Apps(): React.FunctionComponentElement<React.ReactNode> {
       </TopBar>
       <AppsContainer>
         <BusyLoaderWrapper isLoading={isLoading} height={'100%'}>
-          {_.isEmpty(packages) ? (
+          {_.isEmpty(categories) ? (
             <Illustration type={ILLUSTRATION_TYPES.Empty_Apps} />
           ) : (
             <>
@@ -41,21 +58,30 @@ function Apps(): React.FunctionComponentElement<React.ReactNode> {
                   />
                 </Box>
               </Box>
-              <AppsCardWrapper>
-                {Object.keys(packages)
-                  .filter((appName: string) =>
-                    appName
+              {Object.keys(categories).map((category: string) => {
+                const filteredPackages = categories[category].filter(
+                  (pkg: any) =>
+                    pkg.name
                       .toLowerCase()
                       .includes(searchValue.trim().toLowerCase()),
+                );
+
+                return (
+                  filteredPackages.length > 0 && (
+                    <Box key={category} mt='$8'>
+                      <Text weight='$3' color='$textPrimary' size='$8'>
+                        {category}
+                      </Text>
+                      <Separator css={{ mt: '$4' }} />
+                      <AppsCardWrapper>
+                        {filteredPackages.map((pkg: any) => (
+                          <AppCard key={pkg.name} {...pkg} />
+                        ))}
+                      </AppsCardWrapper>
+                    </Box>
                   )
-                  .map((appName: string) => (
-                    <AppCard
-                      key={appName}
-                      name={appName}
-                      {...packages[appName]}
-                    />
-                  ))}
-              </AppsCardWrapper>
+                );
+              })}
             </>
           )}
         </BusyLoaderWrapper>
