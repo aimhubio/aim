884c01f992299de2c31d08b88ead165d9d82233d
Maurice Kraus <dev@mkraus.io>
2023-10-04 21:55:34 +0200

    [fix] Update import + v4 changes (#2746)
    This commit adds a conditional lightning import
    and adapts the adapter for v4
    
    [feat] Add Interval postfixes
    
    [fix] Replace invalid double quotes with single quotes
    
    [fix] Maintain compatibility to older python versions
    
    Fix

--- pkgs/aimstack/pytorch_lightning_tracker/loggers/base_logger.py
+++ pkgs/aimstack/pytorch_lightning_tracker/loggers/base_logger.py
@@ -5,28 +5,37 @@ from typing import Any, Dict, Optional, Union
 import packaging.version
 
 try:
-    import pytorch_lightning as pl
+    import lightning as L
 
-    if packaging.version.parse(pl.__version__) < packaging.version.parse('1.7'):
-        from pytorch_lightning.loggers.base import \
-            LightningLoggerBase as Logger
-        from pytorch_lightning.loggers.base import rank_zero_experiment
-    else:
-        from pytorch_lightning.loggers.logger import (
-            Logger,
-            rank_zero_experiment,
-        )
+    from lightning.pytorch.loggers.logger import Logger, rank_zero_experiment
+    from lightning.pytorch.utilities.rank_zero import rank_zero_only
 
-    from pytorch_lightning.utilities import rank_zero_only
 except ImportError:
-    raise RuntimeError(
-        'This contrib module requires PyTorch Lightning to be installed. '
-        'Please install it with command: \n pip install lightning'
-    )
+    try:
+        import pytorch_lightning as pl
 
-from aim import Repo
+        if packaging.version.parse(pl.__version__) < packaging.version.parse('1.7'):
+            from pytorch_lightning.loggers.base import (
+                LightningLoggerBase as Logger,
+                rank_zero_experiment,
+            )
+        else:
+            from pytorch_lightning.loggers.logger import (
+                Logger,
+                rank_zero_experiment,
+            )
+
+        from pytorch_lightning.utilities import rank_zero_only
+    except ImportError:
+        raise RuntimeError(
+            'This contrib module requires Lightning to be installed. '
+            'Please install version prior 2.0 via: \n pip install pytorch-lightning'
+            'Or for the newest version: \n pip install lightning'
+        )
+# _
 from aim._sdk.utils import clean_repo_path, get_aim_repo_name
 from aimstack.experiment_tracker import TrainingRun
+from aim._sdk.repo import Repo
 
 
 class BaseLogger(Logger):
@@ -41,8 +50,8 @@ class BaseLogger(Logger):
         log_system_params (:obj:`bool`, optional): Enable/Disable logging of system params such as installed packages,
             git info, environment variables, etc.
         train_metric_prefix (:obj:`str`, optional): Training metric prefix.
-        val_metric_prefix (:obj:`str`, optional): validation metric prefix.
-        test_metric_prefix (:obj:`str`, optional): testing metric prefix.
+        val_metric_prefix (:obj:`str`, optional): Validation metric prefix.
+        test_metric_prefix (:obj:`str`, optional): Testing metric prefix.
         run_name (:obj:`str`, optional): Aim run name, for reusing the specified run.
         run_hash (:obj:`str`, optional): Aim run hash, for reusing the specified run.
     """
@@ -121,27 +130,32 @@ class BaseLogger(Logger):
             self.experiment.set(('hparams', key), value, strict=False)
 
     @rank_zero_only
-    def log_metrics(self, metrics: Dict[str, float], step: Optional[int] = None):
+    def log_metrics(
+        self,
+        metrics: Dict[str, float],
+        step: Optional[int] = None,
+    ):
         assert rank_zero_only.rank == 0, 'experiment tried to log from global_rank != 0'
 
-        metric_items: Dict[str:Any] = {k: v for k, v in metrics.items()}
+        metric_items: Dict[str: Any] = {k: v for k, v in metrics.items()}
 
         if 'epoch' in metric_items:
-            epoch: int = metric_items.pop('epoch')
+            epoch: Optional[int] = metric_items.pop('epoch')
         else:
             epoch = None
 
         for k, v in metric_items.items():
             name = k
             context = {}
+
             if self._train_metric_prefix and name.startswith(self._train_metric_prefix):
-                name = name[len(self._train_metric_prefix):]
+                name = name[len(self._train_metric_prefix) :]
                 context['subset'] = 'train'
             elif self._test_metric_prefix and name.startswith(self._test_metric_prefix):
-                name = name[len(self._test_metric_prefix):]
+                name = name[len(self._test_metric_prefix) :]
                 context['subset'] = 'test'
             elif self._val_metric_prefix and name.startswith(self._val_metric_prefix):
-                name = name[len(self._val_metric_prefix):]
+                name = name[len(self._val_metric_prefix) :]
                 context['subset'] = 'val'
             self.experiment.track(
                 v, name=name, step=step, epoch=epoch, context=context
@@ -155,7 +169,7 @@ class BaseLogger(Logger):
             del self._run
             self._run = None
 
-    def __del__(self):
+    def __del__(self) -> None:
         self.finalize()
 
     @property
@@ -166,7 +180,7 @@ class BaseLogger(Logger):
         return None
 
     @property
-    def name(self) -> str:
+    def name(self) -> Optional[str]:
         return self._experiment_name
 
     @property
