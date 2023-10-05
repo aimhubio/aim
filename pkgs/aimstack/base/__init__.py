a2f1d432f75b87dd23d9df8e40d2a0de41826241
Albert Torosyan <torosyanalbert@gmail.com>
2023-10-05 16:20:19 +0400

    [feat] Customize sidebar for base app

--- pkgs/aimstack/base/__init__.py
+++ pkgs/aimstack/base/__init__.py
@@ -8,6 +8,8 @@ from .types.distribution import Distribution, DistributionSequence
 from .types.figures import Figure, Figure3D, FigureSequence, Figure3DSequence
 from .types.logging import LogStream, LogRecordSequence
 
+from aim import Boards, Page
+
 from .actions import get_project_stats, get_sequence_type_preview, get_container_type_preview
 
 __aim_types__ = [
@@ -21,3 +23,9 @@ __aim_actions__ = [get_project_stats, get_sequence_type_preview, get_container_t
 __description__ = 'Base package, providing building blocks for data logging and exploration.'
 __author__ = 'AimStack'
 __category__ = 'Generic Log Exploration'
+
+overview_page = Page(name='Overview', board_path='logs_overview.py')
+metrics_page = Page(name='Logged Metrics', board_path='metrics.py')
+runs_page = Page(name='Logged Runs', board_path='runs.py')
+
+__aim_boards__ = Boards(default='Overview', pages=(overview_page, metrics_page, runs_page))
