f6a87e05a30c9d9a49ba951983d70e31065d248d
Albert Torosyan <torosyanalbert@gmail.com>
2024-03-12 10:02:40 +0400

    [feat] Add abstract interfact for artifacts storage

---
+++ aim/storage/artifacts/artifact_storage.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from typing import Optional
+
+
+class AbstractArtifactStorage:
+    def __init__(self, url: str):
+        self.url = url
+
+    @abstractmethod
+    def upload_artifact(self, file_path: str, artifact_path: str, block: bool = False):
+        ...
+
+    @abstractmethod
+    def download_artifact(self, artifact_path: str, dest_dir: Optional[str] = None) -> str:
+        ...
+
+    @abstractmethod
+    def delete_artifact(self, artifact_path: str):
+        ...
