e0dc41807156bd5a4d7b605c1cc86c22553e9962
Sidney <sbatchelder@whoi.edu>
2024-12-11 12:21:48 -0500

    #3259 AimLoggerWithContext

--- aim/pytorch_lightning.py
+++ aim/pytorch_lightning.py
@@ -1,2 +1,3 @@
 # Alias to SDK PyTorch Lightning interface
 from aim.sdk.adapters.pytorch_lightning import AimLogger  # noqa F401
+from aim.sdk.adapters.pytorch_lightning import AimLoggerWithContext  # noqa F401
--- aim/sdk/adapters/pytorch_lightning.py
+++ aim/sdk/adapters/pytorch_lightning.py
@@ -173,3 +173,65 @@ class AimLogger(Logger):
     @property
     def version(self) -> str:
         return self.experiment.hash
+
+
+class AimLoggerWithContext(AimLogger):
+    def __init__(
+        self,
+        repo: Optional[str] = None,
+        experiment: Optional[str] = None,
+        context_prefixes: Optional[Dict] = dict(subset={'train':'train_', 'val':'val_', 'test':'test_'}),
+        context_postfixes: Optional[Dict] = dict(),
+        system_tracking_interval: Optional[int] = DEFAULT_SYSTEM_TRACKING_INT,
+        log_system_params: Optional[bool] = True,
+        capture_terminal_logs: Optional[bool] = True,
+        run_name: Optional[str] = None,
+        run_hash: Optional[str] = None,
+    ):
+        super().__init__()
+        self._experiment_name = experiment
+        self._run_name = run_name
+        self._repo_path = repo
+
+        self._context_prefixes = context_prefixes
+        self._context_postfixes = context_postfixes
+        self._system_tracking_interval = system_tracking_interval
+        self._log_system_params = log_system_params
+        self._capture_terminal_logs = capture_terminal_logs
+
+        self._run = None
+        self._run_hash = run_hash
+
+    @rank_zero_only
+    def log_metrics(self, metrics: Dict[str, float], step: Optional[int] = None):
+        assert rank_zero_only.rank == 0, 'experiment tried to log from global_rank != 0'
+
+        metric_items: Dict[str:Any] = {k: v for k, v in metrics.items()}
+
+        if 'epoch' in metric_items:
+            epoch: int = metric_items.pop('epoch')
+        else:
+            epoch = None
+
+        for k, v in metric_items.items():
+            name, context = self.parse_context(k)
+            self.experiment.track(v, name=name, step=step, epoch=epoch, context=context)
+
+    def parse_context(self, name):
+        context = {}
+
+        for ctx, mappings in self._context_prefixes.items():
+            for category, prefix in mappings.items():
+                if name.startswith(prefix):
+                    name = name[len(prefix):]
+                    context[ctx] = category
+                    break  # avoid prefix rename cascade
+
+        for ctx, mappings in self._context_postfixes.items():
+            for category, postfix in mappings.items():
+                if name.endswith(postfix):
+                    name = name[:-len(postfix)]
+                    context[ctx] = category
+                    break  # avoid postfix rename cascade
+
+        return name, context
