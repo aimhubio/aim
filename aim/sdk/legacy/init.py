86deb77b35928757da2e9f8b2f7246c0451c3beb
mihran113 <vanyanmihran@gmail.com>
2025-03-13 15:40:04 +0400

    [chore] Remove legacy (`aim 2.x.x`) sdk (#3305)

--- aim/sdk/legacy/init.py
+++
@@ -1,7 +0,0 @@
-from aim.sdk.legacy.deprecation_warning import deprecated
-from aim.sdk.legacy.session import DefaultSession
-
-
-@deprecated
-def init(*args, **kwargs):
-    DefaultSession(*args, **kwargs)
